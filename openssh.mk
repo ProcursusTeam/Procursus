428c81b76b5a51e71966856264fc9ada4b90a783
Diatrus <pinseeker75@outlook.com>
2020-03-13 00:09:23 -0400

    OpenSSH compilation working

--- openssh.mk
+++ openssh.mk
@@ -2,12 +2,22 @@ ifneq ($(CHECKRA1N_MEMO),1)
 $(error Use the main Makefile)
 endif
 
+ifeq ($(UNAME),Linux)
+EXTRA := INSTALL="/usr/bin/install -c --strip-program=$(STRIP)"
+else
+EXTRA :=
+endif
+
 ifneq ($(wildcard openssh/.build_complete),)
 openssh:
 	@echo "Using previously built openssh."
 else
 openssh: setup libressl
-	cd openssh && ./configure -C \
+	if ! [ -f openssh/configure ]; then \
+		cd openssh && autoreconf; \
+	fi
+	cd openssh && $(EXTRA) \
+		./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr \
 		UsePrivilegeSeparation=no \
