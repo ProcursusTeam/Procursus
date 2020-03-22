fb08086ebaf5d11b84e853068e523b8b5200fe93
Diatrus <pinseeker75@outlook.com>
2020-03-22 17:56:07 -0400

    system-cmds add chpass and pack

--- system-cmds.mk
+++ system-cmds.mk
@@ -14,6 +14,15 @@ else
 system-cmds: setup
 	# This hack makes me extremely sad. System-cmds won't build without these libkern headers, but they interfere with iOS headers usually.
 	mv $(BUILD_BASE)/usr/include/libkern.bad $(BUILD_BASE)/usr/include/libkern
+	mkdir -p system-cmds/chpass
+	wget -nc -P system-cmds/chpass \
+		https://raw.githubusercontent.com/coolstar/freebsd-ports-ios/master/lib/libutil/libutil.h \
+		https://raw.githubusercontent.com/coolstar/freebsd-ports-ios/master/lib/libutil/pw_util.c \
+		https://raw.githubusercontent.com/coolstar/freebsd-ports-ios/master/lib/libutil/flopen.c \
+		https://raw.githubusercontent.com/coolstar/freebsd-ports-ios/master/lib/libc/gen/pw_scan.{c,h} \
+		https://raw.githubusercontent.com/coolstar/freebsd-ports-ios/master/usr.bin/chpass/{chpass{.h,.c},edit.c,field.c,table.c,util.c}
+	cd system-cmds/chpass && $(CC) $(CFLAGS) -I. *.c -o chpass
+
 	for gperf in system-cmds/getconf.tproj/*.gperf; do \
 	    LC_ALL=C awk -f system-cmds/getconf.tproj/fake-gperf.awk < $$gperf > system-cmds/getconf.tproj/"$$(basename $$gperf .gperf).c" ; \
 	done
@@ -47,6 +56,7 @@ system-cmds: setup
 	cp -a system-cmds/{dmesg,dynamic_pager} $(BUILD_STAGE)/system-cmds/sbin
 	$(LN) -s ../usr/sbin/reboot $(BUILD_STAGE)/system-cmds/sbin/halt
 	cp -a system-cmds/{arch,getconf,getty,hostinfo,login,passwd} $(BUILD_STAGE)/system-cmds/usr/bin
+	cp -a system-cmds/chpass/chpass $(BUILD_STAGE)/system-cmds/usr/bin
 	$(LN) -s chpass $(BUILD_STAGE)/system-cmds/usr/bin/chfn
 	$(LN) -s chpass $(BUILD_STAGE)/system-cmds/usr/bin/chsh
 	cp -a system-cmds/{ac,accton,iostat,mkfile,pwd_mkdb,sysctl,vifs,vipw,zdump,zic} $(BUILD_STAGE)/system-cmds/usr/sbin
@@ -55,4 +65,21 @@ system-cmds: setup
 	touch system-cmds/.build_complete
 endif
 
-.PHONY: system-cmds
+system-cmds-stage: system-cmds
+	# system-cmds.mk Package Structure
+	rm -rf $(BUILD_DIST)/system-cmds
+	mkdir -p $(BUILD_DIST)/system-cmds
+	
+	# system-cmds.mk Prep system-cmds
+	cp -a $(BUILD_STAGE)/system-cmds/* $(BUILD_DIST)/system-cmds
+
+	# system-cmds.mk Sign
+	$(call SIGN,system-cmds,general.xml)
+	
+	# system-cmds.mk Make .debs
+	$(call PACK,system-cmds,DEB_SYSTEM-CMDS_V)
+	
+	# system-cmds.mk Build cleanup
+	rm -rf $(BUILD_DIST)/system-cmds
+
+.PHONY: system-cmds system-cmds-stage
