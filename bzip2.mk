6b36324a66bb855ff5fc48d1ccbe86c842a67b83
Diatrus <pinseeker75@outlook.com>
2020-03-22 13:33:59 -0400

    Pack bzip2

--- bzip2.mk
+++ bzip2.mk
@@ -16,13 +16,25 @@ bzip2: setup
 		AR=$(AR) \
 		RANLIB=$(RANLIB) \
 		CFLAGS="$(CFLAGS)"
-	$(MAKE) -C $(BUILD_WORK)/bzip2 install \
-		PREFIX=$(BUILD_BASE)/usr \
-		CC=$(CC) \
-		AR=$(AR) \
-		RANLIB=$(RANLIB) \
-		CFLAGS="$(CFLAGS)"
 	touch $(BUILD_WORK)/bzip2/.build_complete
 endif
+
+bzip2-stage: bzip2
+	# bzip2.mk Package Structure
+	rm -rf $(BUILD_DIST)/bzip2
+	mkdir -p $(BUILD_DIST)/bzip2
+	
+	# bzip2.mk Prep bzip2
+	cp -a $(BUILD_STAGE)/bzip2/usr $(BUILD_DIST)/bzip2
+	rm -rf $(BUILD_DIST)/bzip2/usr/man
+	
+	# bzip2.mk Sign
+	$(call SIGN,bzip2,general.xml)
+	
+	# bzip2.mk Make .debs
+	$(call PACK,bzip2,DEB_BZIP2_V)
+	
+	# bzip2.mk Build cleanup
+	rm -rf $(BUILD_DIST)/bzip2
 
 .PHONY: bzip2
