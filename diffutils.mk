5a91d04a57dd03ce461159457765e829676dc11d
Diatrus <pinseeker75@outlook.com>
2020-03-24 13:35:09 -0400

    Pack Diffutils

--- diffutils.mk
+++ diffutils.mk
@@ -20,4 +20,21 @@ diffutils: setup
 	touch $(BUILD_WORK)/diffutils/.build_complete
 endif
 
-.PHONY: diffutils diffutils-stage
+diffutils-package: diffutils-stage
+	# diffutils.mk Package Structure
+	rm -rf $(BUILD_DIST)/diffutils
+	mkdir -p $(BUILD_DIST)/diffutils
+	
+	# diffutils.mk Prep diffutils
+	$(FAKEROOT) cp -a $(BUILD_STAGE)/diffutils/usr $(BUILD_DIST)/diffutils
+	
+	# diffutils.mk Sign
+	$(call SIGN,diffutils,general.xml)
+	
+	# diffutils.mk Make .debs
+	$(call PACK,diffutils,DEB_DIFFUTILS_V)
+	
+	# diffutils.mk Build cleanup
+	rm -rf $(BUILD_DIST)/diffutils
+
+.PHONY: diffutils diffutils-package
