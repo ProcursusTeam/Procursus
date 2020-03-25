f134d2b639fa507645f93cabb1ca90385c4dd1f4
Diatrus <pinseeker75@outlook.com>
2020-03-25 13:36:23 -0400

    Pack less

--- less.mk
+++ less.mk
@@ -22,4 +22,24 @@ less: setup ncurses pcre
 	touch $(BUILD_WORK)/less/.build_complete
 endif
 
-.PHONY: less
+less-package: less-stage
+	# less.mk Package Structure
+	rm -rf $(BUILD_DIST)/less
+	mkdir -p $(BUILD_DIST)/less/{bin,etc/profile.d}
+	
+	# less.mk Prep less
+	$(FAKEROOT) cp -a $(BUILD_STAGE)/less/usr $(BUILD_DIST)/less
+	cp $(BUILD_INFO)/less.sh $(BUILD_DIST)/less/etc/profile.d
+	ln -s /usr/bin/less $(BUILD_DIST)/less/bin/more
+	ln -s /usr/bin/less $(BUILD_DIST)/less/usr/bin/more
+	
+	# less.mk Sign
+	$(call SIGN,less,general.xml)
+	
+	# less.mk Make .debs
+	$(call PACK,less,DEB_LESS_V)
+	
+	# less.mk Build cleanup
+	rm -rf $(BUILD_DIST)/less
+
+.PHONY: less less-package
