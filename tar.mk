a7e293d771ef97d5860b46327d0efa1108f2a361
Diatrus <pinseeker75@outlook.com>
2020-03-25 14:53:39 -0400

    Pack tar

--- tar.mk
+++ tar.mk
@@ -17,6 +17,7 @@ tar: setup
 	cd $(BUILD_WORK)/tar && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr \
+		--disable-nls \
 		gl_cv_func_ftello_works=yes
 	$(MAKE) -C $(BUILD_WORK)/tar
 	$(MAKE) -C $(BUILD_WORK)/tar install \
@@ -24,4 +25,22 @@ tar: setup
 	touch $(BUILD_WORK)/tar/.build_complete
 endif
 
-.PHONY: tar
+tar-package: tar-stage
+	# tar.mk Package Structure
+	rm -rf $(BUILD_DIST)/tar
+	mkdir -p $(BUILD_DIST)/tar/bin
+	
+	# tar.mk Prep tar
+	$(FAKEROOT) cp -a $(BUILD_STAGE)/tar/usr $(BUILD_DIST)/tar
+	ln -s /usr/bin/tar $(BUILD_DIST)/tar/bin/tar
+	
+	# tar.mk Sign
+	$(call SIGN,tar,general.xml)
+	
+	# tar.mk Make .debs
+	$(call PACK,tar,DEB_TAR_V)
+	
+	# tar.mk Build cleanup
+	rm -rf $(BUILD_DIST)/tar
+
+.PHONY: tar tar-package
