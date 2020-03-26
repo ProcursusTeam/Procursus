53e29cb13f9102da807fbc99413ea9c83209d8f7
Diatrus <pinseeker75@outlook.com>
2020-03-25 21:09:02 -0400

    Pack libgpg-error

--- libgpg-error.mk
+++ libgpg-error.mk
@@ -13,7 +13,8 @@ libgpg-error: setup
 	$(SED) -i '/{"armv7-unknown-linux-gnueabihf"  },/a \ \ \ \ {"$(GNU_HOST_TRIPLE)"},' $(BUILD_WORK)/libgpg-error/src/mkheader.c
 	cd $(BUILD_WORK)/libgpg-error && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
-		--prefix=/usr
+		--prefix=/usr \
+		--disable-nls
 	$(MAKE) -C $(BUILD_WORK)/libgpg-error
 	$(MAKE) -C $(BUILD_WORK)/libgpg-error install \
 		DESTDIR=$(BUILD_STAGE)/libgpg-error
@@ -22,4 +23,21 @@ libgpg-error: setup
 	touch $(BUILD_WORK)/libgpg-error/.build_complete
 endif
 
-.PHONY: libgpg-error
+libgpg-error-package: libgpg-error-stage
+	# libgpg-error.mk Package Structure
+	rm -rf $(BUILD_DIST)/libgpg-error
+	mkdir -p $(BUILD_DIST)/libgpg-error
+	
+	# libgpg-error.mk Prep libgpg-error
+	$(FAKEROOT) cp -a $(BUILD_STAGE)/libgpg-error/usr $(BUILD_DIST)/libgpg-error
+	
+	# libgpg-error.mk Sign
+	$(call SIGN,libgpg-error,general.xml)
+	
+	# libgpg-error.mk Make .debs
+	$(call PACK,libgpg-error,DEB_LIBGPG-ERROR_V)
+	
+	# libgpg-error.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libgpg-error
+
+.PHONY: libgpg-error libgpg-error-package
