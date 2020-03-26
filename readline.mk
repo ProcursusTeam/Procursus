d3c8aef3557724ff5537d619dc03df07ae199708
Diatrus <pinseeker75@outlook.com>
2020-03-25 22:09:03 -0400

    Pack readline

--- readline.mk
+++ readline.mk
@@ -16,8 +16,8 @@ readline: setup ncurses
 		ac_cv_type_sig_atomic_t=no \
 		LDFLAGS="$(CLFLAGS) $(LDFLAGS)"
 	$(MAKE) -C $(BUILD_WORK)/readline \
-		SHLIB_LIBS="-lncursesw"
-		TERMCAP_LIB="-lncursesw"
+		SHLIB_LIBS="-lreadlinew"
+		TERMCAP_LIB="-lreadlinew"
 	$(MAKE) -C $(BUILD_WORK)/readline install \
 		DESTDIR=$(BUILD_STAGE)/readline
 	$(MAKE) -C $(BUILD_WORK)/readline install \
@@ -25,4 +25,21 @@ readline: setup ncurses
 	touch $(BUILD_WORK)/readline/.build_complete
 endif
 
-.PHONY: readline
+readline-package: readline-stage
+	# readline.mk Package Structure
+	rm -rf $(BUILD_DIST)/readline
+	mkdir -p $(BUILD_DIST)/readline
+	
+	# readline.mk Prep readline
+	$(FAKEROOT) cp -a $(BUILD_STAGE)/readline/usr $(BUILD_DIST)/readline
+	
+	# readline.mk Sign
+	$(call SIGN,readline,general.xml)
+	
+	# readline.mk Make .debs
+	$(call PACK,readline,DEB_READLINE_V)
+	
+	# readline.mk Build cleanup
+	rm -rf $(BUILD_DIST)/readline
+
+.PHONY: readline readline-package
