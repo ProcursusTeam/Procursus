0405c4b09e25d9f0de5314b27337e10db53477e7
Diatrus <pinseeker75@outlook.com>
2020-04-19 02:56:07 -0400

    Add GNU make

---
+++ make.mk
@@ -0,0 +1,46 @@
+ifneq ($(CHECKRA1N_MEMO),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS  += make
+DOWNLOAD     += https://ftp.gnu.org/gnu/make/make-$(MAKE_VERSION).tar.gz{,.sig}
+MAKE_VERSION := 4.3
+DEB_MAKE_V   ?= $(MAKE_VERSION)
+
+make-setup: setup
+	$(call PGP_VERIFY,make-$(MAKE_VERSION).tar.gz)
+	$(call EXTRACT_TAR,make-$(MAKE_VERSION).tar.gz,make-$(MAKE_VERSION),make)
+
+ifneq ($(wildcard $(BUILD_WORK)/make/.build_complete),)
+make:
+	@echo "Using previously built make."
+else
+make: make-setup
+	$(SED) -i '/case ENOEXEC:/a \ \ \ \ case EPERM:' $(BUILD_WORK)/make/src/job.c
+	cd $(BUILD_WORK)/make && ./configure -C \
+		--host=$(GNU_HOST_TRIPLE) \
+		--prefix=/usr
+	+$(MAKE) -C $(BUILD_WORK)/make
+	+$(MAKE) -C $(BUILD_WORK)/make install \
+		DESTDIR="$(BUILD_STAGE)/make"
+	touch $(BUILD_WORK)/make/.build_complete
+endif
+
+make-package: make-stage
+	# make.mk Package Structure
+	rm -rf $(BUILD_DIST)/make
+	mkdir -p $(BUILD_DIST)/make
+	
+	# make.mk Prep make
+	cp -a $(BUILD_STAGE)/make/usr $(BUILD_DIST)/make
+	
+	# make.mk Sign
+	$(call SIGN,make,general.xml)
+	
+	# make.mk Make .debs
+	$(call PACK,make,DEB_MAKE_V)
+	
+	# make.mk Build cleanup
+	rm -rf $(BUILD_DIST)/make
+
+.PHONY: make make-package
