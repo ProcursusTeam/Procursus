c3466cfd08bd4b5e1d577af61ecb6b2e76f100f9
Diatrus <me@diatr.us>
2020-05-15 05:42:15 -0700

    Fix last stuff for tonight

---
+++ build_tools/make_dummy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)"
+mkdir -p ./"${1}"_"${2}"_"${3}"/DEBIAN
+touch ./"${1}"_"${2}"_"${3}"/DEBIAN/control
+echo -e "Package: ${1}\nMaintainer: Hayden Seay\nVersion:${2}\nArchitecture:${3}\nDescription: Dummy package to help migration from other bootstraps. Safe to remove.\n" > ./"${1}"_"${2}"_"${3}"/DEBIAN/control
+dpkg-deb -b ./"${1}"_"${2}"_"${3}"
+rm -rf ./"${1}"_"${2}"_"${3}"
