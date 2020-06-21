11d029bc67d1b0375e20de20b1a9912e94add314
CRKatri <katri.cameron@gmail.com>
2020-06-21 00:02:18 -0400

    Its aligned on my machine, lol

--- lolcat.mk
+++ lolcat.mk
@@ -2,10 +2,10 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS		+= lolcat 
-DOWNLOAD		+= https://github.com/jaseg/lolcat/archive/v$(LOLCAT_VERSION).tar.gz
-LOLCAT_VERSION  := 1.0
-DEB_LOLCAT_V    ?= $(LOLCAT_VERSION)
+SUBPROJECTS    += lolcat 
+DOWNLOAD       += https://github.com/jaseg/lolcat/archive/v$(LOLCAT_VERSION).tar.gz
+LOLCAT_VERSION := 1.0
+DEB_LOLCAT_V   ?= $(LOLCAT_VERSION)
 
 lolcat-setup: setup
 	$(call EXTRACT_TAR,v$(LOLCAT_VERSION).tar.gz,lolcat-$(LOLCAT_VERSION),lolcat)
