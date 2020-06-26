f3c5960606ab4d0822e522b1528ee4027612e3ec
M1staAwesome <37033149+M1staAwesome@users.noreply.github.com>
2020-06-26 03:14:34 -0500

    Add libirecovery 1.0 (#30)
    * Add libirecovery 1.0
    
    * i need to stop copying makefiles
    
    * fix extracting
    
    * Update Makefile
    
    * Update Makefile
    
    * Update libirecovery.mk
    
    Co-authored-by: Hayden <me@diatr.us>

--- Makefile
+++ Makefile
@@ -533,7 +533,7 @@ rebuild-%:
 
 setup:
 	mkdir -p \
-		$(BUILD_BASE) $(BUILD_BASE)/{System/Library/Frameworks,usr/{include/{bsm,sys,IOKit,mach/machine},lib}} \
+		$(BUILD_BASE) $(BUILD_BASE)/{System/Library/Frameworks,usr/{include/{bsm,sys,IOKit,libkern,mach/machine},lib}} \
 		$(BUILD_WORK) $(BUILD_STAGE) $(BUILD_DIST) $(BUILD_STRAP)
 
 	git submodule update --init --recursive
@@ -549,8 +549,9 @@ setup:
 
 	@# Copy headers from MacOSX.sdk
 	$(CP) -af $(MACOSX_SYSROOT)/usr/include/{arpa,net,xpc} $(BUILD_BASE)/usr/include
+	$(CP) -af $(MACOSX_SYSROOT)/usr/include/libkern/OSTypes.h $(BUILD_BASE)/usr/include/libkern
 	$(CP) -af $(MACOSX_SYSROOT)/usr/include/sys/{tty*,proc*,ptrace,kern*,random,vnode}.h $(BUILD_BASE)/usr/include/sys
-	$(CP) -af $(MACOSX_SYSROOT)/System/Library/Frameworks/IOKit.framework/Headers/ps $(BUILD_BASE)/usr/include/IOKit
+	$(CP) -af $(MACOSX_SYSROOT)/System/Library/Frameworks/IOKit.framework/Headers/* $(BUILD_BASE)/usr/include/IOKit
 	$(CP) -af $(MACOSX_SYSROOT)/usr/include/{ar,launch,libproc,tzfile}.h $(BUILD_BASE)/usr/include
 	$(CP) -af $(MACOSX_SYSROOT)/usr/include/mach/{*.defs,{mach_vm,shared_region}.h} $(BUILD_BASE)/usr/include/mach
 	$(CP) -af $(MACOSX_SYSROOT)/usr/include/mach/machine/*.defs $(BUILD_BASE)/usr/include/mach/machine
---
+++ libirecovery.mk
@@ -0,0 +1,48 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS          += libirecovery
+LIBIRECOVERY_VERSION := 1.0.0
+DEB_LIBIRECOVERY_V   ?= $(LIBIRECOVERY_VERSION)
+
+libirecovery-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libimobiledevice/libirecovery/releases/download/$(LIBIRECOVERY_VERSION)/libirecovery-$(LIBIRECOVERY_VERSION).tar.bz2
+	$(call EXTRACT_TAR,libirecovery-$(LIBIRECOVERY_VERSION).tar.bz2,libirecovery-$(LIBIRECOVERY_VERSION),libirecovery)
+
+ifneq ($(wildcard $(BUILD_WORK)/libirecovery/.build_complete),)
+libirecovery:
+	@echo "Using previously built libirecovery."
+else
+libirecovery: libirecovery-setup readline
+	cd $(BUILD_WORK)/libirecovery && ./configure \
+		--host=$(GNU_HOST_TRIPLE) \
+		--prefix=/usr \
+		--with-sysroot=$(SYSROOT) \
+		--with-iokit \
+		ac_cv_header_IOKit_usb_IOUSBLib_h=yes
+	+$(MAKE) -C $(BUILD_WORK)/libirecovery \
+		CFLAGS="$(CFLAGS) -D__OPEN_SOURCE__"
+	+$(MAKE) -C $(BUILD_WORK)/libirecovery install \
+		DESTDIR=$(BUILD_STAGE)/libirecovery
+	touch $(BUILD_WORK)/libirecovery/.build_complete
+endif
+
+libirecovery-package: libirecovery-stage
+	# libirecovery.mk Package Structure
+	rm -rf $(BUILD_DIST)/libirecovery
+	mkdir -p $(BUILD_DIST)/libirecovery
+	
+	# libirecovery.mk Prep libirecovery
+	cp -a $(BUILD_STAGE)/libirecovery/usr $(BUILD_DIST)/libirecovery
+	
+	# libirecovery.mk Sign
+	$(call SIGN,libirecovery,general.xml)
+	
+	# libirecovery.mk Make .debs
+	$(call PACK,libirecovery,DEB_LIBIRECOVERY_V)
+	
+	# libirecovery.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libirecovery
+
+.PHONY: libirecovery libirecovery-package
