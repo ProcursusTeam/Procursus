450bd4ab85a9c7c8d6313c652384da5c644ed192
M1staAwesome <37033149+M1staAwesome@users.noreply.github.com>
2020-06-26 21:10:38 -0500

    Switch to user based nanorc since system based doesn't get read by nano for some reason

--- nanorc.mk
+++ nanorc.mk
@@ -17,9 +17,10 @@ nanorc:
 else
 nanorc: nanorc-setup nano
 	mkdir -p $(BUILD_STAGE)/nanorc/var/mobile/.nano
-	mkdir -p $(BUILD_STAGE)/nanorc/etc
-	echo "include "/var/mobile/.nano/*.nanorc"" >> $(BUILD_STAGE)/nanorc/etc/nanorc
+	mkdir -p $(BUILD_STAGE)/nanorc/var/root
+	echo "include "/var/mobile/.nano/*.nanorc"" >> $(BUILD_STAGE)/nanorc/var/mobile/.nanorc
 	cp -af $(BUILD_WORK)/nanorc/*.nanorc $(BUILD_STAGE)/nanorc/var/mobile/.nano
+	ln -s $(BUILD_STAGE)/nanorc/var/mobile/.nanorc $(BUILD_STAGE)/nanorc/var/root/.nanorc
 	touch $(BUILD_WORK)/nanorc/.build_complete
 endif
 
@@ -29,7 +30,7 @@ nanorc-package: nanorc-stage
 	mkdir -p $(BUILD_DIST)/nanorc
 	
 	# nanorc.mk Prep nanorc
-	cp -a $(BUILD_STAGE)/nanorc/{etc,var} $(BUILD_DIST)/nanorc
+	cp -a $(BUILD_STAGE)/nanorc/var $(BUILD_DIST)/nanorc
 	
 	# nanorc.mk Make .debs
 	$(call PACK,nanorc,DEB_NANORC_V)
