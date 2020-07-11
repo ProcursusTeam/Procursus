41ecf7f311b8dd63385f00e9527c4b1076a68cba
M1staAwesome <37033149+M1staAwesome@users.noreply.github.com>
2020-07-11 13:54:11 -0500

    Enable UTF8 & Reading config from /etc/nanorc

--- nano.mk
+++ nano.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS  += nano
 NANO_VERSION := 4.9.3
-DEB_NANO_V   ?= $(NANO_VERSION)
+DEB_NANO_V   ?= $(NANO_VERSION)-1
 
 nano-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://ftpmirror.gnu.org/nano/nano-$(NANO_VERSION).tar.xz{,.sig}
@@ -23,8 +23,10 @@ nano: nano-setup ncurses gettext
 		--disable-dependency-tracking \
 		--enable-color \
 		--enable-extra \
+		--sysconfdir=/etc \
 		--enable-multibuffer \
 		--enable-nanorc \
+		--enable-utf8 \
 		NCURSESW_LIBS=$(BUILD_BASE)/usr/lib/libncursesw.dylib
 	+$(MAKE) -C $(BUILD_WORK)/nano
 	+$(MAKE) -C $(BUILD_WORK)/nano install \
