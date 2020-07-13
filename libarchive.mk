bbc23af3e3bb95a7aa3029cab792765d16acd3eb
Cameron Katri <katri.cameron@gmail.com>
2020-07-13 16:41:36 -0400

    Add libarchive

---
+++ libarchive.mk
@@ -0,0 +1,45 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS        += libarchive
+LIBARCHIVE_VERSION := 3.4.3
+DEB_LIBARCHIVE_V   ?= $(LIBARCHIVE_VERSION)
+
+libarchive-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libarchive/libarchive/releases/download/v$(LIBARCHIVE_VERSION)/libarchive-$(LIBARCHIVE_VERSION).tar.xz
+	$(call EXTRACT_TAR,libarchive-$(LIBARCHIVE_VERSION).tar.xz,libarchive-$(LIBARCHIVE_VERSION),libarchive)
+
+ifneq ($(wildcard $(BUILD_WORK)/libarchive/.build_complete),)
+libarchive:
+	@echo "Using previously built libarchive."
+else
+libarchive: libarchive-setup openssl zstd bzip2 xz
+	cd $(BUILD_WORK)/libarchive && ./configure \
+		--host=$(GNU_HOST_TRIPLE) \
+		--without-zlib \
+		--prefix=/usr
+	+$(MAKE) -C $(BUILD_WORK)/libarchive
+	+$(MAKE) -C $(BUILD_WORK)/libarchive install \
+		DESTDIR=$(BUILD_STAGE)/libarchive
+	touch $(BUILD_WORK)/libarchive/.build_complete
+endif
+
+libarchive-package: libarchive-stage
+	# rsync.mk Package Structure
+	rm -rf $(BUILD_DIST)/libarchive
+	mkdir -p $(BUILD_DIST)/libarchive
+	
+	# rsync.mk Prep rsync
+	cp -a $(BUILD_STAGE)/libarchive/usr $(BUILD_DIST)/libarchive
+	
+	# rsync.mk Sign
+	$(call SIGN,libarchive,general.xml)
+	
+	# rsync.mk Make .debs
+	$(call PACK,libarchive,DEB_LIBARCHIVE_V)
+	
+	# rsync.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libarchive
+
+.PHONY: libarchive libarchive-package
