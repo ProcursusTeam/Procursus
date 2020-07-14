ae59a537b93c639742ec2c204b27e894c5ff76e4
CRKatri <48231187+CRKatri@users.noreply.github.com>
2020-07-14 18:15:50 -0400

    Add libarchive (#61)
    * Add libarchive
    
    * Enable zlib (hopefully it'll work), add installing to BUILD_BASE
    
    * Inital zlib
    
    * Unneeded build-deps for zlib
    
    * Add zlib depends
    
    * Remove uneeded zlb
    
    * Easy change to make this actually build
    
    * This is included in iOS, don't mess with it.
    
    Co-authored-by: Hayden <me@diatr.us>

---
+++ libarchive.mk
@@ -0,0 +1,48 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS        += libarchive
+LIBARCHIVE_VERSION := 3.4.3
+DEB_LIBARCHIVE_V   ?= $(LIBARCHIVE_VERSION)
+
+libarchive-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libarchive/libarchive/releases/download/v$(LIBARCHIVE_VERSION)/libarchive-$(LIBARCHIVE_VERSION).tar.xz
+	$(call EXTRACT_TAR,libarchive-$(LIBARCHIVE_VERSION).tar.xz,libarchive-$(LIBARCHIVE_VERSION),libarchive)
+
+ifneq ($(wildcard $(BUILD_WORK)/libarchive/.build_complete),)
+libarchive:
+	@echo "Using previously built libarchive."
+else
+libarchive: libarchive-setup lz4 zstd xz
+	cd $(BUILD_WORK)/libarchive && ./configure \
+		--host=$(GNU_HOST_TRIPLE) \
+		--disable-dependency-tracking \
+		--prefix=/usr
+	+$(MAKE) -C $(BUILD_WORK)/libarchive
+	+$(MAKE) -C $(BUILD_WORK)/libarchive install \
+		DESTDIR="$(BUILD_STAGE)/libarchive"
+	+$(MAKE) -C $(BUILD_WORK)/libarchive install \
+		DESTDIR="$(BUILD_BASE)"
+	rm -f $(BUILD_STAGE)/libarchive/usr/lib/libarchive.dylib
+	touch $(BUILD_WORK)/libarchive/.build_complete
+endif
+
+libarchive-package: libarchive-stage
+	# rsync.mk Package Structure
+	rm -rf $(BUILD_DIST)/libarchive
+	mkdir -p $(BUILD_DIST)/libarchive
+	
+	# rsync.mk Prep rsync
+	cp -a $(BUILD_STAGE)/libarchive/usr $(BUILD_DIST)/libarchive
+	
+	# rsync.mk Sign
+	$(call SIGN,libarchive,general.xml)
+	
+	# rsync.mk Make .debs
+	$(call PACK,libarchive,DEB_LIBARCHIVE_V)
+	
+	# rsync.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libarchive
+
+.PHONY: libarchive libarchive-package
