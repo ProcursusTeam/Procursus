a8313ee65515236c3c37ca5257b943f02d3f027c
1Conan <me@1conan.com>
2020-07-16 09:48:06 +0800

    replace dimentio submodule

--- dimentio.mk
+++ dimentio.mk
@@ -7,10 +7,10 @@ DIMENTIO_VERSION   := 1.0
 DEB_DIMENTIO_V     ?= $(DIMENTIO_VERSION)
 
 dimentio-setup: setup
-	mkdir -p $(BUILD_WORK)/dimentio
-	rm -rf 
-	cp -af dimentio/dimentio.c $(BUILD_WORK)/dimentio/dimentio.c
-
+	-[ ! -e "$(BUILD_SOURCE)/dimentio-$(DIMENTIO_VERSION).tar.gz" ] \
+		&& wget -nc -O$(BUILD_SOURCE)/dimentio-$(DIMENTIO_VERSION).tar.gz \
+			https://github.com/ProcursusTeam/dimentio/archive/$(DIMENTIO_VERSION).tar.gz
+	$(call EXTRACT_TAR,dimentio-$(DIMENTIO_VERSION).tar.gz,dimentio-$(DIMENTIO_VERSION),dimentio)
 
 ifneq ($(wildcard $(BUILD_WORK)/dimentio/.build_complete),)
 dimentio:
