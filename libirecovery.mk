ca6eab15b96d3e855ea58c1b02f9a8410dcec09f
Hayden <me@diatr.us>
2020-08-01 00:07:25 -0400

    Debianize libirecovery

--- libirecovery.mk
+++ libirecovery.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS          += libirecovery
 LIBIRECOVERY_VERSION := 1.0.0
-DEB_LIBIRECOVERY_V   ?= $(LIBIRECOVERY_VERSION)-1
+DEB_LIBIRECOVERY_V   ?= $(LIBIRECOVERY_VERSION)
 
 libirecovery-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libimobiledevice/libirecovery/releases/download/$(LIBIRECOVERY_VERSION)/libirecovery-$(LIBIRECOVERY_VERSION).tar.bz2
@@ -29,19 +29,31 @@ endif
 
 libirecovery-package: libirecovery-stage
 	# libirecovery.mk Package Structure
-	rm -rf $(BUILD_DIST)/libirecovery
-	mkdir -p $(BUILD_DIST)/libirecovery
+	rm -rf $(BUILD_DIST)/libirecovery{3,-dev,-utils}
+	mkdir -p $(BUILD_DIST)/libirecovery3/usr/lib \
+		$(BUILD_DIST)/libirecovery-dev/usr/lib \
+		$(BUILD_DIST)/libirecovery-utils/usr
+
+	# libirecovery.mk Prep libirecovery3
+	cp -a $(BUILD_STAGE)/libirecovery/usr/lib/libirecovery-1.0.3.dylib $(BUILD_DIST)/libirecovery3/usr/lib/
 	
-	# libirecovery.mk Prep libirecovery
-	cp -a $(BUILD_STAGE)/libirecovery/usr $(BUILD_DIST)/libirecovery
+	# libirecovery.mk Prep libirecovery-dev
+	cp -a $(BUILD_STAGE)/libirecovery/usr/lib/{pkgconfig,libirecovery-1.0.{a,dylib}} $(BUILD_DIST)/libirecovery-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libirecovery/usr/include $(BUILD_DIST)/libirecovery-dev/usr
 	
+	# libirecovery.mk Prep libirecovery-utils
+	cp -a $(BUILD_STAGE)/libirecovery/usr/bin $(BUILD_DIST)/libirecovery-utils/usr
+
 	# libirecovery.mk Sign
-	$(call SIGN,libirecovery,general.xml)
-	
+	$(call SIGN,libirecovery3,general.xml)
+	$(call SIGN,libirecovery-utils,general.xml)
+
 	# libirecovery.mk Make .debs
-	$(call PACK,libirecovery,DEB_LIBIRECOVERY_V)
-	
+	$(call PACK,libirecovery3,DEB_LIBIRECOVERY_V)
+	$(call PACK,libirecovery-dev,DEB_LIBIRECOVERY_V)
+	$(call PACK,libirecovery-utils,DEB_LIBIRECOVERY_V)
+
 	# libirecovery.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libirecovery
+	rm -rf $(BUILD_DIST)/libirecovery{3,-dev,-utils}
 
 .PHONY: libirecovery libirecovery-package
