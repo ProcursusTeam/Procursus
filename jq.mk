85de225c01dbe4f8aa307b5dd626f35d0453d9f9
absidue <48293849+absidue@users.noreply.github.com>
2020-08-07 02:39:52 +0200

    Debianise oniguruam and jq

--- jq.mk
+++ jq.mk
@@ -14,16 +14,15 @@ ifneq ($(wildcard $(BUILD_WORK)/jq/.build_complete),)
 jq:
 	@echo "Using previously built jq."
 else
-jq: jq-setup liboniguruma
+jq: jq-setup libonig
 	cd $(BUILD_WORK)/jq && autoreconf -fi
-	cd $(BUILD_WORK)/jq && CFLAGS="$(CFLAGS) -D_REENTRANT" ./configure \
+	cd $(BUILD_WORK)/jq && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr \
-		--enable-shared=yes \
-		--enable-static=no \
-		--with-oniguruma=$(BUILD_STAGE)/liboniguruma/usr \
+		--with-oniguruma=$(BUILD_STAGE)/libonig/usr \
 		--disable-docs \
-		--disable-maintainer-mode
+		--disable-maintainer-mode \
+		CFLAGS="$(CFLAGS) -D_REENTRANT"
 	+$(MAKE) -C $(BUILD_WORK)/jq \
 		DESTDIR=$(BUILD_STAGE)/jq
 	+$(MAKE) -C $(BUILD_WORK)/jq install \
@@ -33,19 +32,32 @@ endif
 
 jq-package: jq-stage
 	# jq.mk Package Structure
-	rm -rf $(BUILD_DIST)/jq
-	mkdir -p $(BUILD_DIST)/jq
+	rm -rf $(BUILD_DIST)/{jq,libjq-dev,libjq1}
+	mkdir -p $(BUILD_DIST)/jq/usr/{bin,share/man/man1} \
+			$(BUILD_DIST)/libjq-dev/usr/{include,lib} \
+			$(BUILD_DIST)/libjq1/usr/lib
 
 	# jq.mk Prep jq
-	cp -a $(BUILD_STAGE)/jq/usr $(BUILD_DIST)/jq
+	cp -a $(BUILD_STAGE)/jq/usr/bin/jq $(BUILD_DIST)/jq/usr/bin
+	cp -a $(BUILD_STAGE)/jq/usr/share/man/man1/jq.1 $(BUILD_DIST)/jq/usr/share/man/man1
+
+	# jq.mk Prep libjq-dev
+	cp -a $(BUILD_STAGE)/jq/usr/include/{jq.h,jv.h} $(BUILD_DIST)/libjq-dev/usr/include
+	cp -a $(BUILD_STAGE)/jq/usr/lib/libjq.dylib $(BUILD_DIST)/libjq-dev/usr/lib
+
+	# jq.mk Prep libjq1
+	cp -a $(BUILD_STAGE)/jq/usr/lib/libjq.{a,1.dylib} $(BUILD_DIST)/libjq1/usr/lib
 
 	# jq.mk Sign
 	$(call SIGN,jq,general.xml)
+	$(call SIGN,libjq1,general.xml)
 
 	# jq.mk Make .debs
 	$(call PACK,jq,DEB_JQ_V)
+	$(call PACK,libjq-dev,DEB_JQ_V)
+	$(call PACK,libjq1,DEB_JQ_V)
 
 	# jq.mk Build cleanup
-	rm -rf $(BUILD_DIST)/jq
+	rm -rf $(BUILD_DIST)/{jq,libjq-dev,libjq1}
 
 .PHONY: jq jq-package
