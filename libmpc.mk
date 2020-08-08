13177d879317668d12a27b47714876c97dd5f698
Brennan LeBlanc <me@brennanleblanc.ca>
2020-08-08 01:35:15 -0300

    Debianize

--- libmpc.mk
+++ libmpc.mk
@@ -14,7 +14,7 @@ ifneq ($(wildcard $(BUILD_WORK)/mpc/.build_complete),)
 libmpc:
 	@echo "Using previously built libmpc."
 else
-libmpc: libmpc-setup libgmp10 mpfr
+libmpc: libmpc-setup libgmp10 libmpfr
 	cd $(BUILD_WORK)/mpc && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr
@@ -30,20 +30,20 @@ libmpc-package: libmpc-stage
 	# libmpc.mk Package Structure
 	rm -rf $(BUILD_DIST)/libmpc
 	mkdir -p \
-		$(BUILD_DIST)/libmpc
+		$(BUILD_DIST)/libmpc3
 		$(BUILD_DIST)/libmpc-dev/{lib,include}
 	
 	# libmpc.mk Prep mpc
-	cp -a $(BUILD_STAGE)/mpc/usr/lib/libmpc*dylib $(BUILD_DIST)/libmpc/usr/lib
+	cp -a $(BUILD_STAGE)/mpc/usr/lib/libmpc*dylib $(BUILD_DIST)/libmpc3/usr/lib
     	cp -a $(BUILD_STAGE)/mpc/usr/include $(BUILD_DIST)/libmpc-dev/usr
     	cp -a $(BUILD_STAGE)/mpc/usr/lib/libmpc.a $(BUILD_DIST)/libmpc-dev/usr/lib
 	
 	# libmpc.mk Sign
-	$(call SIGN,libmpc,general.xml)
+	$(call SIGN,libmpc3,general.xml)
     	$(call SIGN,libmpc-dev,general.xml)
 	
 	# libmpc.mk Make .debs
-	$(call PACK,libmpc,DEB_LIBMPC_V)
+	$(call PACK,libmpc3,DEB_LIBMPC_V)
     	$(call PACK,libmpc-dev,DEB_LIBMPC_V)
 	
 	# libmpc.mk Build cleanup
