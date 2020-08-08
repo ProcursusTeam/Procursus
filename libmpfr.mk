57c461b6d91cf73e883eaa25e5ae3d77baed7154
Brennan LeBlanc <me@brennanleblanc.ca>
2020-08-08 12:56:43 -0300

    Final cleanup

--- libmpfr.mk
+++ libmpfr.mk
@@ -8,22 +8,22 @@ DEB_LIBMPFR_V   ?= $(LIBMPFR_VERSION)
 
 libmpfr-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://www.mpfr.org/mpfr-current/mpfr-$(LIBMPFR_VERSION).tar.gz
-	$(call EXTRACT_TAR,mpfr-$(LIBMPFR_VERSION).tar.gz,mpfr-$(LIBMPFR_VERSION),mpfr)
+	$(call EXTRACT_TAR,mpfr-$(LIBMPFR_VERSION).tar.gz,mpfr-$(LIBMPFR_VERSION),libmpfr)
 
-ifneq ($(wildcard $(BUILD_WORK)/mpfr/.build_complete),)
+ifneq ($(wildcard $(BUILD_WORK)/libmpfr/.build_complete),)
 libmpfr:
 	@echo "Using previously built libmpfr."
 else
 libmpfr: libmpfr-setup
-	cd $(BUILD_WORK)/mpfr && ./configure -C \
+	cd $(BUILD_WORK)/libmpfr && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr
-	+$(MAKE) -C $(BUILD_WORK)/mpfr
-	+$(MAKE) -C $(BUILD_WORK)/mpfr install \
+	+$(MAKE) -C $(BUILD_WORK)/linmpfr
+	+$(MAKE) -C $(BUILD_WORK)/libmpfr install \
 		DESTDIR=$(BUILD_STAGE)/mpfr
-	+$(MAKE) -C $(BUILD_WORK)/mpfr install \
+	+$(MAKE) -C $(BUILD_WORK)/libmpfr install \
 		DESTDIR=$(BUILD_BASE)
-	touch $(BUILD_WORK)/mpfr/.build_complete
+	touch $(BUILD_WORK)/libmpfr/.build_complete
 endif
 
 libmpfr-package: libmpfr-stage
@@ -34,9 +34,9 @@ libmpfr-package: libmpfr-stage
         	$(BUILD_DIST)/libmpfr-dev/usr/{lib,include}
 	
 	# libmpfr.mk Prep mpfr
-	cp -a $(BUILD_STAGE)/mpfr/usr/lib/libmpfr*dylib $(BUILD_DIST)/libmpfr6/usr/lib
-	cp -a $(BUILD_STAGE)/mpfr/usr/include $(BUILD_DIST)/libmpfr-dev/usr
-	cp -a $(BUILD_STAGE)/mpfr/usr/lib/libmpfr.a $(BUILD_DIST)/libmpfr-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libmpfr/usr/lib/libmpfr*dylib $(BUILD_DIST)/libmpfr6/usr/lib
+	cp -a $(BUILD_STAGE)/libmpfr/usr/include $(BUILD_DIST)/libmpfr-dev/usr
+	cp -a $(BUILD_STAGE)/libmpfr/usr/lib/libmpfr.a $(BUILD_DIST)/libmpfr-dev/usr/lib
 	
 	# libmpfr.mk Sign
 	$(call SIGN,libmpfr6,general.xml)
