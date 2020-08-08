5f56de77d0e37e04585698ae6a4d033f51fc10a9
Zamiur <20298544+zamiur@users.noreply.github.com>
2020-08-08 03:42:58 +0000

    Create fff.mk
    On the right branch this time :D

---
+++ build_info/fff.mk
@@ -0,0 +1,12 @@
+Package: fff
+Priority: optional
+Section: Utilities
+Maintainer: @DEB_MAINTAINER@
+Architecture: @DEB_ARCH@
+Version: @DEB_FFF_V@
+Depends: bash, coreutils
+Suggests: w3m-img, vim, xdg-utils
+Description: fff (F---ing Fast File-Manager)
+Homepage: https://github.com/dylanaraps/fff/
+Name: fff
+Tag: purpose::console
