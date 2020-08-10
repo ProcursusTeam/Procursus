605c9945d834f8edef78614f710e2dd3f886a10d
Cameron Katri <katri.cameron@gmail.com>
2020-08-09 23:38:34 -0400

    Mosh compiles (Not linux)

--- mosh.mk
+++ mosh.mk
@@ -9,6 +9,7 @@ DEB_MOSH_V   ?= $(MOSH_VERSION)
 mosh-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://mosh.org/mosh-$(MOSH_VERSION).tar.gz
 	$(call EXTRACT_TAR,mosh-$(MOSH_VERSION).tar.gz,mosh-$(MOSH_VERSION),mosh)
+	$(call DO_PATCH,mosh,mosh,-p1)
 
 ifneq ($(wildcard $(BUILD_WORK)/mosh/.build_complete),)
 mosh:
@@ -17,7 +18,10 @@ else
 mosh: mosh-setup libprotobuf
 	cd $(BUILD_WORK)/mosh && ./configure \
 		--host=$(GNU_HOST_TRIPLE) \
-		--prefix=/usr
+		--prefix=/usr \
+		--with-ncursesw \
+		--disable-dependency-tracking \
+		--enable-completion
 	+$(MAKE) -C $(BUILD_WORK)/mosh 
 	+$(MAKE) -C $(BUILD_WORK)/mosh install \
 		DESTDIR=$(BUILD_STAGE)/mosh
@@ -25,20 +29,20 @@ mosh: mosh-setup libprotobuf
 endif
 
 mosh-package: mosh-stage
-	# rsync.mk Package Structure
+	# mosh.mk Package Structure
 	rm -rf $(BUILD_DIST)/mosh
 	mkdir -p $(BUILD_DIST)/mosh
 	
-	# rsync.mk Prep rsync
+	# mosh.mk Prep mosh
 	cp -a $(BUILD_STAGE)/mosh/usr $(BUILD_DIST)/mosh
 	
-	# rsync.mk Sign
+	# mosh.mk Sign
 	$(call SIGN,mosh,general.xml)
 	
-	# rsync.mk Make .debs
+	# mosh.mk Make .debs
 	$(call PACK,mosh,DEB_MOSH_V)
 	
-	# rsync.mk Build cleanup
+	# mosh.mk Build cleanup
 	rm -rf $(BUILD_DIST)/mosh
 
 .PHONY: mosh mosh-package
