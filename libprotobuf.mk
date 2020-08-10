00c27ec1eaf551935829672a8cd791d662a78798
Cameron Katri <katri.cameron@gmail.com>
2020-08-10 00:20:10 -0400

    Finish

--- libprotobuf.mk
+++ libprotobuf.mk
@@ -2,7 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS    += libprotobuf
+SUBPROJECTS         += libprotobuf
 LIBPROTOBUF_VERSION := 3.12.3
 DEB_LIBPROTOBUF_V   ?= $(LIBPROTOBUF_VERSION)
 
@@ -29,19 +29,25 @@ endif
 
 libprotobuf-package: libprotobuf-stage
 	# libprotobuf.mk Package Structure
-	rm -rf $(BUILD_DIST)/libprotobuf
-	mkdir -p $(BUILD_DIST)/libprotobuf
-	
+	rm -rf $(BUILD_DIST)/{libprotobuf,libprotobuf-dev}
+	mkdir -p $(BUILD_DIST)/{libprotobuf/usr,libprotobuf-dev/usr/lib}
+
 	# libprotobuf.mk Prep libprotobuf
-	cp -a $(BUILD_STAGE)/libprotobuf/usr $(BUILD_DIST)/libprotobuf
-	
+	cp -a $(BUILD_STAGE)/libprotobuf/usr/{lib/*.dylib,bin} $(BUILD_DIST)/libprotobuf/usr
+
+	# libprotobuf.mk Prep libprotobuf-dev
+	cp -a $(BUILD_STAGE)/libprotobuf/usr/lib/{pkgconfig,libprotoc.a,libprotoc.la} $(BUILD_DIST)/libprotobuf-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libprotobuf/usr/include $(BUILD_DIST)/libprotobuf-dev/usr
+
 	# libprotobuf.mk Sign
 	$(call SIGN,libprotobuf,general.xml)
+	$(call SIGN,libprotobuf-dev,general.xml)
 	
 	# libprotobuf.mk Make .debs
 	$(call PACK,libprotobuf,DEB_LIBPROTOBUF_V)
+	$(call PACK,libprotobuf-dev,DEB_LIBPROTOBUF_V)
 	
 	# libprotobuf.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libprotobuf
+	rm -rf $(BUILD_DIST)/{libprotobuf,libprotobuf-dev}
 
 .PHONY: libprotobuf libprotobuf-package
