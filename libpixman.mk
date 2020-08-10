e0442a147528c1856ebe7184ea1dea54c9723ae3
Cameron Katri <katri.cameron@gmail.com>
2020-08-10 13:33:08 -0400

    Fix naming of libpixman

--- libpixman.mk
+++ libpixman.mk
@@ -29,27 +29,27 @@ endif
 
 libpixman-package: libpixman-stage
 	# libpixman.mk Package Structure
-	rm -rf $(BUILD_DIST)/libpixman{-dev}
-	mkdir -p $(BUILD_DIST)/libpixman/usr/lib \
-		$(BUILD_DIST)/libpixman-dev/usr/{lib,include/pixman-1}
+	rm -rf $(BUILD_DIST)/libpixman-1-0{-dev}
+	mkdir -p $(BUILD_DIST)/libpixman-1-0/usr/lib \
+		$(BUILD_DIST)/libpixman-1-0-dev/usr/{lib,include/pixman-1}
 	
 	# libpixman.mk Prep libpixman
-	cp -a $(BUILD_STAGE)/libpixman/usr/lib/libpixman-1.0.40.0.dylib $(BUILD_DIST)/libpixman/usr/lib
+	cp -a $(BUILD_STAGE)/libpixman/usr/lib/libpixman-1.0.40.0.dylib $(BUILD_DIST)/libpixman-1-0/usr/lib
 
 	# libpixman.mk Prep libpixman-dev
-	cp -a $(BUILD_STAGE)/libpixman/usr/lib/{pkgconfig,libpixman-1.{a,dylib}} $(BUILD_DIST)/libpixman-dev/usr/lib
-	cp -a $(BUILD_STAGE)/libpixman/usr/include/pixman-1/ $(BUILD_DIST)/libpixman-dev/usr/include/pixman-1
+	cp -a $(BUILD_STAGE)/libpixman/usr/lib/{pkgconfig,libpixman-1.{a,dylib}} $(BUILD_DIST)/libpixman-1-0-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libpixman/usr/include/pixman-1/ $(BUILD_DIST)/libpixman-1-0-dev/usr/include/pixman-1
 	
 	# libpixman.mk Sign
-	$(call SIGN,libpixman,general.xml)
-	$(call SIGN,libpixman-dev,general.xml)
+	$(call SIGN,libpixman-1-0,general.xml)
+	$(call SIGN,libpixman-1-0-dev,general.xml)
 	
 	# libpixman.mk Make .debs
-	$(call PACK,libpixman,DEB_LIBPIXMAN_V)
-	$(call PACK,libpixman-dev,DEB_LIBPIXMAN_V)
+	$(call PACK,libpixman-1-0,DEB_LIBPIXMAN_V)
+	$(call PACK,libpixman-1-0-dev,DEB_LIBPIXMAN_V)
 	
 	# libpixman.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libpixman \
-		$(BUILD_DIST)/libpixman-dev
+	rm -rf $(BUILD_DIST)/libpixman-1-0 \
+		$(BUILD_DIST)/libpixman-1-0-dev
 
 .PHONY: libpixman libpixman-package
