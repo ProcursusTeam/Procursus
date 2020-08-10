ea22f3b64a40920bb06209a61d8522b4e430b15d
Cameron Katri <katri.cameron@gmail.com>
2020-08-10 14:22:38 -0400

    Rename libpixman-1-dev

--- libpixman.mk
+++ libpixman.mk
@@ -31,25 +31,25 @@ libpixman-package: libpixman-stage
 	# libpixman.mk Package Structure
 	rm -rf $(BUILD_DIST)/libpixman-1-0{-dev}
 	mkdir -p $(BUILD_DIST)/libpixman-1-0/usr/lib \
-		$(BUILD_DIST)/libpixman-1-0-dev/usr/{lib,include/pixman-1}
+		$(BUILD_DIST)/libpixman-1-dev/usr/{lib,include/pixman-1}
 	
 	# libpixman.mk Prep libpixman
 	cp -a $(BUILD_STAGE)/libpixman/usr/lib/libpixman-1.0.40.0.dylib $(BUILD_DIST)/libpixman-1-0/usr/lib
 
 	# libpixman.mk Prep libpixman-dev
-	cp -a $(BUILD_STAGE)/libpixman/usr/lib/{pkgconfig,libpixman-1.{a,dylib}} $(BUILD_DIST)/libpixman-1-0-dev/usr/lib
-	cp -a $(BUILD_STAGE)/libpixman/usr/include/pixman-1/ $(BUILD_DIST)/libpixman-1-0-dev/usr/include/pixman-1
+	cp -a $(BUILD_STAGE)/libpixman/usr/lib/{pkgconfig,libpixman-1.{a,dylib}} $(BUILD_DIST)/libpixman-1-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libpixman/usr/include/pixman-1/ $(BUILD_DIST)/libpixman-1-dev/usr/include/pixman-1
 	
 	# libpixman.mk Sign
 	$(call SIGN,libpixman-1-0,general.xml)
-	$(call SIGN,libpixman-1-0-dev,general.xml)
+	$(call SIGN,libpixman-1-dev,general.xml)
 	
 	# libpixman.mk Make .debs
 	$(call PACK,libpixman-1-0,DEB_LIBPIXMAN_V)
-	$(call PACK,libpixman-1-0-dev,DEB_LIBPIXMAN_V)
+	$(call PACK,libpixman-1-dev,DEB_LIBPIXMAN_V)
 	
 	# libpixman.mk Build cleanup
 	rm -rf $(BUILD_DIST)/libpixman-1-0 \
-		$(BUILD_DIST)/libpixman-1-0-dev
+		$(BUILD_DIST)/libpixman-1-dev
 
 .PHONY: libpixman libpixman-package
