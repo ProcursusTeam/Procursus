cc067d7a3b808b1ab45f5d302397efc52430cd49
Cameron Katri <katri.cameron@gmail.com>
2020-08-11 23:16:12 -0400

    Debianize libarchive

--- libarchive.mk
+++ libarchive.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS        += libarchive
 LIBARCHIVE_VERSION := 3.4.3
-DEB_LIBARCHIVE_V   ?= $(LIBARCHIVE_VERSION)
+DEB_LIBARCHIVE_V   ?= $(LIBARCHIVE_VERSION)-1
 
 libarchive-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libarchive/libarchive/releases/download/v$(LIBARCHIVE_VERSION)/libarchive-$(LIBARCHIVE_VERSION).tar.xz
@@ -14,10 +14,15 @@ ifneq ($(wildcard $(BUILD_WORK)/libarchive/.build_complete),)
 libarchive:
 	@echo "Using previously built libarchive."
 else
-libarchive: libarchive-setup lz4 zstd xz
+libarchive: libarchive-setup lz4 zstd xz bzip2 nettle
 	cd $(BUILD_WORK)/libarchive && ./configure \
 		--host=$(GNU_HOST_TRIPLE) \
 		--disable-dependency-tracking \
+		--without-openssl \
+		--with-nettle \
+		--enable-bsdtar=shared \
+		--enable-bsdcpio=shared \
+		--enable-bsdcat=shared \
 		--prefix=/usr
 	+$(MAKE) -C $(BUILD_WORK)/libarchive
 	+$(MAKE) -C $(BUILD_WORK)/libarchive install \
@@ -29,20 +34,34 @@ libarchive: libarchive-setup lz4 zstd xz
 endif
 
 libarchive-package: libarchive-stage
-	# rsync.mk Package Structure
-	rm -rf $(BUILD_DIST)/libarchive
-	mkdir -p $(BUILD_DIST)/libarchive
+	# libarchive.mk Package Structure
+	rm -rf $(BUILD_DIST)/libarchive{13,-tools,-dev}
+	mkdir -p $(BUILD_DIST)/libarchive13/usr/lib
+	mkdir -p $(BUILD_DIST)/libarchive-tools/usr/share/man
+	mkdir -p $(BUILD_DIST)/libarchive-dev/usr/{lib,share/man}
 	
-	# rsync.mk Prep rsync
-	cp -a $(BUILD_STAGE)/libarchive/usr $(BUILD_DIST)/libarchive
+	# libarchive.mk Prep libarchive13
+	cp -a $(BUILD_STAGE)/libarchive/usr/lib/libarchive.*.dylib $(BUILD_DIST)/libarchive13/usr/lib
 	
-	# rsync.mk Sign
-	$(call SIGN,libarchive,general.xml)
+	# libarchive.mk Prep libarchive-tools
+	cp -a $(BUILD_STAGE)/libarchive/usr/bin $(BUILD_DIST)/libarchive-tools/usr
+	cp -a $(BUILD_STAGE)/libarchive/usr/share/man/man1 $(BUILD_DIST)/libarchive-tools/usr/share/man/
 	
-	# rsync.mk Make .debs
-	$(call PACK,libarchive,DEB_LIBARCHIVE_V)
+	# libarchive.mk Prep libarchive-dev
+	cp -a $(BUILD_STAGE)/libarchive/usr/include $(BUILD_DIST)/libarchive-dev/usr
+	cp -a $(BUILD_STAGE)/libarchive/usr/lib/pkgconfig $(BUILD_DIST)/libarchive-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libarchive/usr/share/man/man{3,5} $(BUILD_DIST)/libarchive-dev/usr/share/man
 	
-	# rsync.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libarchive
+	# libarchive.mk Sign
+	$(call SIGN,libarchive13,general.xml)
+	$(call SIGN,libarchive-tools,general.xml)
+	
+	# libarchive.mk Make .debs
+	$(call PACK,libarchive13,DEB_LIBARCHIVE_V)
+	$(call PACK,libarchive-tools,DEB_LIBARCHIVE_V)
+	$(call PACK,libarchive-dev,DEB_LIBARCHIVE_V)
+	
+	# libarchive.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libarchive{13,-dev,-tools}
 
 .PHONY: libarchive libarchive-package
