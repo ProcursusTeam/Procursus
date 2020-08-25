855f91a580ebe24562bea4580ea2536e6a419c47
Michael Woolweaver <m.woolweaver@icloud.com>
2020-08-25 16:45:06 -0500

    add Section to dummy packages
    Section: Dummy_Packages

--- build_tools/make_dummy.sh
+++ build_tools/make_dummy.sh
@@ -2,6 +2,6 @@
 cd "$(dirname $0)"
 mkdir -p ./"${1}"_"${2}"_"${3}"/DEBIAN
 touch ./"${1}"_"${2}"_"${3}"/DEBIAN/control
-echo -e "Package: ${1}\nMaintainer: Hayden Seay\nVersion:${2}\nArchitecture:${3}\nDescription: Dummy package to help migration from other bootstraps. Safe to remove.\n" > ./"${1}"_"${2}"_"${3}"/DEBIAN/control
+echo -e "Package: ${1}\nMaintainer: Hayden Seay\nVersion:${2}\nArchitecture:${3}\nSection: Dummy_Packages\nDescription: Dummy package to help migration from other bootstraps. Safe to remove.\n" > ./"${1}"_"${2}"_"${3}"/DEBIAN/control
 dpkg-deb -b ./"${1}"_"${2}"_"${3}"
 rm -rf ./"${1}"_"${2}"_"${3}"
