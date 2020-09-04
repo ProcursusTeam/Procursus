731f046d42ea3ca97f14809ed08692a2fbb5a03c
Cameron Katri <katri.cameron@gmail.com>
2020-09-03 21:29:58 -0400

    Generate git manpages

--- git.mk
+++ git.mk
@@ -4,7 +4,17 @@ endif
 
 SUBPROJECTS += git
 GIT_VERSION := 2.28.0
-DEB_GIT_V   ?= $(GIT_VERSION)
+DEB_GIT_V   ?= $(GIT_VERSION)-1
+
+GIT_ARGS += uname_S=Darwin \
+						HOST_CPU=$(GNU_HOST_TRIPLE) \
+						DESTDIR=$(BUILD_STAGE)/git \
+						MANDIR=/usr/share/man \
+						NO_DARWIN_PORTS=1 \
+						NO_FINK=1 \
+						NO_APPLE_COMMON_CRYPTO=1 \
+						INSTALL_SYMLINKS=1 \
+						NO_INSTALL_HARDLINKS=1
 
 git-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://mirrors.edge.kernel.org/pub/software/scm/git/git-$(GIT_VERSION).tar.xz
@@ -37,15 +47,12 @@ git: git-setup openssl curl pcre2 gettext libidn2
 			execve(SHELL_PATH, (char *const *) out.argv, \
  			       (char *const *) childenv); \
 		}' $(BUILD_WORK)/git/run-command.c
+	+$(MAKE) -C $(BUILD_WORK)/git all \
+		$(GIT_ARGS)
+	+$(MAKE) -C $(BUILD_WORK)/git/Documentation man install \
+		$(GIT_ARGS)
 	+$(MAKE) -C $(BUILD_WORK)/git install \
-		uname_S=Darwin \
-		HOST_CPU=$(GNU_HOST_TRIPLE) \
-		DESTDIR=$(BUILD_STAGE)/git \
-		NO_DARWIN_PORTS=1 \
-		NO_FINK=1 \
-		NO_APPLE_COMMON_CRYPTO=1 \
-		INSTALL_SYMLINKS=1 \
-		NO_INSTALL_HARDLINKS=1
+		$(GIT_ARGS)
 	touch $(BUILD_WORK)/git/.build_complete
 endif
 
