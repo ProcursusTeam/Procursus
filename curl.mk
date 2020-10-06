62b37b5ff30401d7ea204fae8ad9e57167f58800
Cameron Katri <katri.cameron@gmail.com>
2020-10-06 01:04:04 -0400

    Debianize curl

--- curl.mk
+++ curl.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS  += curl
 CURL_VERSION := 7.71.1
-DEB_CURL_V   ?= $(CURL_VERSION)
+DEB_CURL_V   ?= $(CURL_VERSION)-1
 
 curl-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://curl.haxx.se/download/curl-$(CURL_VERSION).tar.xz{,.asc}
@@ -34,19 +34,38 @@ endif
 
 curl-package: curl-stage
 	# curl.mk Package Structure
-	rm -rf $(BUILD_DIST)/curl
-	mkdir -p $(BUILD_DIST)/curl
+	rm -rf $(BUILD_DIST)/curl \
+		$(BUILD_DIST)/libcurl4{,-openssl-dev}
+	mkdir -p $(BUILD_DIST)/curl/usr/{bin,share/man/man1} \
+		$(BUILD_DIST)/libcurl4-openssl-dev/usr/{bin,lib,share/man/man1} \
+		$(BUILD_DIST)/libcurl4/usr/lib
 	
 	# curl.mk Prep curl
-	cp -a $(BUILD_STAGE)/curl/usr $(BUILD_DIST)/curl
+	cp -a $(BUILD_STAGE)/curl/usr/bin/curl $(BUILD_DIST)/curl/usr/bin
+	cp -a $(BUILD_STAGE)/curl/usr/share/man/man1/curl.1 $(BUILD_DIST)/curl/usr/share/man/man1
+	
+	# curl.mk Prep libcurl4
+	cp -a $(BUILD_STAGE)/curl/usr/lib/libcurl.4.dylib $(BUILD_DIST)/libcurl4/usr/lib
+	
+	# curl.mk Prep libcurl4-openssl-dev
+	cp -a $(BUILD_STAGE)/curl/usr/lib/{pkgconfig,libcurl.{dylib,a}} $(BUILD_DIST)/libcurl4-openssl-dev/usr/lib
+	cp -a $(BUILD_STAGE)/curl/usr/share/man/man1/curl-config.1 $(BUILD_DIST)/libcurl4-openssl-dev/usr/share/man/man1
+	cp -a $(BUILD_STAGE)/curl/usr/share/man/man3 $(BUILD_DIST)/libcurl4-openssl-dev/usr/share/man
+	cp -a $(BUILD_STAGE)/curl/usr/bin/curl-config $(BUILD_DIST)/libcurl4-openssl-dev/usr/bin
+	cp -a $(BUILD_STAGE)/curl/usr/include $(BUILD_DIST)/libcurl4-openssl-dev/usr
 	
 	# curl.mk Sign
 	$(call SIGN,curl,general.xml)
+	$(call SIGN,libcurl4,general.xml)
+	$(call SIGN,libcurl4-openssl-dev,general.xml)
 	
 	# curl.mk Make .debs
 	$(call PACK,curl,DEB_CURL_V)
+	$(call PACK,libcurl4,DEB_CURL_V)
+	$(call PACK,libcurl4-openssl-dev,DEB_CURL_V)
 	
 	# curl.mk Build cleanup
-	rm -rf $(BUILD_DIST)/curl
+	#rm -rf $(BUILD_DIST)/curl \
+	#	$(BUILD_DIST)/libcurl4{,-openssl-dev}
 
 .PHONY: curl curl-package
