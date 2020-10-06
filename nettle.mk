9c115a138e13d5be2e97c77707f57e92fd2df08c
Cameron Katri <katri.cameron@gmail.com>
2020-10-06 14:27:35 -0400

    Debianize nettle

--- nettle.mk
+++ nettle.mk
@@ -31,19 +31,43 @@ endif
 
 nettle-package: nettle-stage
 	# nettle.mk Package Structure
-	rm -rf $(BUILD_DIST)/nettle
-	mkdir -p $(BUILD_DIST)/nettle
+	rm -rf $(BUILD_DIST)/nettle-bin \
+		$(BUILD_DIST)/nettle-dev \
+		$(BUILD_DIST)/libnettle8 \
+		$(BUILD_DIST)/libhogweed6
+	mkdir -p $(BUILD_DIST)/nettle-bin/usr \
+		$(BUILD_DIST)/nettle-dev/usr/lib \
+		$(BUILD_DIST)/libnettle8/usr/lib \
+		$(BUILD_DIST)/libhogweed6/usr/lib
 	
-	# nettle.mk Prep nettle
-	cp -a $(BUILD_STAGE)/nettle/usr $(BUILD_DIST)/nettle
+	# nettle.mk Prep nettle-bin
+	cp -a $(BUILD_STAGE)/nettle/usr/bin $(BUILD_DIST)/nettle-bin/usr
+	
+	# nettle.mk Prep libnettle8
+	cp -a $(BUILD_STAGE)/nettle/usr/lib/libnettle.8{,.0}.dylib $(BUILD_DIST)/libnettle8/usr/lib
+	
+	# nettle.mk Prep libhogweed6
+	cp -a $(BUILD_STAGE)/nettle/usr/lib/libhogweed.6{,.0}.dylib $(BUILD_DIST)/libhogweed6/usr/lib
+	
+	# nettle.mk Prep nettle-dev
+	cp -a $(BUILD_STAGE)/nettle/usr/lib/{pkgconfig,lib{nettle,hogweed}.{dylib,a}} $(BUILD_DIST)/nettle-dev/usr/lib
+	cp -a $(BUILD_STAGE)/nettle/usr/include $(BUILD_DIST)/nettle-dev/usr
 	
 	# nettle.mk Sign
-	$(call SIGN,nettle,general.xml)
+	$(call SIGN,nettle-bin,general.xml)
+	$(call SIGN,libnettle8,general.xml)
+	$(call SIGN,libhogweed6,general.xml)
 	
 	# nettle.mk Make .debs
-	$(call PACK,nettle,DEB_NETTLE_V)
+	$(call PACK,nettle-bin,DEB_NETTLE_V)
+	$(call PACK,nettle-dev,DEB_NETTLE_V)
+	$(call PACK,libnettle8,DEB_NETTLE_V)
+	$(call PACK,libhogweed6,DEB_NETTLE_V)
 	
 	# nettle.mk Build cleanup
-	rm -rf $(BUILD_DIST)/nettle
+	rm -rf $(BUILD_DIST)/nettle-bin \
+		$(BUILD_DIST)/nettle-dev \
+		$(BUILD_DIST)/libnettle8 \
+		$(BUILD_DIST)/libhogweed6
 
 .PHONY: nettle nettle-package
