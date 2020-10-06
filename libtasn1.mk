5d0fa5eeb6e9d1ed25d09fbdc15397d25c8391ad
Hayden <me@diatr.us>
2020-10-06 14:25:59 -0400

    Debianize libtasn1

--- libtasn1.mk
+++ libtasn1.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS      += libtasn1
 LIBTASN1_VERSION := 4.16.0
-DEB_LIBTASN1_V   ?= $(LIBTASN1_VERSION)
+DEB_LIBTASN1_V   ?= $(LIBTASN1_VERSION)-1
 
 libtasn1-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://ftpmirror.gnu.org/libtasn1/libtasn1-$(LIBTASN1_VERSION).tar.gz{,.sig}
@@ -29,19 +29,33 @@ endif
 
 libtasn1-package: libtasn1-stage
 	# libtasn1.mk Package Structure
-	rm -rf $(BUILD_DIST)/libtasn1
-	mkdir -p $(BUILD_DIST)/libtasn1
+	rm -rf $(BUILD_DIST)/libtasn1-{6{,-dev},bin}
+	mkdir -p $(BUILD_DIST)/libtasn1-6/usr/lib \
+		$(BUILD_DIST)/libtasn1-6-dev/usr/{lib,share/man} \
+		$(BUILD_DIST)/libtasn1-bin/usr/share/man
 	
-	# libtasn1.mk Prep libtasn1
-	cp -a $(BUILD_STAGE)/libtasn1/usr $(BUILD_DIST)/libtasn1
+	# libtasn1.mk Prep libtasn1-6
+	cp -a $(BUILD_STAGE)/libtasn1/usr/lib/libtasn1.6.dylib $(BUILD_DIST)/libtasn1-6/usr/lib
+
+	# libtasn1.mk Prep libtasn1-dev
+	cp -a $(BUILD_STAGE)/libtasn1/usr/lib/!(libtasn1.6.dylib) $(BUILD_DIST)/libtasn1-6-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libtasn1/usr/share/man/man3 $(BUILD_DIST)/libtasn1-6-dev/usr/share/man
+	cp -a $(BUILD_STAGE)/libtasn1/usr/include $(BUILD_DIST)/libtasn1-6-dev/usr
+
+	# libtasn1.mk Prep libtasn1-bin
+	cp -a $(BUILD_STAGE)/libtasn1/usr/share/man/man1 $(BUILD_DIST)/libtasn1-bin/usr/share/man
+	cp -a $(BUILD_STAGE)/libtasn1/usr/bin $(BUILD_DIST)/libtasn1-bin/usr
 	
 	# libtasn1.mk Sign
-	$(call SIGN,libtasn1,general.xml)
+	$(call SIGN,libtasn1-6,general.xml)
+	$(call SIGN,libtasn1-bin,general.xml)
 	
 	# libtasn1.mk Make .debs
-	$(call PACK,libtasn1,DEB_LIBTASN1_V)
+	$(call PACK,libtasn1-6,DEB_LIBTASN1_V)
+	$(call PACK,libtasn1-bin,DEB_LIBTASN1_V)
+	$(call PACK,libtasn1-6-dev,DEB_LIBTASN1_V)
 	
 	# libtasn1.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libtasn1
+	rm -rf $(BUILD_DIST)/libtasn1-{6{,-dev},bin}
 
 .PHONY: libtasn1 libtasn1-package
