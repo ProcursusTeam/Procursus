d27ae5a18d6008146a752eaa002d863c50104c3e
Cameron Katri <katri.cameron@gmail.com>
2020-10-05 22:41:29 -0400

    Debianize libidn2

--- libidn2.mk
+++ libidn2.mk
@@ -29,19 +29,35 @@ endif
 
 libidn2-package: libidn2-stage
 	# libidn2.mk Package Structure
-	rm -rf $(BUILD_DIST)/libidn2
-	mkdir -p $(BUILD_DIST)/libidn2
+	rm -rf $(BUILD_DIST)/libidn2{-0,-dev} \
+		$(BUILD_DIST)/idn2
+	mkdir -p $(BUILD_DIST)/libidn2-0/usr/{share,lib} \
+		$(BUILD_DIST)/libidn2-dev/usr/{share/man,lib} \
+		$(BUILD_DIST)/idn2/usr/share/man
 	
-	# libidn2.mk Prep libidn2
-	cp -a $(BUILD_STAGE)/libidn2/usr $(BUILD_DIST)/libidn2
+	# libidn2.mk Prep idn2
+	cp -a $(BUILD_STAGE)/libidn2/usr/bin $(BUILD_DIST)/idn2/usr
+	cp -a $(BUILD_STAGE)/libidn2/usr/share/man/man1 $(BUILD_DIST)/idn2/usr/share/man
+	
+	# libidn2.mk Prep libidn2-0
+	cp -a $(BUILD_STAGE)/libidn2/usr/lib/libidn2.0.dylib $(BUILD_DIST)/libidn2-0/usr/lib
+	cp -a $(BUILD_STAGE)/libidn2/usr/share/locale $(BUILD_DIST)/libidn2-0/usr/share
+	
+	# libidn2.mk Prep libidn2-dev
+	cp -a $(BUILD_STAGE)/libidn2/usr/lib/{libidn2.{dylib,a},pkgconfig} $(BUILD_DIST)/libidn2-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libidn2/usr/share/man/man3 $(BUILD_DIST)/libidn2-dev/usr/share/man
 	
 	# libidn2.mk Sign
-	$(call SIGN,libidn2,general.xml)
+	$(call SIGN,idn2,general.xml)
+	$(call SIGN,libidn2-0,general.xml)
 	
 	# libidn2.mk Make .debs
-	$(call PACK,libidn2,DEB_IDN2_V)
+	#$(call PACK,idn2,DEB_IDN2_V)
+	#$(call PACK,libidn2-0,DEB_IDN2_V)
+	#$(call PACK,libidn2-dev,DEB_IDN2_V)
 	
 	# libidn2.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libidn2
+	#rm -rf $(BUILD_DIST)/libidn2-{0,dev} \
+	#	$(BUILD_DIST)/idn2
 
 .PHONY: libidn2 libidn2-package
