4e121a08f49dcc6957394477da1f22ce44c0f51f
Hayden <me@diatr.us>
2020-10-07 20:21:53 -0400

    libpng16-16

--- libpng16.mk
+++ libpng16.mk
@@ -28,19 +28,33 @@ endif
 
 libpng16-package: libpng16-stage
 	# libpng16.mk Package Structure
-	rm -rf $(BUILD_DIST)/libpng16
-	mkdir -p $(BUILD_DIST)/libpng16
+	rm -rf $(BUILD_DIST)/libpng16-{16,dev,tools}
+	mkdir -p $(BUILD_DIST)/libpng16-16/usr/lib \
+		$(BUILD_DIST)/libpng16-dev/usr/{bin,lib} \
+		$(BUILD_DIST)/libpng16-tools/usr/bin
 	
-	# libpng16.mk Prep libpng16
-	cp -a $(BUILD_STAGE)/libpng16/usr $(BUILD_DIST)/libpng16
+	# libpng16.mk Prep libpng16-16
+	cp -a $(BUILD_STAGE)/libpng16/usr/lib/libpng16.16.dylib $(BUILD_DIST)/libpng16-16/usr/lib
 	
+	# libpng16.mk Prep libpng16-dev
+	cp -a $(BUILD_STAGE)/libpng16/usr/bin/*-config $(BUILD_DIST)/libpng16-dev/usr/bin
+	cp -a $(BUILD_STAGE)/libpng16/usr/lib/!(libpng16.16.dylib) $(BUILD_DIST)/libpng16-dev/usr/lib
+	cp -a $(BUILD_STAGE)/libpng16/usr/include $(BUILD_DIST)/libpng16-dev/usr
+	cp -a $(BUILD_STAGE)/libpng16/usr/share $(BUILD_DIST)/libpng16-dev/usr
+
+	# libpng16.mk Prep libpng16-tools
+	cp -a $(BUILD_STAGE)/libpng16/usr/bin/!(*-config) $(BUILD_DIST)/libpng16-tools/usr/bin
+
 	# libpng16.mk Sign
-	$(call SIGN,libpng16,general.xml)
+	$(call SIGN,libpng16-16,general.xml)
+	$(call SIGN,libpng16-tools,general.xml)
 	
 	# libpng16.mk Make .debs
-	$(call PACK,libpng16,DEB_LIBPNG16_V)
+	$(call PACK,libpng16-16,DEB_LIBPNG16_V)
+	$(call PACK,libpng16-dev,DEB_LIBPNG16_V)
+	$(call PACK,libpng16-tools,DEB_LIBPNG16_V)
 	
 	# libpng16.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libpng16
+	rm -rf $(BUILD_DIST)/libpng16-{16,dev,tools}
 
 .PHONY: libpng16 libpng16-package
