3ac3d311e5c6555e9d385e55ef35d9d873e8cb96
quiprr <quiprr@ametrine.dev>
2020-10-13 23:52:28 +0000

    Ok I did it really bad the first time so better speedtest

--- speedtest.mk
+++ speedtest.mk
@@ -2,31 +2,38 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-STRAPPROJECTS      += speedtest
-SPEEDTEST_VERSION  := 2.1.2
-DEB_SPEEDTEST_V    ?= $(SPEEDTEST_VERSION)
+SUBPROJECTS       += speedtest
+SPEEDTEST_VERSION := 2.1.2
+DEB_SPEEDTEST_V   ?= $(SPEEDTEST_VERSION)
 
 speedtest-setup: setup
-	wget -O speedtest -P $(BUILD_SOURCE) https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+	wget -q -nc -P $(BUILD_SOURCE) https://github.com/sivel/speedtest-cli/archive/v$(SPEEDTEST_VERSION).tar.gz
+	$(call EXTRACT_TAR,v$(SPEEDTEST_VERSION).tar.gz,speedtest-$(SPEEDTEST_VERSION),speedtest)
 
 ifneq ($(wildcard $(BUILD_WORK)/speedtest/.build_complete),)
 speedtest:
 	@echo "Using previously built speedtest."
 else
-speedtest: speedtest-setup pcre
+speedtest: speedtest-setup 
+	mkdir -p $(BUILD_STAGE)/speedtest/usr/bin
+	cp $(BUILD_WORK)/speedtest/speedtest.py $(BUILD_STAGE)/speedtest/usr/bin/speedtest
 	touch $(BUILD_WORK)/speedtest/.build_complete
 endif
 
 speedtest-package: speedtest-stage
+	# speedtest.mk Package Structure
 	rm -rf $(BUILD_DIST)/speedtest
 	mkdir -p $(BUILD_DIST)/speedtest
 	
-	cp -a $(BUILD_STAGE)/speedtest/usr $(BUILD_DIST)/speedtest
+	# speedtest.mk Prep speedtest
+	cp -a $(BUILD_STAGE)/speedtest $(BUILD_DIST)
+
+	# speedtest.mk Fix permissions
 	
-	$(call SIGN,speedtest,general.xml)
-	
+	# speedtest.mk Make .debs
 	$(call PACK,speedtest,DEB_SPEEDTEST_V)
 	
+	# speedtest.mk Build cleanup
 	rm -rf $(BUILD_DIST)/speedtest
 
 .PHONY: speedtest speedtest-package
