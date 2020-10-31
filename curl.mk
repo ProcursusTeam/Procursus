101d4c7d7a8449b15479f4a6cef900894d36ebea
Cameron Katri <katri.cameron@gmail.com>
2020-10-31 15:11:47 -0400

    Add curl dep as requested (#183)
    * Add libbrotli1 and libzstd1 dep to curl
    
    * Update libcurl4.control
    
    Co-authored-by: Hayden <me@diatr.us>

--- curl.mk
+++ curl.mk
@@ -15,7 +15,7 @@ ifneq ($(wildcard $(BUILD_WORK)/curl/.build_complete),)
 curl:
 	@echo "Using previously built curl."
 else
-curl: curl-setup gettext openssl libssh2 nghttp2 libidn2
+curl: curl-setup gettext openssl libssh2 nghttp2 libidn2 brotli zstd
 	cd $(BUILD_WORK)/curl && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr \
