2f4204e7e48cbde08c3f6e606ce16662eddaabd1
demhademha <ahmedtexas05@gmail.com>
2020-11-25 20:42:08 +0000

    fix libzip

--- lzip.mk
+++ lzip.mk
@@ -17,6 +17,9 @@ lzip:
 else
 lzip: lzip-setup
 	cd $(BUILD_WORK)/lzip && ./configure \
+	CXX=$(CXX) \        CXXFLAGS="$(CXXFLAGS)" \
+        CPPFLAGS="$(CPPFLAGS)" \
+        LDFLAGS="$(LDFLAGS)" \
 		--prefix=/usr
 	+$(MAKE) -C $(BUILD_WORK)/lzip
 	+$(MAKE) -C $(BUILD_WORK)/lzip install \
