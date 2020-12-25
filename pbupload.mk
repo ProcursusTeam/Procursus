a9397bc8d60ea9fa03327d0bd6a8ec9b7aeb9a5f
quiprr <quiprr@ametrine.dev>
2020-12-25 15:07:23 -0800

    add pbupload

---
+++ pbupload.mk
@@ -0,0 +1,47 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS           += pbupload
+PBUPLOAD_VERSION      := 1.0.0
+DEB_PBUPLOAD_V        ?= $(PBUPLOAD_VERSION)
+PBUPLOAD_LIBS         := -framework Foundation -framework UIKit
+
+pbupload-setup: setup
+	-[ ! -f "$(BUILD_SOURCE)/pbupload-$(PBUPLOAD_VERSION).tar.gz" ] && \
+		wget -q -nc -O$(BUILD_SOURCE)/pbupload-$(PBUPLOAD_VERSION).tar.gz \
+			https://github.com/quiprr/pbupload/archive/v$(PBUPLOAD_VERSION).tar.gz
+	$(call EXTRACT_TAR,pbupload-$(PBUPLOAD_VERSION).tar.gz,pbupload-$(PBUPLOAD_VERSION),pbupload)
+	mkdir -p $(BUILD_STAGE)/pbupload/usr/bin
+
+ifneq ($(wildcard $(BUILD_WORK)/pbupload/.build_complete),)
+pbupload:
+	@echo "Using previously built pbupload."
+else
+pbupload: pbupload-setup 
+	$(CC) -arch arm64e -fobjc-arc $(PBUPLOAD_LIBS) $(CFLAGS) \
+		-o $(BUILD_WORK)/pbupload/pbupload \
+		$(BUILD_WORK)/pbupload/Sources/pbupload.m
+
+	cp -a $(BUILD_WORK)/pbupload/pbupload $(BUILD_STAGE)/pbupload/usr/bin
+	touch $(BUILD_WORK)/pbupload/.build_complete
+endif
+
+pbupload-package: pbupload-stage
+	# pbupload.mk Package Structure
+	rm -rf $(BUILD_DIST)/pbupload
+	mkdir -p $(BUILD_DIST)/pbupload/usr/bin
+
+	# pbupload.mk Prep pbupload
+	cp -a $(BUILD_STAGE)/pbupload/usr/bin/pbupload $(BUILD_DIST)/pbupload/usr/bin
+
+	# pbupload.mk Permissions
+	chmod u+s $(BUILD_DIST)/pbupload/usr/bin/pbupload
+
+	# pbupload.mk Make .debs
+	$(call PACK,pbupload,DEB_PBUPLOAD_V)
+
+	# pbupload.mk Build cleanup
+	rm -rf $(BUILD_DIST)/pbupload
+
+.PHONY: pbupload pbupload-package
