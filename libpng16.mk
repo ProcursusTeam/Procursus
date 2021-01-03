d1b1269693476b46b2dbc2d91d4b4e9e9dccd494
Davis Deaton <deaton.dg@gmail.com>
2021-01-02 21:25:08 -0500

    Fix libpng.pc file to use Apple's zlib

--- libpng16.mk
+++ libpng16.mk
@@ -9,6 +9,8 @@ DEB_LIBPNG16_V   ?= $(LIBPNG16_VERSION)
 libpng16-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://sourceforge.net/projects/libpng/files/libpng16/$(LIBPNG16_VERSION)/libpng-$(LIBPNG16_VERSION).tar.xz
 	$(call EXTRACT_TAR,libpng-$(LIBPNG16_VERSION).tar.xz,libpng-$(LIBPNG16_VERSION),libpng16)
+	# Fix the .pc file to use Apple's zlib
+	$(SED) -i 's/Requires: zlib/Requires: /;s/\(Libs:.*\)/\1 -lz/' $(BUILD_WORK)/libpng16/libpng.pc.in
 
 ifneq ($(wildcard $(BUILD_WORK)/libpng16/.build_complete),)
 libpng16:
