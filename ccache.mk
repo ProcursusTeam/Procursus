981519a4e01634136706a03fd25ae03b6b133968
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-01-18 02:58:17 +0000

    Update ccache.mk

--- ccache.mk
+++ ccache.mk
@@ -15,12 +15,26 @@ ccache:
 	@echo "Using previously built ccache."
 else
 ccache: ccache-setup
-	cd $(BUILD_WORK)/ccache && ./configure -C \
-		--host=$(GNU_HOST_TRIPLE) \
-		--prefix=/usr \
-		--mandir=/usr/share/man \
-		--infodir=/usr/share/info \
-		--sysconfdir=/etc
+	cd $(BUILD_WORK)/ccache && cmake . -j$(shell $(GET_LOGICAL_CORES)) \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_SYSTEM_NAME=Darwin \
+		-DCMAKE_CROSSCOMPILING=true \
+		-DSTATE_DIR=/usr/bin \
+		-DHAVE_C_SSE2=0 \
+		-DHAVE_C_SSE41=0 \
+		-DHAVE_AVX2=0 \
+		-DHAVE_C_AVX2=0 \
+		-DHAVE_C_AVX512=0 \
+		-DCMAKE_INSTALL_NAME_TOOL=$(I_N_T) \
+		-DCMAKE_INSTALL_PREFIX=/ \
+		-DCMAKE_INSTALL_NAME_DIR=/usr/bin \
+		-DCMAKE_INSTALL_RPATH=/usr \
+		-DCMAKE_OSX_SYSROOT="$(TARGET_SYSROOT)" \
+		-DCMAKE_C_FLAGS="$(CFLAGS)" \
+		-DCMAKE_CXX_FLAGS="$(CXXFLAGS)" \
+		-DCOMMON_ARCH=$(DEB_ARCH) \
+		-DCMAKE_FIND_ROOT_PATH=$(BUILD_BASE) \
+		.
 	+$(MAKE) -C $(BUILD_WORK)/ccache
 	+$(MAKE) -C $(BUILD_WORK)/ccache install \
 		DESTDIR="$(BUILD_STAGE)/ccache"
