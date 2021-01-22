f8240388d6e3d8e822c937ada4c9f1fef8c1de10
Rolex <rolex@Rolexs-MacBook-Pro.local>
2021-01-22 14:19:21 -0800

    Update grep to version 3.6

--- grep.mk
+++ grep.mk
@@ -3,8 +3,8 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS += grep
-GREP_VERSION  := 3.5
-DEB_GREP_V    ?= $(GREP_VERSION)-1
+GREP_VERSION  := 3.6
+DEB_GREP_V    ?= $(GREP_VERSION)
 
 grep-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://ftpmirror.gnu.org/grep/grep-$(GREP_VERSION).tar.xz{,.sig}
