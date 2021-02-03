a1bbd5c8cd9a9b5b9bc06a21c053956e4af20a68
Hayden <me@diatr.us>
2021-02-03 11:38:07 -0500

    Update libxmu.mk

--- libxmu.mk
+++ libxmu.mk
@@ -57,6 +57,6 @@ libxmu-package: libxmu-stage
 	$(call PACK,libxmu-dev,DEB_LIBXMU_V)
 	
 	# libxmu.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libxmu{6,-dev} $(BUILD_DIST)/libxmu
+	rm -rf $(BUILD_DIST)/libxmu{6,-dev} $(BUILD_DIST)/libxmuu1
 
 .PHONY: libxmu libxmu-package
