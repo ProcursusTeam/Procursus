9dc748157fef0c28ffca605e11ad4748741fe85e
Rolex <rolex@Rolexs-MacBook-Pro.local>
2021-02-08 11:32:17 -0800

    Update autoconf to version 2.71

--- autoconf.mk
+++ autoconf.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS       += autoconf
-AUTOCONF_VERSION  := 2.70
+AUTOCONF_VERSION  := 2.71
 DEB_AUTOCONF_V    ?= $(AUTOCONF_VERSION)
 
 autoconf-setup: setup
