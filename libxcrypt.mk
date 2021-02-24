6eacd4cf2873809c00caba3a157881b533d02522
Hayden <me@diatr.us>
2021-02-23 22:10:59 -0500

    libplist and ldid M1

--- libxcrypt.mk
+++ libxcrypt.mk
@@ -3,10 +3,8 @@ $(error Use the main Makefile)
 endif
 
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
+
 STRAPPROJECTS     += libxcrypt
-else # ($(MEMO_TARGET),darwin-\*)
-SUBPROJECTS       += libxcrypt
-endif # ($(MEMO_TARGET),darwin-\*)
 LIBXCRYPT_VERSION := 4.4.17
 DEB_LIBXCRYPT_V   ?= $(LIBXCRYPT_VERSION)
 
@@ -53,3 +51,5 @@ libxcrypt-package: libxcrypt-stage
 	rm -rf $(BUILD_DIST)/libcrypt{2,-dev}
 
 .PHONY: libxcrypt libxcrypt-package
+
+endif # ($(MEMO_TARGET),darwin-\*)
