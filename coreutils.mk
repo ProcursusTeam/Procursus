f87f515f39761ab962a57e93c826c2e3ffe6e1f6
Hayden <me@diatr.us>
2021-02-23 22:33:34 -0500

    Fix coreutils depends for M1

--- coreutils.mk
+++ coreutils.mk
@@ -2,11 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 STRAPPROJECTS     += coreutils
-else # ($(MEMO_TARGET),darwin-\*)
-SUBPROJECTS       += coreutils
-endif # ($(MEMO_TARGET),darwin-\*)
 COREUTILS_VERSION := 8.32
 DEB_COREUTILS_V   ?= $(COREUTILS_VERSION)-7
 
