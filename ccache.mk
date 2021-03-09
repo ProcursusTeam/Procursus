3aba8338aceab9b47c1a1338acec4d612d69439e
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-03-09 08:39:47 -0800

    Darwin based Ccache for MacOS

--- ccache.mk
+++ ccache.mk
@@ -6,6 +6,12 @@ SUBPROJECTS  	 += ccache
 CCACHE_VERSION := 4.2
 DEB_CCACHE_V   ?= $(CCACHE_VERSION)
 
+ifeq (,$(findstring darwin,$(amd64)))
+CCACHE_CMAKE_ARGS := -DHAVE_NEON=FALSE
+else
+CCACHE_CMAKE_ARGS := -DHAVE_NEON=TRUE
+endif
+
 ccache-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/ccache/ccache/releases/download/v$(CCACHE_VERSION)/ccache-$(CCACHE_VERSION).tar.xz
 	$(call EXTRACT_TAR,ccache-$(CCACHE_VERSION).tar.xz,ccache-$(CCACHE_VERSION),ccache)
@@ -23,11 +29,10 @@ ccache: ccache-setup
 		-DHAVE_ASM_SSE2=FALSE \
 		-DHAVE_ASM_SSE41=FALSE \
 		-DHAVE_ASM_AVX512=FALSE \
-		-DHAVE_NEON=TRUE \
 		-DCMAKE_INSTALL_NAME_TOOL=$(I_N_T) \
-		-DCMAKE_INSTALL_PREFIX=/ \
-		-DCMAKE_INSTALL_NAME_DIR=/usr/bin \
-		-DCMAKE_INSTALL_RPATH=/usr \
+		-DCMAKE_INSTALL_PREFIX=/$(MEMO_PREFIX)/ \
+		-DCMAKE_INSTALL_NAME_DIR=/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin \
+		-DCMAKE_INSTALL_RPATH=/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX) \
 		-DCMAKE_OSX_SYSROOT="$(TARGET_SYSROOT)" \
 		-DCMAKE_C_FLAGS="$(CFLAGS)" \
 		-DCMAKE_CXX_FLAGS="$(CXXFLAGS)" \
@@ -42,10 +47,10 @@ endif
 ccache-package: ccache-stage
 	# ccache.mk Package Structure
 	rm -rf $(BUILD_DIST)/ccache
-	mkdir -p $(BUILD_DIST)/ccache
+	mkdir -p $(BUILD_DIST)/ccache/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/{bin,share/man/man1}
 
 	# ccache.mk Prep ccache
-	cp -a $(BUILD_STAGE)/ccache/usr $(BUILD_DIST)/ccache
+	cp -a $(BUILD_STAGE)/ccache/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/{bin,share} $(BUILD_DIST)/ccache/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)
 
 	# ccache.mk Sign
 	$(call SIGN,ccache,general.xml)
