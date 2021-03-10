a8a3d25194d8b6fb589d60b37a1f838b35d4b02e
Brennan LeBlanc <me@brennanleblanc.ca>
2021-03-10 06:40:07 -0400

    Update prefixes

--- lzip.mk
+++ lzip.mk
@@ -17,7 +17,7 @@ lzip:
 else
 lzip: lzip-setup
 	cd $(BUILD_WORK)/lzip && ./configure \
-		--prefix=/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX) \
+		--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
         CXX=$(CXX) \
         CXXFLAGS="$(CXXFLAGS)" \
         CPPFLAGS="$(CPPFLAGS)" \
