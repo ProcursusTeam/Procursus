6cdd6ac240f01e4f328b48d89c8a194c98e89e49
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-03-12 09:42:36 -0800

    Made prefixes a little more readable

--- libboost.mk
+++ libboost.mk
@@ -22,7 +22,7 @@ libboost: libboost-setup xz zstd
 		--without-icu
 	echo 'using clang-darwin : arm : $(CXX) : <compileflags>"$(CPPFLAGS)" <cflags>"$(CFLAGS)" <cxxflags>"$(CXXFLAGS)" <linkflags>"$(LDFLAGS)" ;' > $(BUILD_WORK)/libboost/tools/build/src/user-config.jam
 	cd $(BUILD_WORK)/libboost && ./b2 \
-		--prefix=$(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+		--prefix=$(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
 		--without-python \
 		threading=multi \
 		variant=release \
@@ -36,7 +36,7 @@ libboost: libboost-setup xz zstd
 		abi=aapcs \
 		install
 	# F u boost!
-	for lib in $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_*.dylib $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_*.dylib; do \
+	for lib in $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_*.dylib $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_*.dylib; do \
 		$(I_N_T) -id $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/$$(basename $$lib .dylib).$(LIBBOOST_VERSION).dylib $$lib; \
 		mv $$lib $$(dirname $$lib)/$$(basename $$lib .dylib).$(LIBBOOST_VERSION).dylib; \
 		ln -s $$(basename $$lib .dylib).$(LIBBOOST_VERSION).dylib $$lib; \
@@ -47,39 +47,39 @@ endif
 libboost-package: libboost-stage
 	# libboost.mk Package Structure
 	rm -rf $(BUILD_DIST)/libboost*/
-	mkdir -p $(BUILD_DIST)/libboost-all-dev$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
-		$(BUILD_DIST)/libboost-{atomic,chrono,container,context,contract,coroutine,date-time,filesystem,graph,iostreams,locale,log,math,program-options,random,regex,serialization,stacktrace,system,test,thread,timer,type-erasure,wave}$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+	mkdir -p $(BUILD_DIST)/libboost-all-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/libboost-{atomic,chrono,container,context,contract,coroutine,date-time,filesystem,graph,iostreams,locale,log,math,program-options,random,regex,serialization,stacktrace,system,test,thread,timer,type-erasure,wave}$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
 		$(BUILD_DIST)/libboost$(LIBBOOST_VERSION)-all
 	
 	# libboost.mk Prep libboost-all-dev
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libboost-all-dev$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(*$(LIBBOOST_VERSION)*) $(BUILD_DIST)/libboost-all-dev$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libboost-all-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(*$(LIBBOOST_VERSION)*) $(BUILD_DIST)/libboost-all-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# libboost.mk Prep libboost-*$(LIBBOOST_VERSION)
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_atomic.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-atomic$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_chrono.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-chrono$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_container.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-container$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_context.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-context$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_contract.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-contract$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_coroutine.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-coroutine$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_date_time.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-date-time$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_filesystem.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-filesystem$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_graph.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-graph$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_iostreams.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-iostreams$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_locale.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-locale$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_log{,_setup}.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-log$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_math_*.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-math$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_program_options.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-program-options$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_random.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-random$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_regex.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-regex$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_{,w}serialization.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-serialization$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_stacktrace*.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-stacktrace$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_system.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-system$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_{prg_exec_monitor,unit_test_framework}.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-test$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_thread.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-thread$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_timer.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-timer$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_type_erasure.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-type-erasure$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libboost$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_wave.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-wave$(LIBBOOST_VERSION)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_atomic.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-atomic$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_chrono.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-chrono$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_container.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-container$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_context.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-context$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_contract.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-contract$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_coroutine.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-coroutine$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_date_time.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-date-time$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_filesystem.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-filesystem$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_graph.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-graph$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_iostreams.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-iostreams$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_locale.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-locale$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_log{,_setup}.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-log$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_math_*.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-math$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_program_options.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-program-options$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_random.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-random$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_regex.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-regex$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_{,w}serialization.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-serialization$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_stacktrace*.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-stacktrace$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_system.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-system$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_{prg_exec_monitor,unit_test_framework}.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-test$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_thread.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-thread$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_timer.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-timer$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_type_erasure.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-type-erasure$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libboost/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libboost_wave.$(LIBBOOST_VERSION).dylib $(BUILD_DIST)/libboost-wave$(LIBBOOST_VERSION)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# libboost.mk Sign
 	$(call SIGN,libboost-atomic$(LIBBOOST_VERSION),general.xml)
