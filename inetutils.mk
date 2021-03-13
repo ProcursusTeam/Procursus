8f1d14b405d7ee710b929233465d6e92bf5f49a5
1Conan <7620342+1Conan@users.noreply.github.com>
2021-03-13 22:05:17 +0800

    add missing /

--- inetutils.mk
+++ inetutils.mk
@@ -31,8 +31,8 @@ inetutils: inetutils-setup ncurses readline
 	$(SED) -i 's/-ltermcap/-lncursesw/g' $(BUILD_WORK)/inetutils/telnetd/Makefile
 	+$(MAKE) -C $(BUILD_WORK)/inetutils install \
 		DESTDIR=$(BUILD_STAGE)/inetutils
-	$(LN) -sf ..$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/ping $(BUILD_STAGE)/inetutils/bin
-	$(LN) -sf ..$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/ping $(BUILD_STAGE)/inetutils/sbin
+	$(LN) -sf ../$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/ping $(BUILD_STAGE)/inetutils/bin
+	$(LN) -sf ../$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/ping $(BUILD_STAGE)/inetutils/sbin
 	touch $(BUILD_WORK)/inetutils/.build_complete
 endif
 
