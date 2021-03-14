410b3e0ccf1b454bae236b2f18a9370cc3dbe86f
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-03-13 16:09:24 -0800

    Fixed git packaging

--- git.mk
+++ git.mk
@@ -61,10 +61,10 @@ endif
 git-package: git-stage
 	# git.mk Package Structure
 	rm -rf $(BUILD_DIST)/git
-	mkdir -p $(BUILD_DIST)/git
+	mkdir -p $(BUILD_DIST)/git/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share,libexec}
 	
 	# git.mk Prep git
-	cp -a $(BUILD_STAGE)/git/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/git
+	cp -a $(BUILD_STAGE)/git/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share,libexec} $(BUILD_DIST)/git/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	
 	# git.mk Sign
 	$(call SIGN,git,general.xml)
