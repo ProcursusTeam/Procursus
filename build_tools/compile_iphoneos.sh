f8750c8bb8c2f2b59a16ab8975e4cd4de1d27ebf
Hayden <me@diatr.us>
2021-03-14 18:44:48 -0400

    Create compile_iphoneos.sh

---
+++ build_tools/compile_iphoneos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if command -v gmake &> /dev/null; then
+	MAKE=gmake
+else
+	MAKE=make
+fi
+${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1500 rebuild-${1}-package &
+${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1600 rebuild-${1}-package &
+${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1700 rebuild-${1}-package &
+wait
