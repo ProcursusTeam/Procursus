d4eecfb9baf77bdb3d737ddca42dd908c6b30e7a
Sudhip Nashi <sudhip@nashi.us>
2021-03-22 15:38:32 -0500

    add jtool2

---
+++ jtool2.mk
@@ -0,0 +1,41 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS  += jtool2
+JTOOL2_VERSION := 2020.12.21
+DEB_JTOOL2_V   ?= $(JTOOL2_VERSION)
+
+jtool2-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) http://newosxbook.com/tools/jtool2.tgz
+	rm -rf $(BUILD_WORK)/jtool2-bin && mkdir -p $(BUILD_WORK)/jtool2-bin && pushd $(BUILD_WORK)/jtool2-bin && $(TAR) -C . -xf $(BUILD_SOURCE)/jtool2.tgz && popd
+
+ifneq ($(wildcard $(BUILD_WORK)/jtool2/.build_complete),)
+jtool2:
+	@echo "Using previously built jtool2."
+else
+jtool2: jtool2-setup
+	echo $(MEMO_TARGET)
+	mkdir -p $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin
+	$(LIPO) $(BUILD_WORK)/jtool2-bin/jtool2 -thin $$(echo $(MEMO_TARGET) | cut -d- -f2) -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/jtool2
+	$(LIPO) $(BUILD_WORK)/jtool2-bin/disarm -thin $$(echo $(MEMO_TARGET) | cut -d- -f2) -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/disarm
+endif
+
+jtool2-package: jtool2-stage
+	# jtool2.mk Package Structure
+	mkdir -p $(BUILD_DIST)/jtool2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	
+	# jtool2.mk Prep jtool2
+	cp -a $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/jtool2 $(BUILD_DIST)/jtool2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	cp -a $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/disarm $(BUILD_DIST)/jtool2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+
+	# jtool2.mk Sign
+	$(call SIGN,jtool2,general.xml)
+	
+	# jtool2.mk Make .debs
+	$(call PACK,jtool2,DEB_JTOOL2_V)
+	
+	# jtool2.mk Build cleanup
+	rm -rf $(BUILD_DIST)/jtool2
+
+.PHONY: jtool2 jtool2-package
