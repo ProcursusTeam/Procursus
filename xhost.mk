a48a892284502cfa45b88e15bfc66adebb799925
Gymxo <55960119+Gymxo@users.noreply.github.com>
2021-03-22 16:00:14 -0400

    Update xhost.mk

--- xhost.mk
+++ xhost.mk
@@ -4,10 +4,10 @@ endif
 
 SUBPROJECTS    += xhost
 XHOST_VERSION := 1.0.8
-DEB_xhost_V   ?= $(XHOST_VERSION)-1
+DEB_XHOST_V   ?= $(XHOST_VERSION)
 
 xhost-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://xorg.freedesktop.org/archive/individual/app/xhost-$(XHOST_VERSION).tar.gz{,.sig}
+	wget -q -nc -P $(BUILD_SOURCE) https://www.x.org/archive//individual/app/xhost-$(XHOST_VERSION).tar.gz{,.sig}
 	$(call PGP_VERIFY,xhost-$(XHOST_VERSION).tar.gz)
 	$(call EXTRACT_TAR,xhost-$(XHOST_VERSION).tar.gz,xhost-$(XHOST_VERSION),xhost)
 
@@ -15,12 +15,12 @@ ifneq ($(wildcard $(BUILD_WORK)/xhost/.build_complete),)
 xhost:
 	@echo "Using previously built xhost."
 else
-xhost: xhost-setup libx11 libxau libxmu xorgproto
+xhost: xhost-setup libx11 libxau libxmu xorgproto xxhash
 	cd $(BUILD_WORK)/xhost && ./configure -C \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=/usr \
-		--sysconfdir=/etc \
-		--localstatedir=/var
+		--sysconfdir=$(MEMO_PREFIX)/etc \
+		--localstatedir=$(MEMO_PREFIX)/var
 	+$(MAKE) -C $(BUILD_WORK)/xhost
 	+$(MAKE) -C $(BUILD_WORK)/xhost install \
 		DESTDIR=$(BUILD_STAGE)/xhost
@@ -30,20 +30,19 @@ xhost: xhost-setup libx11 libxau libxmu xorgproto
 endif
 
 xhost-package: xhost-stage
-	# xhost.mk Package Structure
+# xhost.mk Package Structure
 	rm -rf $(BUILD_DIST)/xhost
-	mkdir -p $(BUILD_DIST)/xhost/usr/bin
 	
-	# xhost.mk Prep xhost
-	cp -a $(BUILD_STAGE)/xhost/usr/bin/xhost $(BUILD_DIST)/xhost/usr/bin
+# xhost.mk Prep xhost
+	cp -a $(BUILD_STAGE)/xhost $(BUILD_DIST)
 	
-	# xhost.mk Sign
+# xhost.mk Sign
 	$(call SIGN,xhost,general.xml)
 	
-	# xhost.mk Make .debs
-	$(call PACK,xhost,DEB_xhost_V)
+# xhost.mk Make .debs
+	$(call PACK,xhost,DEB_XHOST_V)
 	
-	# xhost.mk Build cleanup
+# xhost.mk Build cleanup
 	rm -rf $(BUILD_DIST)/xhost
 
 .PHONY: xhost xhost-package
