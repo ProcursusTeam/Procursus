a63bfb1faab49a9dfa30cfcb4a68b901107b598e
Gymxo <55960119+Gymxo@users.noreply.github.com>
2021-03-28 11:27:38 -0400

    Add the mk files

---
+++ xkeyboard-config.mk
@@ -0,0 +1,47 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS    += xkeyboard-config
+XKEYBOARD-CONFIG_VERSION := 2.5
+DEB_XKEYBOARD-CONFIG_V   ?= $(XKEYBOARD-CONFIG_VERSION)
+
+xkeyboard-config-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) https://www.x.org/archive/individual/data/xkeyboard-config-$(XKEYBOARD-CONFIG_VERSION).tar.gz
+	$(call EXTRACT_TAR,xkeyboard-config-$(XKEYBOARD-CONFIG_VERSION).tar.gz,xkeyboard-config-$(XKEYBOARD-CONFIG_VERSION),xkeyboard-config)
+
+ifneq ($(wildcard $(BUILD_WORK)/xkeyboard-config/.build_complete),)
+xkeyboard-config:
+	@echo "Using previously built xkeyboard-config."
+else
+xkeyboard-config: xkeyboard-config-setup libx11 libxau libxmu xorgproto xxhash
+	cd $(BUILD_WORK)/xkeyboard-config && ./autogen.sh \
+		--host=$(GNU_HOST_TRIPLE) \
+		--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+		--sysconfdir=$(MEMO_PREFIX)/etc \
+		--localstatedir=$(MEMO_PREFIX)/var 
+	+$(MAKE) -C $(BUILD_WORK)/xkeyboard-config
+	+$(MAKE) -C $(BUILD_WORK)/xkeyboard-config install \
+		DESTDIR=$(BUILD_STAGE)/xkeyboard-config
+	+$(MAKE) -C $(BUILD_WORK)/xkeyboard-config install \
+		DESTDIR=$(BUILD_BASE)
+	touch $(BUILD_WORK)/xkeyboard-config/.build_complete
+endif
+
+xkeyboard-config-package: xkeyboard-config-stage
+# xkeyboard-config.mk Package Structure
+	rm -rf $(BUILD_DIST)/xkeyboard-config
+	
+# xkeyboard-config.mk Prep xkeyboard-config
+	cp -a $(BUILD_STAGE)/xkeyboard-config $(BUILD_DIST)
+	
+# xkeyboard-config.mk Sign
+	$(call SIGN,xkeyboard-config,general.xml)
+	
+# xkeyboard-config.mk Make .debs
+	$(call PACK,xkeyboard-config,DEB_XKEYBOARD-CONFIG_V)
+	
+# xkeyboard-config.mk Build cleanup
+	rm -rf $(BUILD_DIST)/xkeyboard-config
+
+.PHONY: xkeyboard-config xkeyboard-config-package
