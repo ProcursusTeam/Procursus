0ff52d6ed302e331f5d22f022dc613c48637ebd2
Hayden <me@diatr.us>
2021-04-05 16:47:13 -0400

    Update zpaq.mk

--- zpaq.mk
+++ zpaq.mk
@@ -2,26 +2,27 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS       += zpaq
-ZPAQ_VERSION  := 7.15
-DEB_ZPAQ_V    ?= $(ZPAQ_VERSION)
+SUBPROJECTS  += zpaq
+ZPAQ_VERSION := 7.15
+DEB_ZPAQ_V   ?= $(ZPAQ_VERSION)
 
 zpaq-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/zpaq/zpaq/archive/refs/tags/$(ZPAQ_VERSION).tar.gz
 	$(call EXTRACT_TAR,$(ZPAQ_VERSION).tar.gz,zpaq-$(ZPAQ_VERSION),zpaq)
+
 ifneq ($(wildcard $(BUILD_WORK)/zpaq/.build_complete),)
 zpaq:
 	@echo "Using previously built zpaq."
 else
 zpaq: zpaq-setup
 	+$(MAKE) -C $(BUILD_WORK)/zpaq \
-	CXX=$(CXX) \
-	CXXFLAGS="$(CXXFLAGS)" \
-	LDFLAGS="$(LDFLAGS)"
+		CXX=$(CXX) \
+		CXXFLAGS="$(CXXFLAGS)" \
+		LDFLAGS="$(LDFLAGS)"
 	+$(MAKE) -C $(BUILD_WORK)/zpaq install \
-	PREFIX=$(BUILD_STAGE)/zpaq/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+		PREFIX=$(BUILD_STAGE)/zpaq/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	+$(MAKE) -C $(BUILD_WORK)/zpaq install \
-	PREFIX=$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+		PREFIX=$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	touch $(BUILD_WORK)/zpaq/.build_complete
 endif
 zpaq-package: zpaq-stage
