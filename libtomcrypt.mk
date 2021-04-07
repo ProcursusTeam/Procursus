5f7b7fd765ddaab1ed0300f76cfdb074571da7db
Cameron Katri <me@cameronkatri.com>
2021-04-07 18:46:59 -0400

    Should fix libtom* install names but I didn't test

--- libtomcrypt.mk
+++ libtomcrypt.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPPROJECTS        += libtomcrypt
 LIBTOMCRYPT_VERSION := 1.18.2
-DEB_LIBTOMCRYPT_V   ?= $(LIBTOMCRYPT_VERSION)
+DEB_LIBTOMCRYPT_V   ?= $(LIBTOMCRYPT_VERSION)-1
 
 libtomcrypt-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/libtom/libtomcrypt/releases/download/v$(LIBTOMCRYPT_VERSION)/crypt-$(LIBTOMCRYPT_VERSION).tar.xz
@@ -23,16 +23,19 @@ libtomcrypt: libtomcrypt-setup libgmp10 libtommath
 	cd $(BUILD_WORK)/libtomcrypt/libtool && LIBTOOLIZE="$(LIBTOOLIZE) -i" autoreconf -fi
 	cd $(BUILD_WORK)/libtomcrypt/libtool && ./configure -C \
 		--build=$$($(BUILD_MISC)/config.guess) \
-		--host=$(GNU_HOST_TRIPLE)
+		--host=$(GNU_HOST_TRIPLE) \
+		--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	+$(MAKE) -C $(BUILD_WORK)/libtomcrypt -f makefile.shared \
 		EXTRALIBS="-lgmp -ltommath" \
 		LIBTOOL="$(BUILD_WORK)/libtommath/libtool/libtool" \
 		CFLAGS="$(CFLAGS) -DGMP_DESC -DLTM_DESC -DUSE_LTM"
 	+$(MAKE) -C $(BUILD_WORK)/libtomcrypt -f makefile.shared install \
-		PREFIX="/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
+		LIBTOOL="$(BUILD_WORK)/libtommath/libtool/libtool" \
+		PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
 		DESTDIR="$(BUILD_STAGE)/libtomcrypt"
 	+$(MAKE) -C $(BUILD_WORK)/libtomcrypt -f makefile.shared install \
-		PREFIX="/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
+		LIBTOOL="$(BUILD_WORK)/libtommath/libtool/libtool" \
+		PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
 		DESTDIR="$(BUILD_BASE)"
 	touch $(BUILD_WORK)/libtomcrypt/.build_complete
 endif
@@ -41,19 +44,19 @@ libtomcrypt-package: libtomcrypt-stage
 	# libtomcrypt.mk Package Structure
 	rm -rf $(BUILD_DIST)/libtomcrypt{1,-dev}
 	mkdir -p $(BUILD_DIST)/libtomcrypt{1,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
+	
 	# libtomcrypt.mk Prep libtomcrypt
 	cp -a $(BUILD_STAGE)/libtomcrypt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libtomcrypt.1.dylib $(BUILD_DIST)/libtomcrypt1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/libtomcrypt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libtomcrypt-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	cp -a $(BUILD_STAGE)/libtomcrypt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{libtomcrypt.{a,dylib},pkgconfig} $(BUILD_DIST)/libtomcrypt-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
+	
 	# libtomcrypt.mk Sign
 	$(call SIGN,libtomcrypt1,general.xml)
-
+	
 	# libtomcrypt.mk Make .debs
 	$(call PACK,libtomcrypt1,DEB_LIBTOMCRYPT_V)
 	$(call PACK,libtomcrypt-dev,DEB_LIBTOMCRYPT_V)
-
+	
 	# libtomcrypt.mk Build cleanup
 	rm -rf $(BUILD_DIST)/libtomcrypt{1,-dev}
 
