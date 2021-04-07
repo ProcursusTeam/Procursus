1dcf8369338fa2d1ceafa25dc0aa94559f1b10b7
Hayden <me@diatr.us>
2021-04-07 05:25:23 -0400

    Link correct ncurses rc

--- rc.mk
+++ rc.mk
@@ -15,33 +15,34 @@ ifneq ($(wildcard $(BUILD_WORK)/rc/.build_complete),)
 rc:
 	@echo "Using previously built rc."
 else
-rc: rc-setup readline
+rc: rc-setup readline ncurses
 	cd $(BUILD_WORK)/rc && autoreconf -fi && ac_cv_func_setpgrp_void=yes ./configure \
 		--host=$(GNU_HOST_TRIPLE) \
 		--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
-		--with-edit=readline \
+		--with-edit=readline
 
 	sed -i 's/HAVE_SYSV_SIGCLD\ 1/HAVE_SYSV_SIGCLD\ 0/g' $(BUILD_WORK)/rc/config.h
-	
-	+$(MAKE) -C $(BUILD_WORK)/rc
+
+	+$(MAKE) -C $(BUILD_WORK)/rc \
+		LIBS="-lreadline -lncursesw"
 	+$(MAKE) -C $(BUILD_WORK)/rc install \
 		DESTDIR=$(BUILD_STAGE)/rc
-		
+
 	touch $(BUILD_WORK)/rc/.build_complete
 endif
 rc-package: rc-stage
 	# rc.mk Package Structure
 	rm -rf $(BUILD_DIST)/rc
-	
+
 	# rc.mk Prep rc
 	cp -a $(BUILD_STAGE)/rc $(BUILD_DIST)
-	
+
 	# rc.mk Sign
 	$(call SIGN,rc,general.xml)
-	
+
 	# rc.mk Make .debs
 	$(call PACK,rc,DEB_RC_V)
-	
+
 	# rc.mk Build cleanup
 	rm -rf $(BUILD_DIST)/rc
 
