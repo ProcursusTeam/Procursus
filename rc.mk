96669d591c525f858104ef0e0786f5fc548c6e5a
sudhip <sudhip@nashi.us>
2021-04-07 04:32:16 -0500

    add tcsh and rc (#606)
    * tcsh
    
    * rc from plan9
    
    * fix rc mkfile
    
    * fix rc mkfile again
    
    * fix tcsh mkfile
    
    * Make rc control more accurate
    
    This is not the original rc shell, but rather a reimplementation of it.
    
    * add ncurses dependency and force linkage with procursus ncursesw
    
    * Update tcsh.control
    
    * Link correct ncurses rc
    
    * Update rc.control
    
    * Update rc.mk
    
    Co-authored-by: Hayden <me@diatr.us>

---
+++ rc.mk
@@ -0,0 +1,50 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS += rc
+RC_VERSION  := 1.7.4
+DEB_RC_V    ?= $(RC_VERSION)
+
+rc-setup: setup
+	## Convert this to the GITHUB_ARCHIVE function.
+	wget -O $(BUILD_SOURCE)/rc-$(RC_VERSION).tar.gz https://github.com/rakitzis/rc/archive/refs/tags/v$(RC_VERSION).tar.gz
+	$(call EXTRACT_TAR,rc-$(RC_VERSION).tar.gz,rc-$(RC_VERSION),rc)
+	$(call DO_PATCH,rc,rc,-p1)
+
+ifneq ($(wildcard $(BUILD_WORK)/rc/.build_complete),)
+rc:
+	@echo "Using previously built rc."
+else
+rc: rc-setup readline ncurses
+	cd $(BUILD_WORK)/rc && autoreconf -fi && ac_cv_func_setpgrp_void=yes ./configure \
+		--host=$(GNU_HOST_TRIPLE) \
+		--prefix=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+		--with-edit=readline
+
+	sed -i 's/HAVE_SYSV_SIGCLD\ 1/HAVE_SYSV_SIGCLD\ 0/g' $(BUILD_WORK)/rc/config.h
+
+	+$(MAKE) -C $(BUILD_WORK)/rc \
+		LIBS="-lreadline -lncursesw"
+	+$(MAKE) -C $(BUILD_WORK)/rc install \
+		DESTDIR=$(BUILD_STAGE)/rc
+
+	touch $(BUILD_WORK)/rc/.build_complete
+endif
+rc-package: rc-stage
+	# rc.mk Package Structure
+	rm -rf $(BUILD_DIST)/rc
+
+	# rc.mk Prep rc
+	cp -a $(BUILD_STAGE)/rc $(BUILD_DIST)
+
+	# rc.mk Sign
+	$(call SIGN,rc,general.xml)
+
+	# rc.mk Make .debs
+	$(call PACK,rc,DEB_RC_V)
+
+	# rc.mk Build cleanup
+	rm -rf $(BUILD_DIST)/rc
+
+.PHONY: rc rc-package
