96dfa5511f5a30660a8b80982ca884df8cd5db14
sudhip <sudhip@nashi.us>
2021-04-07 17:31:07 -0500

    allow executing shell scripts in fish (#610)
    * add fish patch
    
    * Fixup formatting
    
    * Link libintl :/
    
    Co-authored-by: Hayden <me@diatr.us>

--- fish.mk
+++ fish.mk
@@ -8,9 +8,10 @@ DEB_FISH_V    ?= $(FISH_VERSION)
 
 fish-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/fish-shell/fish-shell/releases/download/$(FISH_VERSION)/fish-$(FISH_VERSION).tar.xz{,.asc}
- 	$(call PGP_VERIFY,fish-$(FISH_VERSION).tar.xz,asc)
- 	$(call EXTRACT_TAR,fish-$(FISH_VERSION).tar.xz,fish-$(FISH_VERSION),fish)
- 	$(SED) -i '/codesign_on_mac/d' $(BUILD_WORK)/fish/CMakeLists.txt
+	$(call PGP_VERIFY,fish-$(FISH_VERSION).tar.xz,asc)
+	$(call EXTRACT_TAR,fish-$(FISH_VERSION).tar.xz,fish-$(FISH_VERSION),fish)
+	$(SED) -i '/codesign_on_mac/d' $(BUILD_WORK)/fish/CMakeLists.txt
+	$(call DO_PATCH,fish,fish,-p0)
 
 ifneq ($(wildcard $(BUILD_WORK)/fish/.build_complete),)
 fish:
@@ -32,6 +33,8 @@ fish: fish-setup ncurses gettext pcre2
 		-DCURSES_INCLUDE_PATH="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/ncursesw" \
 		-DSYS_PCRE2_LIB="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libpcre2-32.dylib" \
  		-DSYS_PCRE2_INCLUDE_DIR="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/pcre2" \
+		-DIntl_LIBRARY="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libintl.dylib" \
+		-DIntl_INCLUDE_DIR="$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include" \
  		-DSED=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/$(GNU_PREFIX)sed \
 		-DCMAKE_INSTALL_SYSCONFDIR=$(MEMO_PREFIX)/etc \
 		-Dextra_functionsdir=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_functions.d \
@@ -41,7 +44,6 @@ fish: fish-setup ncurses gettext pcre2
 	+$(MAKE) -C $(BUILD_WORK)/fish
 	+$(MAKE) -C $(BUILD_WORK)/fish install \
 		DESTDIR=$(BUILD_STAGE)/fish
-
 	touch $(BUILD_WORK)/fish/.build_complete
 endif
 
