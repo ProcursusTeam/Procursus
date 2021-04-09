b89c6cb79f70837515eb9cadf538768d872f4cb9
DaOfficialRolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-04-08 17:15:07 -0700

    Final fixes for symlinking

--- ccache.mk
+++ ccache.mk
@@ -28,36 +28,43 @@ ifneq ($(wildcard $(BUILD_WORK)/ccache/.build_complete),)
 ccache:
 	@echo "Using previously built ccache."
 else
-ccache: ccache-setup
+ccache: ccache-setup zstd
 	cd $(BUILD_WORK)/ccache && cmake . \
 		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_SYSTEM_NAME=Darwin \
+		-DCMAKE_SYSTEM=Darwin \
 		-DCMAKE_CROSSCOMPILING=true \
 		-DCMAKE_INSTALL_NAME_TOOL=$(I_N_T) \
 		-DCMAKE_INSTALL_PREFIX=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
 		-DCMAKE_INSTALL_NAME_DIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin \
 		-DCMAKE_INSTALL_RPATH=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
-		-DCMAKE_INSTALL_SYSCONFDIR=$(MEMO_PREFIX)/etc \
+		-DCMAKE_INSTALL_SYSCONFDIR=$(MEMO_PREFIX)/etc/ \
 		-DCMAKE_OSX_SYSROOT="$(TARGET_SYSROOT)" \
 		-DCMAKE_C_FLAGS="$(CFLAGS)" \
 		-DCMAKE_CXX_FLAGS="$(CXXFLAGS)" \
 		-DCMAKE_FIND_ROOT_PATH=$(BUILD_BASE) \
+		-DZSTD_INCLUDE_DIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include \
+		-DZSTD_LIBRARY=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libzstd.1.dylib \
 		$(CCACHE_CMAKE_ARGS) \
 		.
 	+$(MAKE) -C $(BUILD_WORK)/ccache
 	+$(MAKE) -C $(BUILD_WORK)/ccache install \
-		DESTDIR="$(BUILD_STAGE)/ccache"
+		DESTDIR="$(BUILD_STAGE)/ccache"  
 	touch $(BUILD_WORK)/ccache/.build_complete
 endif
 
 ccache-package: ccache-stage
 	# ccache.mk Package Structure
 	rm -rf $(BUILD_DIST)/ccache
+	rm -rf $(BUILD_DIST)/ccache/usr/
+	mkdir -p $(BUILD_STAGE)/ccache/usr/libexec/ccache/{clang,clang++,cc,gcc,gcc2,gcc3,gcc-3.3,gcc-4.0,gcc-4.2,gcc-4.3,gcc-4.4,gcc-4.5,gcc-4.6,gcc-4.7,gcc-4.8,gcc-4.9,gcc-5,gcc-6,gcc-7,gcc-8,gcc-9,gcc-10,c++,c++3,c++-3.3,c++-4.0,c++-4.2,c++-4.3,c++-4.4,c++-4.5,c++-4.6,c++-4.7,c++-4.8,c++-4.9,c++-5,c++-6,c++-7,c++-8,c++-9,c++-10,g++,g++2,g++3,g++-3.3,g++-4.0,g++-4.2,g++-4.3,g++-4.4,g++-4.5,g++-4.6,g++-4.7,g++-4.8,g++-4.9,g++-5,g++-6,g++-7,g++-8,g++-9,g++-10}
+	mkdir -p $(BUILD_DIST)/ccache/usr/libexec/ccache/
+	ln -s $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/{clang,clang++,cc,gcc,gcc2,gcc3,gcc-3.3,gcc-4.0,gcc-4.2,gcc-4.3,gcc-4.4,gcc-4.5,gcc-4.6,gcc-4.7,gcc-4.8,gcc-4.9,gcc-5,gcc-6,gcc-7,gcc-8,gcc-9,gcc-10,c++,c++3,c++-3.3,c++-4.0,c++-4.2,c++-4.3,c++-4.4,c++-4.5,c++-4.6,c++-4.7,c++-4.8,c++-4.9,c++-5,c++-6,c++-7,c++-8,c++-9,c++-10,g++,g++2,g++3,g++-3.3,g++-4.0,g++-4.2,g++-4.3,g++-4.4,g++-4.5,g++-4.6,g++-4.7,g++-4.8,g++-4.9,g++-5,g++-6,g++-7,g++-8,g++-9,g++-10} $(BUILD_DIST)/ccache/usr/libexec/ccache/
 	mkdir -p $(BUILD_DIST)/ccache/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/man/man1}
+	
 
 	# ccache.mk Prep ccache
 	cp -a $(BUILD_STAGE)/ccache/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share} $(BUILD_DIST)/ccache/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-
+	cp -a $(BUILD_STAGE)/ccache/usr/libexec/ccache	$(BUILD_DIST)/ccache/usr/libexec/ccache
 	# ccache.mk Sign
 	$(call SIGN,ccache,general.xml)
 
