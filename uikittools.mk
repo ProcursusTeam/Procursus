373436e466dd8f930ae8528e8be0f1b430514796
Cameron Katri <me@cameronkatri.com>
2021-04-13 16:04:27 -0400

    Update uikittools

--- uikittools.mk
+++ uikittools.mk
@@ -7,12 +7,13 @@ STRAPPROJECTS      += uikittools
 else # ($(MEMO_TARGET),darwin-\*)
 SUBPROJECTS        += uikittools
 endif # ($(MEMO_TARGET),darwin-\*)
-UIKITTOOLS_VERSION := 2.0.3
+UIKITTOOLS_COMMIT  := 5612b677b09dc8851f2c830a26d70183c10045a2
+UIKITTOOLS_VERSION := 2.0.4
 DEB_UIKITTOOLS_V   ?= $(UIKITTOOLS_VERSION)
 
 uikittools-setup: setup
-	$(call GITHUB_ARCHIVE,Diatrus,uikittools-ng,$(UIKITTOOLS_VERSION),v$(UIKITTOOLS_VERSION))
-	$(call EXTRACT_TAR,uikittools-ng-$(UIKITTOOLS_VERSION).tar.gz,uikittools-ng-$(UIKITTOOLS_VERSION),uikittools)
+	$(call GITHUB_ARCHIVE,Diatrus,uikittools-ng,$(UIKITTOOLS_COMMIT),$(UIKITTOOLS_COMMIT))
+	$(call EXTRACT_TAR,uikittools-ng-$(UIKITTOOLS_COMMIT).tar.gz,uikittools-ng-$(UIKITTOOLS_COMMIT),uikittools)
 
 ifneq ($(wildcard $(BUILD_WORK)/uikittools/.build_complete),)
 uikittools:
@@ -36,13 +37,13 @@ uikittools-package: uikittools-stage
 	# uikittools.mk Package Structure
 	rm -rf $(BUILD_DIST)/uikittools
 	mkdir -p $(BUILD_DIST)/uikittools
-
+	
 	# uikittools.mk Prep uikittools
 	cp -a $(BUILD_STAGE)/uikittools/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/uikittools
-
+	
 	# uikittools.mk Make .debs
 	$(call PACK,uikittools,DEB_UIKITTOOLS_V)
-
+	
 	# uikittools.mk Build cleanup
 	rm -rf $(BUILD_DIST)/uikittools
 
