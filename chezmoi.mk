11c6d996c0f05e72ccea0ddf2809348b9647cc8f
Benjamin G <randomblock1@pm.me>
2021-04-17 17:22:48 -0500

    use DEFAULT_GOLANG_FLAGS

--- chezmoi.mk
+++ chezmoi.mk
@@ -6,17 +6,6 @@ SUBPROJECTS     += chezmoi
 CHEZMOI_VERSION := 2.0.10
 DEB_CHEZMOI_V   ?= $(CHEZMOI_VERSION)
 
-ifeq ($(MEMO_TARGET),iphoneos-arm64)
-GO_ARGS := GOARCH=arm64 \
-        GOOS=ios
-else ifeq ($(MEMO_TARGET),darwin-amd64)
-GO_ARGS := GOARCH=amd64 \
-        GOOS=darwin
-else ifneq ($(MEMO_TARGET),darwin-arm64)
-GO_ARGS := GOARCH=arm64 \
-        GOOS=darwin
-endif
-
 chezmoi-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://github.com/twpayne/chezmoi/archive/refs/tags/v$(CHEZMOI_VERSION).tar.gz
 	$(call EXTRACT_TAR,v$(CHEZMOI_VERSION).tar.gz,chezmoi-$(CHEZMOI_VERSION),chezmoi)
@@ -30,12 +19,7 @@ else
 chezmoi: chezmoi-setup
 	cd $(BUILD_WORK)/chezmoi && go get -d -v .
 	cd $(BUILD_WORK)/chezmoi && \
-		CGO_ENABLED=1 \
-		CGO_CFLAGS="$(CFLAGS)" \
-		CGO_CPPFLAGS="$(CPPFLAGS)" \
-		CGO_LDFLAGS="$(LDFLAGS)" \
-		CC="$(CC)" \
-		$(GO_ARGS) \
+		$(DEFAULT_GOLANG_FLAGS) \
 		go build
 	cp -a $(BUILD_WORK)/chezmoi/chezmoi $(BUILD_STAGE)/chezmoi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/usr/bin
 	touch $(BUILD_WORK)/chezmoi/.build_complete
@@ -50,7 +34,7 @@ chezmoi-package: chezmoi-stage
 
 	# chezmoi.mk Sign
 	$(call SIGN,chezmoi,general.xml)
-	
+
 	# chezmoi.mk Make .debs
 	$(call PACK,chezmoi,DEB_CHEZMOI_V)
 
