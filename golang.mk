0d13b603c93b3f43310a1919834125b2ceac7751
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-04-17 20:57:23 +0000

    Updated golang to 1.16.3 (#579)
    * Updated golang and fixed most of it. AMD64 compilation still needs work.
    
    * Updated and fixed golang for amd64
    
    * Removed clang as a package dependency for macOS and fixed a packaging mistake.
    
    * Specified GOROOT_BOOTSTRAP to fix golang checking wrong src directory.
    
    * Added condition to compile correctly across all 3 arches.
    
    * Better
    
    * Fix a hayden-issue
    
    Co-authored-by: Hayden <me@diatr.us>

--- golang.mk
+++ golang.mk
@@ -1,10 +1,9 @@
 ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
-
 SUBPROJECTS    += golang
-GOLANG_MAJOR_V := 1.15
-GOLANG_VERSION := $(GOLANG_MAJOR_V).7
+GOLANG_MAJOR_V := 1.16
+GOLANG_VERSION := $(GOLANG_MAJOR_V).3
 DEB_GOLANG_V   ?= $(GOLANG_VERSION)
 
 golang-setup: setup
@@ -12,12 +11,11 @@ golang-setup: setup
 	$(call EXTRACT_TAR,go$(GOLANG_VERSION).src.tar.gz,go,golang)
 	mkdir -p $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)
 	mkdir -p $(BUILD_WORK)/golang/superbin
-	cp -a $(BUILD_WORK)/golang/misc/ios/clangwrap.sh $(BUILD_WORK)/golang/superbin/clang
+ifneq (,$(findstring ios,$(shell echo $(RUST_TARGET) | cut -f3 -d-)))
+	cp -a $(BUILD_WORK)/golang/misc/ios/clangwrap.sh $(BUILD_WORK)/golang/superbin/$(CC)
+endif
 
-ifneq ($(MEMO_ARCH),arm64)
-golang:
-	@echo "Unsupported target $(MEMO_TARGET)"
-else ifneq ($(UNAME),Darwin)
+ifneq ($(UNAME),Darwin)
 golang:
 	@echo "golang building only supported on macOS"
 else ifneq ($(wildcard $(BUILD_WORK)/golang/.build_complete),)
@@ -26,18 +24,20 @@ golang:
 else
 golang: golang-setup
 	export PATH="$(BUILD_WORK)/golang/superbin:$(PATH)"; \
-		cd $(BUILD_WORK)/golang/src && \
-			CGO_ENABLED=1 \
-			GOROOT_FINAL=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V) \
-			GOHOSTARCH=amd64 \
-			GOHOSTOS=darwin \
-			GOARCH=arm64 \
-			GOOS=darwin \
-			CC=cc \
-			CC_FOR_TARGET=clang \
-			./make.bash
+	cd $(BUILD_WORK)/golang/src && \
+		CGO_ENABLED=1 \
+		GOROOT_FINAL=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V) \
+        GOOS=$(shell echo $(RUST_TARGET) | cut -f3 -d-) \
+		GOARCH=$(shell echo $(MEMO_TARGET) | cut -f2 -d-) \
+		CC=$(shell which cc) \
+		CC_FOR_TARGET="$(CC)" \
+		./make.bash
 	cp -a $(BUILD_WORK)/golang/* $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)
+ifneq (,$(findstring arm64,$(MEMO_TARGET)))
 	-find $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V) -name darwin_amd64 -type d -exec rm -rf {} \;
+else
+	-find $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V) -name darwin_arm64 -type d -exec rm -rf {} \;
+endif
 	touch $(BUILD_WORK)/golang/.build_complete
 endif
 
@@ -47,20 +47,22 @@ golang-package: golang-stage
 	mkdir -p $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-src/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V) \
 		$(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/{bin,pkg} \
 		$(BUILD_DIST)/golang-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,lib}
-
 	# golang.mk Prep golang-$(GOLANG_MAJOR_V)-src
 	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/{api,misc,src,test} $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-src/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)
 
 	# golang.mk Prep golang-$(GOLANG_MAJOR_V)-go
 	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/VERSION $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)
-	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin/darwin_arm64/go{,fmt} $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin
+ifneq ($(wildcard $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin/$(shell echo $(RUST_TARGET) | cut -f3 -d-)_$(shell echo $(MEMO_TARGET) | cut -f2 -d-)/.*),)
+	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin/$(shell echo $(RUST_TARGET) | cut -f3 -d-)_$(shell echo $(MEMO_TARGET) | cut -f2 -d-)/go{,fmt} $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin
+else
+	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin/go{,fmt} $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/bin
+endif
 	cp -a $(BUILD_STAGE)/golang/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/pkg/{*_*,include,tool} $(BUILD_DIST)/golang-$(GOLANG_MAJOR_V)-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go-$(GOLANG_MAJOR_V)/pkg
 
 	# golang.mk Prep golang-go
 	ln -s ../lib/go-$(GOLANG_MAJOR_V)/bin/go $(BUILD_DIST)/golang-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/go
 	ln -s ../lib/go-$(GOLANG_MAJOR_V)/bin/gofmt $(BUILD_DIST)/golang-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gofmt
 	ln -s ../lib/go-$(GOLANG_MAJOR_V) $(BUILD_DIST)/golang-go/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/go
-
 	# golang.mk Sign
 	$(call SIGN,golang-$(GOLANG_MAJOR_V)-go,general.xml)
 
@@ -71,5 +73,4 @@ golang-package: golang-stage
 
 	# golang.mk Build cleanup
 	rm -rf $(BUILD_DIST)/golang{,-$(GOLANG_MAJOR_V)}-{src,go}
-
 .PHONY: golang golang-package
