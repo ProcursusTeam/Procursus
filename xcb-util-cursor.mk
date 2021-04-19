45fbfc0471dd7b5840d8d371c8f92736628caa5c
Gymxo <55960119+Gymxo@users.noreply.github.com>
2021-04-19 13:32:08 -0400

    did I mess up all of these?

--- xcb-util-cursor.mk
+++ xcb-util-cursor.mk
@@ -27,24 +27,24 @@ xcb-util-cursor: xcb-util-cursor-setup libxcb xcb-util
 endif
 
 xcb-util-cursor-package: xcb-util-cursor-stage
-	rm -rf $(BUILD_DIST)/libxcb-cursor{1,-dev}
-	mkdir -p $(BUILD_DIST)/libxcb-cursor{1,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	rm -rf $(BUILD_DIST)/libxcb-cursor{0,-dev}
+	mkdir -p $(BUILD_DIST)/libxcb-cursor{0,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# libxcb-curosr.mk Prep libutil-xrm1
-	cp -a $(BUILD_STAGE)/libxcb-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/lib-xrm.1.dylib $(BUILD_DIST)/libxcb-cursor1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	# xcb-util-cursor.mk Prep libxcb-cursor0
+	cp -a $(BUILD_STAGE)/xcb-util-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxcb-cursor.0.dylib $(BUILD_DIST)/libxcb-cursor0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# libxcb-cursor.mk Prep libxcb-cursor-dev
-	cp -a $(BUILD_STAGE)/libxcb-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-cursor.1.dylib) $(BUILD_DIST)/libxcb-cursor-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libxcb-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-cursor-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	# xcb-util-cursor.mk Prep libxcb-cursor-dev
+	cp -a $(BUILD_STAGE)/xcb-util-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-cursor.0.dylib) $(BUILD_DIST)/libxcb-cursor-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/xcb-util-cursor/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-cursor-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
-	# libxcb-cursor.mk Sign
+	# xcb-util-cursor.mk Sign
 	$(call SIGN,libxcb-cursor1,general.xml)
 
-	# libxcb-cursor.mk Make .debs
-	$(call PACK,libxcb-cursor1,DEB_xcb-cursor_V)
-	$(call PACK,libxcb-cursor-dev,DEB_xcb-cursor_V)
+	# xcb-util-cursor.mk Make .debs
+	$(call PACK,libxcb-cursor0,DEB_XCB-UTIL-CURSOR_V)
+	$(call PACK,libxcb-cursor-dev,DEB_XCB-UTIL-CURSOR_V)
 
-	# libxcb-cursor.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libxcb-cursor{1,-dev}
+	# xcb-util-cursor.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libxcb-cursor{0,-dev}
 
 .PHONY: xcb-util-cursor xcb-util-cursor-package
