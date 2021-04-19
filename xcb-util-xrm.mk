c00ffcc7cae269b1aeef001fa4ac3b13efc2e2e6
Gymxo <55960119+Gymxo@users.noreply.github.com>
2021-04-19 13:24:15 -0400

    fix packing errors

--- xcb-util-xrm.mk
+++ xcb-util-xrm.mk
@@ -29,24 +29,24 @@ xcb-util-xrm: xcb-util-xrm-setup libxcb xcb-util
 endif
 
 xcb-util-xrm-package: xcb-util-xrm-stage
-	rm -rf $(BUILD_DIST)/libxcb-xrm{1,-dev}
-	mkdir -p $(BUILD_DIST)/libxcb-xrm{1,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	rm -rf $(BUILD_DIST)/libxcb-xrm{0,-dev}
+	mkdir -p $(BUILD_DIST)/libxcb-xrm{0,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	# xcb-util-xrm.mk Prep libutil-xrm1
-	cp -a $(BUILD_STAGE)/libxcb-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/lib-xrm.1.dylib $(BUILD_DIST)/libxcb-xrm1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/xcb-util-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/lib-xrm.0.dylib $(BUILD_DIST)/libxcb-xrm1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	# libxcb-xrm.mk Prep libxcb-xrm-dev
-	cp -a $(BUILD_STAGE)/libxcb-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-xrm.1.dylib) $(BUILD_DIST)/libxcb-xrm-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libxcb-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-xrm-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/xcb-util-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-xrm.0.dylib) $(BUILD_DIST)/libxcb-xrm-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/xcb-util-xrm/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-xrm-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# libxcb.mk Sign
-	$(call SIGN,libxcb-xrm1,general.xml)
+	$(call SIGN,libxcb-xrm0,general.xml)
 
 	# libxcb-xrm.mk Make .debs
-	$(call PACK,libxcb-xrm1,DEB_xcb-xrm_V)
-	$(call PACK,libxcb-xrm-dev,DEB_xcb-xrm_V)
+	$(call PACK,libxcb-xrm0,DEB_XCB-UTIL-XRM_V)
+	$(call PACK,libxcb-xrm-dev,DEB_XCB-UTIL-XRM_V)
 
 	# libxcb-xrm.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libxcb-xrm{1,-dev}
+	rm -rf $(BUILD_DIST)/libxcb-xrm{0,-dev}
 
 .PHONY: xcb-util-xrm xcb-util-xrm-package
