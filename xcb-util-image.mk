23c4a1ac990a18de8a4f71e1e8421f0099e10356
Gymxo <55960119+Gymxo@users.noreply.github.com>
2021-04-19 13:29:58 -0400

    more dumb mistakes

--- xcb-util-image.mk
+++ xcb-util-image.mk
@@ -29,21 +29,21 @@ xcb-util-image-package: xcb-util-image-stage
 	rm -rf $(BUILD_DIST)/libxcb-image{0,-dev}
 	mkdir -p $(BUILD_DIST)/libxcb-image{0,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# xcb-util-image.mk Prep libutil-xrm1
-	cp -a $(BUILD_STAGE)/libxcb-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/lib-xrm.0.dylib $(BUILD_DIST)/libxcb-image1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	# xcb-util-image.mk Prep libxcb-image9
+	cp -a $(BUILD_STAGE)/xcb-util-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxcb-image.0.dylib $(BUILD_DIST)/libxcb-image1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# libxcb-image.mk Prep libxcb-image-dev
-	cp -a $(BUILD_STAGE)/libxcb-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-image.0.dylib) $(BUILD_DIST)/libxcb-image-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libxcb-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-image-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	# xcb-util-image.mk Prep libxcb-image-dev
+	cp -a $(BUILD_STAGE)/xcb-util-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(libxcb-image.0.dylib) $(BUILD_DIST)/libxcb-image-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/xcb-util-image/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libxcb-image-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
-	# libxcb-image.mk Sign
-	$(call SIGN,libxcb-image1,general.xml)
+	# xcb-util-image.mk Sign
+	$(call SIGN,libxcb-image0,general.xml)
 
-	# libxcb-image.mk Make .debs
-	$(call PACK,libxcb-image1,DEB_xcb-image_V)
-	$(call PACK,libxcb-image-dev,DEB_xcb-image_V)
+	# xcb-util-image.mk Make .debs
+	$(call PACK,libxcb-image0,DEB_XCB-UTIL-IMAGE_V)
+	$(call PACK,libxcb-image-dev,DEB_XCB-UTIL-IMAGE_V)
 
-	# libxcb-image.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libxcb-image{1,-dev}
+	# xcb-util-image.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libxcb-image{0,-dev}
 
 .PHONY: xcb-util-image xcb-util-image-package
