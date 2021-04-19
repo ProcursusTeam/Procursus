4c9134d717a9909e29cc131d3aa23fa26fbf3bd1
Benjamin G <randomblock1@pm.me>
2021-04-19 15:15:06 -0500

    Update nethack with proper sysconf + use games directory correctly

--- nethack.mk
+++ nethack.mk
@@ -11,10 +11,10 @@ nethack-setup: setup
 	rm -Rf $(BUILD_WORK)/nethack
 	$(call GITHUB_ARCHIVE,NetHack,NetHack,$(NETHACK_COMMIT),$(NETHACK_COMMIT))
 	$(call EXTRACT_TAR,NetHack-$(NETHACK_COMMIT).tar.gz,NetHack-$(NETHACK_COMMIT),nethack)
-	mkdir -p $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	sed -i 's/WINTTYLIB=-lncurses -ltinfo/WINTTYLIB=-lncurses/' $(BUILD_WORK)/nethack/sys/unix/hints/linux
 	sed -i 's,PREFIX=.*/nh/install,'"PREFIX=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"',' $(BUILD_WORK)/nethack/sys/unix/hints/linux
 	sed -i 's,INSTDIR=.*,'"INSTDIR=$(BUILD_WORK)/nethack/BUILD"',' $(BUILD_WORK)/nethack/sys/unix/hints/linux
+	sed -i 's,HACKDIR=.*/games/lib/.*,HACKDIR=$(MEMO_PREFIX)/var/games/nethack,' $(BUILD_WORK)/nethack/sys/unix/hints/linux
 
 ifneq ($(wildcard $(BUILD_WORK)/nethack/.build_complete),)
 nethack:
@@ -32,19 +32,25 @@ nethack: nethack-setup ncurses
 		$(MAKE)
 	cd $(BUILD_WORK)/nethack && \
 		$(MAKE) install
-	cp -a $(BUILD_WORK)/nethack/BUILD/* $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	mkdir -p $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/games
+	mkdir -p $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/
+	sed -i '125,126 {s/^/#/}' $(BUILD_WORK)/nethack/BUILD/sysconf
+	cp -a $(BUILD_WORK)/nethack/BUILD/nethack $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/games/
+	cp -a $(BUILD_WORK)/nethack/BUILD/recover $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/games/
+	cp -a $(BUILD_WORK)/nethack/BUILD/license $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/
+	cp -a $(BUILD_WORK)/nethack/BUILD/nhdat $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/
+	cp -a $(BUILD_WORK)/nethack/BUILD/symbols $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/
+	cp -a $(BUILD_WORK)/nethack/BUILD/sysconf $(BUILD_STAGE)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/
 	touch $(BUILD_WORK)/nethack/.build_complete
 endif
 
 nethack-package: nethack-stage
 	# nethack.mk Package Structure
 	rm -rf $(BUILD_DIST)/nethack
-	mkdir -p $(BUILD_DIST)/nethack/$(MEMO_PREFIX)$(MEMO_SUBPREFIX)/games/lib/nethackdir
-	touch $(BUILD_DIST)/nethack/$(MEMO_PREFIX)$(MEMO_SUBPREFIX)/games/lib/nethackdir/sysconf
-	touch $(BUILD_DIST)/nethack/$(MEMO_PREFIX)$(MEMO_SUBPREFIX)/games/lib/nethackdir/perm
 
 	# nethack.mk Prep nethack
 	cp -a $(BUILD_STAGE)/nethack $(BUILD_DIST)/
+	touch $(BUILD_DIST)/nethack/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/games/nethack/perm
 
 	# nethack.mk Sign
 	$(call SIGN,nethack,general.xml)
