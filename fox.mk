5e8921cd066613bcedd5c1d7793e1e39da719a15
Hayden <me@diatr.us>
2021-04-20 14:52:25 -0400

    fixes

--- fox.mk
+++ fox.mk
@@ -15,9 +15,10 @@ ifneq ($(wildcard $(BUILD_WORK)/fox/.build_complete),)
 fox:
 	@echo "Using previously built fox."
 else
-fox: fox-setup slang2 glib2.0 gettext libxft mesa libx11
+fox: fox-setup libxft mesa libx11 libxcursor libxext libxrender libxrandr libxfixes libpng16 libtiff libjpeg-turbo libxi freetype fontconfig
 	cd $(BUILD_WORK)/fox && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
+		--enable-release \
 		--with-x \
 		--with-xft \
 		--with-xshm \
@@ -25,11 +26,12 @@ fox: fox-setup slang2 glib2.0 gettext libxft mesa libx11
 		--with-xcursor \
 		--with-xrender \
 		--with-xrandr \
-		--with-opengl=no \
+		--with-opengl=yes \
 		--with-xim \
 		--with-xinput \
 		--with-xfixes
-	+$(MAKE) -C $(BUILD_WORK)/fox
+	+$(MAKE) -C $(BUILD_WORK)/fox \
+		CXXFLAGS+=\ -I$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/freetype2
 	+$(MAKE) -C $(BUILD_WORK)/fox install \
  		DESTDIR=$(BUILD_STAGE)/fox
 	+$(MAKE) -C $(BUILD_WORK)/fox install \
@@ -38,7 +40,7 @@ fox: fox-setup slang2 glib2.0 gettext libxft mesa libx11
 endif
 
 fox-package: fox-stage
- # fox.mk Package Structure
+	# fox.mk Package Structure
 	rm -rf $(BUILD_DIST)/libfox-1.6-0 $(BUILD_DIST)/libfox-1.6-dev
 	mkdir -p $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,bin}
 	mkdir -p $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib/pkgconfig,include/fox-1.6,share,bin}
@@ -51,27 +53,25 @@ fox-package: fox-stage
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/PathFinder $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libCHART-1.6.0.dylib $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libFOX-1.6.0.dylib $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libCHART-1.6.dylib $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libFOX-1.6.dylib $(BUILD_DIST)/libfox-1.6-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
-	# libxcb-wm.mk Prep libfox-1.6-dev
+	# fox.mk Prep libfox-1.6-dev
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/fox-config $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/reswrap $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libCHART-1.6.a $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libFOX-1.6.a $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libCHART-1.6.{a,dylib} $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libFOX-1.6.{a,dylib} $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/fox-1.6 $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/fox-1.6
 	cp -a $(BUILD_STAGE)/fox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share $(BUILD_DIST)/libfox-1.6-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
 
-	# libxcb.mk Sign
+	# fox.mk Sign
 	$(call SIGN,libfox-1.6-0,general.xml)
 	$(call SIGN,libfox-1.6-dev,general.xml)
 
-	# libxcb-wm.mk Make .debs
+	# fox.mk Make .debs
 	$(call PACK,libfox-1.6-0,DEB_FOX_V)
 	$(call PACK,libfox-1.6-dev,DEB_FOX_V)
 
-	# libxcb-wm.mk Build cleanup
+	# fox.mk Build cleanup
 	rm -rf $(BUILD_DIST)/libfox-1.6-0 $(BUILD_DIST)/libfox-1.6-dev
 
 .PHONY: fox fox-package
