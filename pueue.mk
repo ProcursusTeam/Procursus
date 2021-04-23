928e3332f789d7e54b5a1770e949784db46ee5f2
TheRealKeto <therealketo@gmail.com>
2021-04-22 21:55:01 -0400

    Add pueue
    Manage your shell commands!

---
+++ pueue.mk
@@ -0,0 +1,40 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS   += pueue
+PUEUE_VERSION := 0.12.2
+DEB_PUEUE_V   ?= $(PUEUE_VERSION)
+
+pueue-setup: setup
+	$(call GITHUB_ARCHIVE,Nukerson,pueue,$(PUEUE_VERSION),v$(PUEUE_VERSION))
+	$(call EXTRACT_TAR,pueue-$(PUEUE_VERSION).tar.gz,pueue-$(PUEUE_VERSION),pueue)
+
+ifneq ($(wildcard $(BUILD_WORK)/pueue/.build_complete),)
+pueue:
+	@echo "Using previously built pueue."
+else
+pueue: pueue-setup
+	cd $(BUILD_WORK)/pueue && SDKROOT=$(TARGET_SYSROOT) cargo \
+		build \
+		--release \
+		--target=$(RUST_TARGET)
+	$(GINSTALL) -Dm755 $(BUILD_WORK)/bat/target/$(RUST_TARGET)/release/pueue \
+		$(BUILD_STAGE)/bat/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/bat
+	touch $(BUILD_WORK)/pueue/.build_complete
+endif
+
+pueue-package: pueue-stage
+	# pueue.mk Prep pueue
+	cp -a $(BUILD_STAGE)/pueue $(BUILD_DIST)
+
+	# pueue.mk Sign
+	$(call SIGN,pueue,general.xml)
+
+	# pueue.mk Make .debs
+	$(call PACK,pueue,DEB_PUEUE_V)
+
+	# pueue.mk Build cleanup
+	rm -rf $(BUILD_DIST)/pueue
+
+.PHONY: pueue pueue-package
