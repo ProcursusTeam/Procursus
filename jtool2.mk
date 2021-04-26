0d1b005ec4461944a992fe84e72ae91bf4370d54
Hayden <pinseeker75@outlook.com>
2021-04-26 18:43:49 -0400

    Untested thing

--- jtool2.mk
+++ jtool2.mk
@@ -2,7 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS  += jtool2
+SUBPROJECTS    += jtool2
 JTOOL2_VERSION := 2020.12.21
 DEB_JTOOL2_V   ?= $(JTOOL2_VERSION)
 
@@ -19,7 +19,11 @@ jtool2: jtool2-setup
 	[[ $$(echo $(MEMO_TARGET) | cut -d- -f2) = "amd64" ]] && LIPO_OUTPUT_TARGET="x86_64" || LIPO_OUTPUT_TARGET="arm64"; \
 	mkdir -p $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin; \
 	$(LIPO) $(BUILD_WORK)/jtool2-bin/jtool2 -thin $$LIPO_OUTPUT_TARGET -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/jtool2; \
-	$(LIPO) $(BUILD_WORK)/jtool2-bin/disarm -thin $$LIPO_OUTPUT_TARGET -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/disarm
+	$(LIPO) $(BUILD_WORK)/jtool2-bin/disarm -thin $$LIPO_OUTPUT_TARGET -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/disarm; \
+	if [ LIPO_OUTPUT_TARGET = "arm64" ] && \
+		vtool -arch arm64 -remove-build-version ios -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/jtool2 $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/jtool2 && \
+		vtool -arch arm64 -remove-build-version ios -output $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/disarm $(BUILD_STAGE)/jtool2/$(MEMO_PREFIX)/$(MEMO_SUB_PREFIX)/bin/disarm
+	touch $(BUILD_WORK)/jtool2/.build_complete
 endif
 
 jtool2-package: jtool2-stage
