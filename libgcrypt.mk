35115a80c6a9398cbea5fe4aa129c15230b5cb2c
Rolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-04-27 04:00:47 +0000

    Updated GNUPG and libgcrypt to latest. (#680)
    * Updated GNUPG and libgcrypt to latest.
    
    * Small mistake.
    
    * fixed formating.
    
    * Added jent support back into iOS since it compiles fine.
    
    * :////////
    
    * Fix
    
    Co-authored-by: Hayden <me@diatr.us>

--- libgcrypt.mk
+++ libgcrypt.mk
@@ -3,13 +3,14 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS     += libgcrypt
-LIBGCRYPT_VERSION := 1.9.2
+LIBGCRYPT_VERSION := 1.9.3
 DEB_LIBGCRYPT_V   ?= $(LIBGCRYPT_VERSION)
 
 libgcrypt-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2{,.sig}
 	$(call PGP_VERIFY,libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2)
 	$(call EXTRACT_TAR,libgcrypt-$(LIBGCRYPT_VERSION).tar.bz2,libgcrypt-$(LIBGCRYPT_VERSION),libgcrypt)
+	$(call DO_PATCH,libgcrypt,libgcrypt,-p1)
 
 ifneq ($(wildcard $(BUILD_WORK)/libgcrypt/.build_complete),)
 libgcrypt:
