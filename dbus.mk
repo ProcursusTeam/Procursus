cf94b81279b57a8ff8bec739c6bc2b25e4b8a469
Hayden <me@diatr.us>
2021-04-26 23:40:38 -0400

    make this actually work :/

--- dbus.mk
+++ dbus.mk
@@ -2,31 +2,33 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS   += dbus
+SUBPROJECTS  += dbus
 DBUS_VERSION := 1.12.20
 DEB_DBUS_V   ?= $(DBUS_VERSION)
 
 dbus-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://dbus.freedesktop.org/releases/dbus/dbus-1.12.20.tar.gz
+	wget -q -nc -P $(BUILD_SOURCE) https://dbus.freedesktop.org/releases/dbus/dbus-$(DBUS_VERSION).tar.gz
 	$(call EXTRACT_TAR,dbus-$(DBUS_VERSION).tar.gz,dbus-$(DBUS_VERSION),dbus)
 
 ifneq ($(wildcard $(BUILD_WORK)/dbus/.build_complete),)
 dbus:
 	@echo "Using previously built dbus."
 else
-dbus: dbus-setup libx11 libxau libxmu xorgproto
+dbus: dbus-setup expat glib2.0 libx11 libsm libice
 	cd $(BUILD_WORK)/dbus && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
         --enable-user-session \
         --disable-doxygen-docs \
         --disable-xml-docs \
-        --disable-static \
-        --with-systemduserunitdir=no \
-        --with-systemdsystemunitdir=no \
-        --docdir=/usr/share/doc/dbus-1.12.20 \
-        --with-console-auth-dir=/var/run/console \
-        --with-system-pid-file=/var/run/dbus/pid \
-        --with-system-socket=/var/run/dbus/system_bus_socket
+		--enable-launchd \
+		--with-launchd-agent-dir=$(MEMO_PREFIX)/Library/LaunchDaemons \
+        --with-console-auth-dir=$(MEMO_PREFIX)/var/run/console \
+        --with-system-pid-file=$(MEMO_PREFIX)/var/run/dbus/pid \
+        --with-system-socket=$(MEMO_PREFIX)/var/run/dbus/system_bus_socket \
+		EXPAT_CFLAGS="-I$(TARGET_SYSROOT)/usr/include" \
+		EXPAT_LIBS="-L$(TARGET_SYSROOT)/usr/lib -lexpat" \
+		PKG_CONFIG="pkg-config --define-prefix"
+#		--disable-tests
 	+$(MAKE) -C $(BUILD_WORK)/dbus
 	+$(MAKE) -C $(BUILD_WORK)/dbus install \
 		DESTDIR=$(BUILD_STAGE)/dbus
