56831b4fe0f3a8ff838c55fb8de83c8b7974ed0e
sudhip <sudhip@nashi.us>
2021-04-29 09:40:44 -0500

    Fix manpages (#691)
    * fix manpages again
    
    * update version

--- libsystem-man.mk
+++ libsystem-man.mk
@@ -5,7 +5,7 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 SUBPROJECTS += libsystem-man
-LIBSYSTEM-MAN_VERSION  := 1.0
+LIBSYSTEM-MAN_VERSION  := 1.1
 DEB_LIBSYSTEM-MAN_V    ?= $(LIBSYSTEM-MAN_VERSION)
 
 libsystem-man-setup: setup
@@ -25,7 +25,6 @@ libsystem-man:
 else
 libsystem-man: libsystem-man-setup
 	mkdir -p $(BUILD_STAGE)/libsystem-man/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/
-	zstd --rm --exclude-compressed -r $(BUILD_WORK)/libsystem-man
 	find $(BUILD_WORK)/libsystem-man -type l -exec sh -c 'd=$$(readlink {}); unlink {}; ln -s "$$d.zst" {}' \;
 	$(CP) -a $(BUILD_WORK)/libsystem-man $(BUILD_STAGE)/libsystem-man/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man
 	touch $(BUILD_WORK)/libsystem-man/.build_complete
