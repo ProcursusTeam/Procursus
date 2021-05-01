09cca5e33c27050a17cefd2f538f29a12c5a0d4b
Ahmed Hassan <62548586+demhademha@users.noreply.github.com>
2021-05-01 10:20:42 +0100

    Add more debs

--- liblouis.mk
+++ liblouis.mk
@@ -27,24 +27,31 @@ liblouis: liblouis-setup libyaml
 endif
 liblouis-package: liblouis-stage
 	# liblouis.mk Package Structure
-	rm -rf $(BUILD_DIST)/liblouis{20,-dev}
+	rm -rf $(BUILD_DIST)/liblouis{20,-dev,-data,-bin}
 	mkdir -p $(BUILD_DIST)/liblouis{20,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-		
-	# liblouis.mk Prep liblouis20
+			mkdir -p $(BUILD_DIST)/liblouis-bin/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+				mkdir -p $(BUILD_DIST)/liblouis-data/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/tables
+
+				# liblouis.mk Prep liblouis20
 	cp -a $(BUILD_STAGE)/liblouis/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/liblouis.20.dylib $(BUILD_DIST)/liblouis20/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	# liblouis.mk Prep liblouis-dev
 	cp -a $(BUILD_STAGE)/liblouis/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/!(liblouis.20.dylib) $(BUILD_DIST)/liblouis-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/liblouis/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/liblouis-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-
+	# liblouis.mk Prep liblouis-bin
+	cp -a $(BUILD_STAGE)/liblouis/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/* $(BUILD_DIST)/liblouis-bin/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	
+				# liblouis.mk Prep liblouis-data
+	cp -a $(BUILD_STAGE)/liblouis/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/tables $(BUILD_DIST)/liblouis-data/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/tables
 	# liblouis.mk Sign
 	$(call SIGN,liblouis20,general.xml)
 	
 	# liblouis.mk Make .debs
 	$(call PACK,liblouis20,DEB_LIBLOUIS_V)
 	$(call PACK,liblouis-dev,DEB_LIBLOUIS_V)
-	
-	# liblouis.mk Build cleanup
+		$(call PACK,liblouis-bin,DEB_LIBLOUIS_V)
+			$(call PACK,liblouis-data,DEB_LIBLOUIS_V)
+			# liblouis.mk Build cleanup
 	rm -rf $(BUILD_DIST)/liblouis{20,-dev}
 
-.PHONY: liblouis liblouis-package
+	.PHONY: liblouis liblouis-package
