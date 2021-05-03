e0dcc37477847f6f0b5394d2280f6fb7e8e88633
Sudhip Nashi <sudhip@nashi.us>
2021-05-03 17:18:21 -0500

    split packages

--- libiosexec.mk
+++ libiosexec.mk
@@ -20,12 +20,13 @@ else
 libiosexec: libiosexec-setup
 	mkdir -p $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	$(MAKE) -C $(BUILD_WORK)/libiosexec
+	$(MAKE) -C $(BUILD_WORK)/libiosexec static
 
 	$(CP) -a $(BUILD_WORK)/libiosexec/libiosexec.$(SONAME).dylib $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	$(LN) -s $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.$(SONAME).dylib $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.dylib 
+	$(CP) -a $(BUILD_WORK)/libiosexec/libiosexec.a $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	$(CP) -a $(BUILD_WORK)/libiosexec/libiosexec.$(SONAME).dylib $(BUILD_BASE)/usr/lib/
-	$(LN) -s $(BUILD_BASE)/usr/lib/libiosexec.$(SONAME).dylib $(BUILD_BASE)/usr/lib/libiosexec.dylib
+	$(LN) -sf $(BUILD_BASE)/usr/lib/libiosexec.$(SONAME).dylib $(BUILD_BASE)/usr/lib/libiosexec.dylib
 
 	touch $(BUILD_WORK)/libiosexec/.build_complete
 endif
@@ -34,11 +35,21 @@ libiosexec-package: libiosexec-stage
 	# libiosexec.mk Package Structure
 	rm -rf $(BUILD_DIST)/libiosexec
 
-	# libiosexec.mk Prep libiosexec
-	cp -a $(BUILD_STAGE)/libiosexec $(BUILD_DIST)
+	# libiosexec1 Prep libiosexec
+	mkdir -p $(BUILD_DIST)/libiosexec1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
+	cp -a $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.$(SONAME).dylib $(BUILD_DIST)/libiosexec1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
+
+	# libiosexec-dev Prep libiosexec-dev
+	mkdir -p $(BUILD_DIST)/libiosexec-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
+	$(LN) -sf $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.$(SONAME).dylib $(BUILD_DIST)/libiosexec-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.dylib 
+	$(CP) $(BUILD_STAGE)/libiosexec/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libiosexec.a $(BUILD_DIST)/libiosexec-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
+
+	# libiosexec-1 sign
+	$(call SIGN,libiosexec1,general.xml)
 
 	# libiosexec.mk Make .debs
-	$(call PACK,libiosexec,DEB_LIBIOSEXEC_V)
+	$(call PACK,libiosexec1,DEB_LIBIOSEXEC_V)
+	$(call PACK,libiosexec-dev,DEB_LIBIOSEXEC_V)
 
 	# libiosexec.mk Build cleanup
 	rm -rf $(BUILD_DIST)/libiosexec
