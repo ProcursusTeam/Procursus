f4bc7d42dae6c31a1442a1b98eb2dd412801fc0a
Hayden <me@diatr.us>
2021-05-03 22:44:13 -0400

    Update libiosexec

--- libiosexec.mk
+++ libiosexec.mk
@@ -5,7 +5,7 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS      += libiosexec
-LIBIOSEXEC_VERSION := 1.0.6
+LIBIOSEXEC_VERSION := 1.0.7
 LIBIOSEXEC_SOVER   := 1
 DEB_LIBIOSEXEC_V   ?= $(LIBIOSEXEC_VERSION)
 
