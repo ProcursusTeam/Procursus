8011621dd46567e7fd072eeb78374eeb494f25f0
Hayden <pinseeker75@outlook.com>
2021-05-06 20:04:31 -0400

    Fix libiosexec on Linux

--- libiosexec.mk
+++ libiosexec.mk
@@ -5,7 +5,7 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS      += libiosexec
-LIBIOSEXEC_VERSION := 1.0.14
+LIBIOSEXEC_VERSION := 1.0.15
 LIBIOSEXEC_SOVER   := 1
 DEB_LIBIOSEXEC_V   ?= $(LIBIOSEXEC_VERSION)
 
