10fdad1d265ea21f0baa56817f49d0d421fc4ff5
Hayden <pinseeker75@outlook.com>
2021-05-09 20:30:13 -0400

    Update lrzip-next.mk

--- lrzip-next.mk
+++ lrzip-next.mk
@@ -1,7 +1,7 @@
 ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
-SUBPROJECTS   += lrzip-next
+SUBPROJECTS        += lrzip-next
 LRZIP_NEXT_VERSION := 0.7.50
 DEB_LRZIP_NEXT_V   ?= $(LRZIP_NEXT_VERSION)
 lrzip-next-setup: setup
@@ -12,7 +12,7 @@ ifneq ($(wildcard $(BUILD_WORK)/lrzip-next/.build_complete),)
 lrzip-next:
 	@echo "Using previously built lrzip-next."
 else
-lrzip-next: lrzip-next-setup bzip2 lz4 liblzo2
+lrzip-next: lrzip-next-setup lz4 liblzo2
 	cd $(BUILD_WORK)/lrzip-next && ./autogen.sh && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/lrzip-next
@@ -23,13 +23,17 @@ endif
 lrzip-next-package: lrzip-next-stage
         # lrzip-next.mk Package Structure
 	rm -rf $(BUILD_DIST)/lrzip-next
-	mkdir -p $(BUILD_DIST)/lrzip-next
+
 	# lrzip-next.mk Prep lrzip-next
 	cp -a $(BUILD_STAGE)/lrzip-next $(BUILD_DIST)
+
 	# lrzip-next.mk Sign
 	$(call SIGN,lrzip-next,general.xml)
+
 	# lrzip-next.mk Make .debs
 	$(call PACK,lrzip-next,DEB_LRZIP_NEXT_V)
+
 	# lrzip-next.mk Build cleanup
-rm -rf $(BUILD_DIST)/lrzip-next
-		.PHONY: lrzip-next lrzip-next-package
+	rm -rf $(BUILD_DIST)/lrzip-next
+
+.PHONY: lrzip-next lrzip-next-package
