3a7b7a931fcc18e806e0b80c358dcde42b60632a
Hayden <pinseeker75@outlook.com>
2021-05-09 20:14:34 -0400

    Update mawk.mk

--- mawk.mk
+++ mawk.mk
@@ -1,13 +1,13 @@
 ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
+
 SUBPROJECTS   += mawk
-MAWK_VERSION := 1.3.4
-MAWK_COMMIT := 20200120
+MAWK_VERSION := 1.3.4.20200120
 DEB_MAWK_V ?= $(MAWK_VERSION)
 
 mawk-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) http://deb.debian.org/debian/pool/main/m/mawk/mawk_$(MAWK_VERSION).$(MAWK_COMMIT).orig.tar.gz
+	wget -q -nc -P $(BUILD_SOURCE) http://deb.debian.org/debian/pool/main/m/mawk/mawk_$(MAWK_VERSION).orig.tar.gz
 	$(call EXTRACT_TAR,mawk_$(MAWK_VERSION).$(MAWK_COMMIT).orig.tar.gz,mawk-$(MAWK_VERSION)-$(MAWK_COMMIT),mawk)
 
 ifneq ($(wildcard $(BUILD_WORK)/mawk/.build_complete),)
@@ -27,7 +27,6 @@ mawk-package: mawk-stage
 	
 	# mawk.mk Package Structure
 	rm -rf $(BUILD_DIST)/mawk
-	mkdir -p $(BUILD_DIST)/mawk
 	
 	# mawk.mk Prep mawk
 	cp -a $(BUILD_STAGE)/mawk $(BUILD_DIST)
