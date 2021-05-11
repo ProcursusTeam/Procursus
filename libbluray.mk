2a91c55903082fb1c890b8cec852eec13ab5a12c
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-05-11 21:36:29 +0800

    libbluray

---
+++ libbluray.mk
@@ -0,0 +1,61 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS       += libbluray
+LIBBLURAY_VERSION := 1.3.0
+DEB_LIBBLURAY_V   ?= $(LIBBLURAY_VERSION)
+
+libbluray-setup: setup
+	wget -q -nc -P $(BUILD_SOURCE) https://download.videolan.org/pub/videolan/libbluray/$(LIBBLURAY_VERSION)/libbluray-$(LIBBLURAY_VERSION).tar.bz2
+	$(call EXTRACT_TAR,libbluray-$(LIBBLURAY_VERSION).tar.bz2,libbluray-$(LIBBLURAY_VERSION),libbluray)
+ifeq (,$(findstring darwin,$(MEMO_TARGET)))
+	mkdir -p $(BUILD_BASE)$(MEMO_PREFIX)/System/Library/Frameworks
+	$(CP) -a $(MACOSX_SYSROOT)/System/Library/Frameworks/DiskArbitration.framework $(BUILD_BASE)$(MEMO_PREFIX)/System/Library/Frameworks
+endif
+	$(call DO_PATCH,libbluray,libbluray,-p1)
+ifneq ($(wildcard $(BUILD_WORK)/libbluray/.build_complete),)
+libbluray:
+	@echo "Using previously built libbluray."
+else
+libbluray: fontconfig expat libbluray-setup
+	cd $(BUILD_WORK)/libbluray && ./bootstrap && CFLAGS="$(CFLAGS) -F$(BUILD_BASE)$(MEMO_PREFIX)/System/Library/Frameworks" ./configure -C \
+		$(DEFAULT_CONFIGURE_FLAGS)
+	+$(MAKE) -C $(BUILD_WORK)/libbluray
+	+$(MAKE) -C $(BUILD_WORK)/libbluray install \
+		DESTDIR=$(BUILD_STAGE)/libbluray
+	+$(MAKE) -C $(BUILD_WORK)/libbluray install \
+		DESTDIR=$(BUILD_BASE)
+
+	touch $(BUILD_WORK)/libbluray/.build_complete
+endif
+
+libbluray-package: libbluray-stage
+	# libbluray.mk Package Structure
+	rm -rf $(BUILD_DIST)/libbluray{2,-dev}
+	mkdir -p $(BUILD_DIST)/libbluray{2,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	mkdir -p $(BUILD_DIST)/libbluray2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc
+	
+	# libbluray.mk Prep libbluray2
+	cp -a $(BUILD_STAGE)/libbluray2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libbluray.8.dylib $(BUILD_DIST)/libbluray/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libbluray2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc/libbluray $(BUILD_DIST)/libbluray/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc/libbluray
+	
+	# libbluray.mk Prep libbluray-dev
+	cp -a $(BUILD_STAGE)/libbluray/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libbluray-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/libbluray/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{libbluray.{dylib,la,a},pkgconfig} $(BUILD_DIST)/libbluray-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	
+	# libbluray.mk Prep libbluray-doc
+	# libbluray.mk Prep libbluray-bin
+	# libbluray.mk Prep libbluray-bdj
+
+	# libbluray.mk Sign
+	$(call SIGN,libbluray2,general.xml)
+	
+	# libbluray.mk Make .debs
+	$(call PACK,libbluray2,DEB_LIBBLURAY_V)
+	$(call PACK,libbluray-dev,DEB_LIBBLURAY_V)
+	
+	# libbluray.mk Build cleanup
+	rm -rf $(BUILD_DIST)/libbluray{2,-dev}
+
+.PHONY: libbluray libbluray-package
