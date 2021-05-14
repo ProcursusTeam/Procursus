4376968fdb659a908931616dbd28f522fd529b69
Hayden <pinseeker75@outlook.com>
2021-05-14 12:14:03 -0400

    Update lf.mk

--- lf.mk
+++ lf.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS += lf
 LF_VERSION  := r22
-DEB_LF_V    ?= 1.0-$(LF_VERSION)
+DEB_LF_V    ?= 0~$(LF_VERSION)
 
 lf-setup: setup
 	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_VERSION),$(LF_VERSION))
@@ -18,15 +18,18 @@ else
 lf: lf-setup
 	# Compile lf and move binaries
 	cd $(BUILD_WORK)/lf && $(DEFAULT_GOLANG_FLAGS) \
-		go build --ldflags="-s -w" .
+		go build --ldflags="-s -w -X main.gVersion=$(LF_VERSION)" .
 	cp -a $(BUILD_WORK)/lf/lf $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_WORK)/lf/lf.1 $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
+	
+###	Another time, look into packaging the rest of the stuff the AUR does.
+###	https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=lf
 	touch $(BUILD_WORK)/lf/.build_complete
 endif
 
 lf-package: lf-stage
 	# lf.mk Package Structure
-	mkdir -p $(BUILD_DIST)/lf
+	rm -rf $(BUILD_DIST)/lf
 	cp -a $(BUILD_STAGE)/lf $(BUILD_DIST)
 
 	# lf.mk Sign
