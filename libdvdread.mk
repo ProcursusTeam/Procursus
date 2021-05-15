ded8c30505f6ef452063a875a23549cbbd3aa910
Hayden <pinseeker75@outlook.com>
2021-05-14 21:06:02 -0400

    Update libdvdread.mk

--- libdvdread.mk
+++ libdvdread.mk
@@ -30,12 +30,10 @@ libdvdread-package: libdvdread-stage
 	# libdvdread.mk Package Structure
 	rm -rf $(BUILD_DIST)/libdvdread{4-dev}
 	mkdir -p $(BUILD_DIST)/libdvdread{4,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	mkdir -p $(BUILD_DIST)/libdvdread4/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc
 	
 	# libdvdread.mk Prep libdvdread4
 	cp -a $(BUILD_STAGE)/libdvdread/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libdvdread.8.dylib $(BUILD_DIST)/libdvdread4/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libdvdread/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc/libdvdread $(BUILD_DIST)/libdvdread4/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/doc/libdvdread4
-	
+
 	# libdvdread.mk Prep libdvdread-dev
 	cp -a $(BUILD_STAGE)/libdvdread/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libdvdread-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	cp -a $(BUILD_STAGE)/libdvdread/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{libdvdread.{dylib,la,a},pkgconfig} $(BUILD_DIST)/libdvdread-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
