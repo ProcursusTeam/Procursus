20da3290539b320bc907f1c8892f4fe745d36b18
Clyde <79387074+clyde37@users.noreply.github.com>
2021-05-18 23:13:18 -0400

    add allahcalculator (lite version)
    This is the lite version that does not require tons of space and processing power

---
+++ allahcalc.mk
@@ -0,0 +1,26 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS += allahcalc
+ALLAHCALC_VERSION := 1.1
+DEB_ALLAHCALC_V ?= $(ALLAHCALC_VERSION)
+
+allahcalc-setup: setup
+	wget -q -nc -P (BUILD_SOURCE) https://github.com/clyde37/allah-calculator/releases/download/$(ALLAHCALC_VERSION)/allah_calculator_generator.cpp
+
+ifneq ($(wildcard $(BUILD_WORK)/allahcalc/.build_complete),)
+allahcalc:
+	@echo "Using previously built allahcalc."
+else
+allahcalc: allahcalc-setup
+    gcc++ allah_calculator_generator.cpp -o allahcalc/allah_calculator_generator
+    ./allah_calculator_generator
+    gcc allah_calculator_final.mp4_custom.c -o allahcalc/allahcalc
+endif
+
+allahcalc-package: allahcalc-stage
+    $(call PACK,allahcalc,DEB_ALLAHCALC_V)
+    rm -rf allahcalc
+
+.PHONY: allahcalc allahcalc-package
