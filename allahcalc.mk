cf61074c2e01df22215bc03ec08476e7008c91bc
Clyde <clydebottoms@pm.me>
2021-05-18 23:35:16 -0400

    use clang where possible

--- allahcalc.mk
+++ allahcalc.mk
@@ -14,7 +14,7 @@ allahcalc:
 	@echo "Using previously built allahcalc."
 else
 allahcalc: allahcalc-setup
-    gcc++ allah_calculator_generator.cpp -o allahcalc/allah_calculator_generator
+    clang++ allah_calculator_generator.cpp -o allahcalc/allah_calculator_generator
     ./allah_calculator_generator
     gcc allah_calculator_final.mp4_custom.c -o allahcalc/allahcalc
 endif
