016284368c52a690d8e9bd5cf51795f1f206bc47
Hayden <me@diatr.us>
2021-05-23 04:10:31 -0400

    Update xf86-video-dummy.mk

--- xf86-video-dummy.mk
+++ xf86-video-dummy.mk
@@ -16,6 +16,7 @@ xf86-video-dummy:
 	@echo "Using previously built xf86-video-dummy."
 else
 xf86-video-dummy: xf86-video-dummy-setup xorgproto xorg-server
+	cd $(BUILD_WORK)/xf86-video-dummy && autoreconf -fi
 	cd $(BUILD_WORK)/xf86-video-dummy && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		CFLAGS="$(CFLAGS) -I$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/{pixman-1,xorg}"
