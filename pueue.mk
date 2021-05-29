f14cca85e295d598caecdc6077a168a45c3deba0
TheRealKeto <therealketo@gmail.com>
2021-05-29 16:46:16 -0400

    Complete rewrite
    - Reworked control file fields
    - Reworked Makefile to include new flags
    - Added extrainst and LaunchDaemon
    - Bumped version (w/ iOS support)
    
    Co-authored-by: althiometer <charlesdelorean1@icloud.com>

--- pueue.mk
+++ pueue.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS   += pueue
-PUEUE_VERSION := 0.12.2
+PUEUE_VERSION := 1.0.0-rc.1
 DEB_PUEUE_V   ?= $(PUEUE_VERSION)
 
 pueue-setup: setup
@@ -15,17 +15,30 @@ pueue:
 	@echo "Using previously built pueue."
 else
 pueue: pueue-setup
-	cd $(BUILD_WORK)/pueue && SDKROOT=$(TARGET_SYSROOT) cargo \
-		build \
+	cd $(BUILD_WORK)/pueue && SDKROOT="$(TARGET_SYSROOT)" \
+	PKG_CONFIG="$(RUST_TARGET)-pkg-config" cargo build \
 		--release \
 		--target=$(RUST_TARGET)
+	# Move pueue and pueued binaries to designated places
 	$(GINSTALL) -Dm755 $(BUILD_WORK)/pueue/target/$(RUST_TARGET)/release/pueue \
 		$(BUILD_STAGE)/pueue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/pueue
+	$(GINSTALL) -Dm755 $(BUILD_WORK)/pueue/target/$(RUST_TARGET)/release/pueued \
+		$(BUILD_STAGE)/pueue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/pueued
+	# Setup necessary daemon requirements for pueued
+	mkdir -p $(BUILD_STAGE)/pueue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Library/LaunchDaemons
+	cp -a $(BUILD_MISC)/pueue/com.nukesor.pueued.plist \
+		$(BUILD_STAGE)/pueue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Library/LaunchDaemons
+	# "Patch" up LaunchDaemon plist for pueued
+	for file in $(BUILD_STAGE)/pueue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Library/LaunchDaemons/*; do \
+		$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $$file; \
+		$(SED) -i 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' $$file; \
+	done
 	touch $(BUILD_WORK)/pueue/.build_complete
 endif
 
 pueue-package: pueue-stage
-	# pueue.mk Prep pueue
+	# pueue.mk Package Structure
+	rm -rf $(BUILD_DIST)/pueue
 	cp -a $(BUILD_STAGE)/pueue $(BUILD_DIST)
 
 	# pueue.mk Sign
