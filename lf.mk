fa64683a53a2cdf44af1955bf143bc09b2a9ab8b
TheRealKeto <therealketo@gmail.com>
2021-06-05 21:52:27 -0400

    lf: Minor fixes and changes
    - Package more files
    - Display correct version when building
    - Update package to the latest commit hash

--- lf.mk
+++ lf.mk
@@ -3,13 +3,15 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS += lf
-LF_VERSION  := r22
+LF_COMMIT   := eaaf08e42504f5986b919ca4690d876424cee4e9
+LF_VERSION  := r22.$(shell echo $(LF_COMMIT) | cut -c -7)
 DEB_LF_V    ?= 0~$(LF_VERSION)
 
 lf-setup: setup
-	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_VERSION),$(LF_VERSION))
-	$(call EXTRACT_TAR,lf-$(LF_VERSION).tar.gz,lf-$(LF_VERSION),lf)
-	mkdir -p $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/man/man1}
+	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_COMMIT),$(LF_COMMIT))
+	$(call EXTRACT_TAR,lf-$(LF_COMMIT).tar.gz,lf-$(LF_COMMIT),lf)
+	mkdir -p $(BUILD_STAGE)/lf/{etc/profile.d,$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin} \
+		$(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/{lf,man/man1,zsh/site-functions,vim/vimfiles/syntax}
 
 ifneq ($(wildcard $(BUILD_WORK)/lf/.build_complete),)
 lf:
@@ -22,12 +24,16 @@ lf: lf-setup libiosexec
 endif
 	# Compile lf and move binaries
 	cd $(BUILD_WORK)/lf && $(DEFAULT_GOLANG_FLAGS) \
-		go build --ldflags="-s -w -X main.gVersion=$(LF_VERSION)" .
+		go build --ldflags="-s -w -X main.gVersion=$(DEB_LF_V)" .
 	cp -a $(BUILD_WORK)/lf/lf $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_WORK)/lf/lf.1 $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
-	
-###	Another time, look into packaging the rest of the stuff the AUR does.
-###	https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=lf
+	# Copy over other files for zsh, vim, csh, etc
+	cp -a $(BUILD_WORK)/lf/etc/lf.zsh \
+		$(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_lf
+	cp -a $(BUILD_WORK)/lf/etc/lfcd.sh $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/lf
+	cp -a $(BUILD_WORK)/lf/etc/lf.vim \
+		$(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/syntax
+	cp -a $(BUILD_WORK)/lf/etc/{lf.csh,lfcd.csh} $(BUILD_STAGE)/lf/etc/profile.d
 	touch $(BUILD_WORK)/lf/.build_complete
 endif
 
