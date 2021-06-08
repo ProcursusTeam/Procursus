12bab1df43dee7b818ceec2309337e580df0341d
Cameron Katri <me@cameronkatri.com>
2021-06-08 11:18:29 -0400

    Add Gitea

---
+++ gitea.mk
@@ -0,0 +1,50 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS   += gitea
+GITEA_VERSION := 1.14.2
+DEB_GITEA_V   ?= $(GITEA_VERSION)
+
+gitea-setup: setup
+	-[ ! -f "$(BUILD_SOURCE)/gitea-$(GITEA_VERSION).tar.gz" ] && \
+		wget -q -nc -O$(BUILD_SOURCE)/gitea-$(GITEA_VERSION).tar.gz \
+			https://github.com/go-gitea/gitea/releases/download/v$(GITEA_VERSION)/gitea-src-$(GITEA_VERSION).tar.gz
+	-[ ! -f "$(BUILD_WORK)/gitea" ] && \
+		mkdir -p $(BUILD_WORK)/gitea && \
+			tar xf $(BUILD_SOURCE)/gitea-$(GITEA_VERSION).tar.gz -C $(BUILD_WORK)/gitea
+	$(call DO_PATCH,gitea,gitea,-p1)
+	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/Makefile
+
+ifneq ($(wildcard $(BUILD_WORK)/gitea/.build_complete),)
+gitea:
+	@echo "Using previously built gitea."
+else
+gitea: gitea-setup
+	cd $(BUILD_WORK)/gitea && go mod vendor
+	+TAGS="bindata sqlite sqlite_unlock_notify" \
+		$(MAKE) -C $(BUILD_WORK)/gitea build \
+		$(DEFAULT_GOLANG_FLAGS)
+	$(GINSTALL) -Dm755 $(BUILD_WORK)/gitea/gitea $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gitea
+	$(GINSTALL) -Dm644 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/example.ini
+	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/example.ini
+	touch $(BUILD_WORK)/gitea/.build_complete
+endif
+
+gitea-package: gitea-stage
+	# gitea.mk Package Structure
+	rm -rf $(BUILD_DIST)/gitea
+	
+	# gitea.mk Prep gitea
+	cp -a $(BUILD_STAGE)/gitea $(BUILD_DIST)
+	
+	# gitea.mk Sign
+	$(call SIGN,gitea,general.xml)
+	
+	# gitea.mk Make .debs
+	$(call PACK,gitea,DEB_GITEA_V)
+	
+	# gitea.mk Build cleanup
+	rm -rf $(BUILD_DIST)/gitea
+
+.PHONY: gitea gitea-package
