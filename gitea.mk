963631f764384eb19f4b28eeb516e7928c635a75
Cameron Katri <me@cameronkatri.com>
2021-06-09 13:47:13 -0400

    gitea: Use global gitea.ini and add LaunchDaemon

--- gitea.mk
+++ gitea.mk
@@ -15,6 +15,7 @@ gitea-setup: setup
 			tar xf $(BUILD_SOURCE)/gitea-$(GITEA_VERSION).tar.gz -C $(BUILD_WORK)/gitea
 	$(call DO_PATCH,gitea,gitea,-p1)
 	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/Makefile
+	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/custom/conf/app.example.ini
 
 ifneq ($(wildcard $(BUILD_WORK)/gitea/.build_complete),)
 gitea:
@@ -26,8 +27,16 @@ gitea: gitea-setup
 		$(MAKE) -C $(BUILD_WORK)/gitea build \
 		$(DEFAULT_GOLANG_FLAGS)
 	$(GINSTALL) -Dm755 $(BUILD_WORK)/gitea/gitea $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gitea
-	$(GINSTALL) -Dm644 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/example.ini
-	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/example.ini
+	$(GINSTALL) -Dm755 $(BUILD_MISC)/gitea/gitea-wrapper \
+		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/gitea-wrapper
+	$(GINSTALL) -Dm644 $(BUILD_MISC)/gitea/io.gitea.gitea.plist \
+		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/Library/LaunchDaemons/io.gitea.gitea.plist
+	$(SED) -i -e 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' -e 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' \
+		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/gitea-wrapper
+	$(SED) -i -e 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' -e 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' \
+		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/Library/LaunchDaemons/io.gitea.gitea.plist
+	$(GINSTALL) -Dm644 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/app.example.ini
+	$(GINSTALL) -Dm660 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/etc/gitea.ini
 	touch $(BUILD_WORK)/gitea/.build_complete
 endif
 
