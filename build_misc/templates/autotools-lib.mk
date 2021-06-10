dd9f9772cb52d8721c94ae7f291a2469954d7797
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-06-11 04:01:11 +0800

    soversion

--- build_misc/templates/autotools-lib.mk
+++ build_misc/templates/autotools-lib.mk
@@ -31,24 +31,24 @@ endif
 
 @pkg@-package: @pkg@-stage
 	# @pkg@.mk Package Structure
-	rm -rf $(BUILD_DIST)/@pkg@{@SOVER@,-dev}
-	mkdir -p $(BUILD_DIST)/@pkg@{@SOVER@,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	rm -rf $(BUILD_DIST)/@pkg@{$(@PKG@_SOVER),-dev}
+	mkdir -p $(BUILD_DIST)/@pkg@{$(@PKG@_SOVER),-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# @pkg@.mk Prep @pkg@@SOVER@
-	cp -a $(BUILD_STAGE)/@pkg@/$(MEMO_PREIFX)$(MEMO_SUB_PREFIX)/lib/@pkg@.@SOVER@.dylib $(BUILD_DIST)/@pkg@@SOVER@/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/@pkg@/$(MEMO_PREIFX)$(MEMO_SUB_PREFIX)/lib/@pkg@.$(@PKG@_SOVER).dylib $(BUILD_DIST)/@pkg@$(@PKG@_SOVER)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# @pkg@.mk Prep @pkg@-dev
 	cp -a $(BUILD_STAGE)/@pkg@/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/@pkg@-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	cp -a $(BUILD_STAGE)/@pkg@/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{pkgconfig,@pkg@.{dylib,a}} $(BUILD_DIST)/@pkg@-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# @pkg@.mk Sign
-	$(call SIGN,@pkg@@SOVER@,general.xml)
+	$(call SIGN,@pkg@$(@PKG@_SOVER),general.xml)
 	
 	# @pkg@.mk Make .debs
-	$(call PACK,@pkg@@SOVER@,DEB_@PKG@_V)
+	$(call PACK,@pkg@$(@PKG@_SOVER),DEB_@PKG@_V)
 	$(call PACK,@pkg@-dev,DEB_@PKG@_V)
 	
 	# @pkg@.mk Build cleanup
-	rm -rf $(BUILD_DIST)/@pkg@{@SOVER@,-dev}
+	rm -rf $(BUILD_DIST)/@pkg@{$(@PKG@_SOVER),-dev}
 
 .PHONY: @pkg@ @pkg@-package
