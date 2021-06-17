1c508c98db78ee2c53f580039c5e7397cb08ef4a
TheRealKeto <therealketo@gmail.com>
2021-06-16 23:04:23 -0400

    meson: Update to 0.58.1

--- meson.mk
+++ meson.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS   += meson
-MESON_VERSION := 0.58.0
+MESON_VERSION := 0.58.1
 DEB_MESON_V   ?= $(MESON_VERSION)
 
 meson-setup: setup
@@ -14,22 +14,19 @@ ifneq ($(wildcard $(BUILD_WORK)/meson/.build_complete),)
 meson:
 	@echo "Using previously built meson."
 else
-meson: meson-setup python3
-	cd $(BUILD_WORK)/meson && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py \
-		install \
-		--prefix="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
+meson: meson-setup python3 ninja
+	cd $(BUILD_WORK)/meson && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py install \
+		--install-layout=deb \
 		--root="$(BUILD_STAGE)/meson" \
-		--install-layout=deb
+		--prefix="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
 	find $(BUILD_STAGE)/meson -name __pycache__ -prune -exec rm -rf {} \;
 	$(SED) -i "s|#!.*|#!$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/python3|" $(BUILD_STAGE)/meson/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/meson
 	touch $(BUILD_WORK)/meson/.build_complete
 endif
 
 meson-package: meson-stage
-	# meson.mk Build Cleanup
-	rm -rf $(BUILD_DIST)/meson
-
 	# meson.mk Package Structure
+	rm -rf $(BUILD_DIST)/meson
 	cp -a $(BUILD_STAGE)/meson $(BUILD_DIST)
 
 	# meson.mk Sign
