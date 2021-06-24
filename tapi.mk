1d6316a36428c7918d4453f2a79abf9a250cfe28
sunflsks <sudhip@nashi.us>
2021-06-24 01:40:19 -0500

    Add tapi command (#795)
    * add tapi
    
    * Add tapi manpages
    
    * Update tapi.mk
    
    * Update Makefile
    
    * Update tapi.mk
    
    * Update tapi.mk
    
    Co-authored-by: Cameron Katri <me@cameronkatri.com>
    Co-authored-by: Hayden <me@diatr.us>

--- tapi.mk
+++ tapi.mk
@@ -5,7 +5,7 @@ endif
 SUBPROJECTS    += tapi
 TAPI_COMMIT    := 664b8414f89612f2dfd35a9b679c345aa5389026
 TAPI_VERSION   := 1100.0.11
-DEB_TAPI_V     ?= $(TAPI_VERSION)
+DEB_TAPI_V     ?= $(TAPI_VERSION)-1
 
 tapi-setup: setup
 	$(call GITHUB_ARCHIVE,tpoechtrager,apple-libtapi,$(TAPI_COMMIT),$(TAPI_COMMIT),tapi)
@@ -31,6 +31,7 @@ tapi: tapi-setup
 
 	cd $(BUILD_WORK)/tapi/build && cmake . \
 		$(DEFAULT_CMAKE_FLAGS) \
+		-DCMAKE_CXX_FLAGS="$(CXXFLAGS) -I$(BUILD_WORK)/tapi/src/llvm/projects/clang/include -I$(BUILD_WORK)/tapi/build/projects/clang/include" \
 		-DCROSS_TOOLCHAIN_FLAGS_NATIVE='-DCMAKE_C_COMPILER=cc;-DCMAKE_CXX_COMPILER=c++;-DCMAKE_OSX_SYSROOT="$(MACOSX_SYSROOT)";-DCMAKE_OSX_ARCHITECTURES="";-DCMAKE_C_FLAGS="";-DCMAKE_CXX_FLAGS="";-DCMAKE_EXE_LINKER_FLAGS="";-DLLVM_INCLUDE_TESTS=OFF;-DTAPI_INCLUDE_TESTS=OFF' \
 		-DLLVM_ENABLE_PROJECTS="libtapi" \
 		-DLLVM_INCLUDE_TESTS=OFF \
@@ -43,24 +44,35 @@ tapi: tapi-setup
 	+$(MAKE) -C $(BUILD_WORK)/tapi/build libtapi tapi
 	+$(MAKE) -C $(BUILD_WORK)/tapi/build install-libtapi install-tapi-headers install-tapi \
 		DESTDIR="$(BUILD_STAGE)/tapi"
+	mkdir -p $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
+	$(INSTALL) -Dm644 $(BUILD_WORK)/tapi/src/libtapi/docs/man/*.1 $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
 	touch $(BUILD_WORK)/tapi/.build_complete
 endif
 
 tapi-package: tapi-stage
 	# tapi.mk Package Structure
-	rm -rf $(BUILD_DIST)/libtapi
-	mkdir -p $(BUILD_DIST)/libtapi
+	rm -rf $(BUILD_DIST)/tapi $(BUILD_DIST)/libtapi{,-dev}
+	mkdir -p $(BUILD_DIST)/libtapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	mkdir -p $(BUILD_DIST)/libtapi-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	mkdir -p $(BUILD_DIST)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man
 
 	# tapi.mk Prep tapi
-	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/libtapi
+	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libtapi.dylib $(BUILD_DIST)/libtapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libtapi-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
+	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/tapi $(BUILD_DIST)/libtapi-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin $(BUILD_DIST)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
+	cp -a $(BUILD_STAGE)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1 $(BUILD_DIST)/tapi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man
 
 	# tapi.mk Sign
 	$(call SIGN,libtapi,general.xml)
+	$(call SIGN,tapi,general.xml)
 
 	# tapi.mk Make .debs
 	$(call PACK,libtapi,DEB_TAPI_V)
+	$(call PACK,libtapi-dev,DEB_TAPI_V)
+	$(call PACK,tapi,DEB_TAPI_V)
 
 	# tapi.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libtapi
+	rm -rf $(BUILD_DIST)/tapi $(BUILD_DIST)/libtapi{,-dev}
 
 .PHONY: tapi tapi-package
