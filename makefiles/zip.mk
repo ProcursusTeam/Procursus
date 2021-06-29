5455e273fe514f0055b16de3d32a0076546a9c5a
Hayden <me@diatr.us>
2021-06-29 12:34:31 -0400

    Fix p7zip + zip build

--- makefiles/zip.mk
+++ makefiles/zip.mk
@@ -12,6 +12,7 @@ zip-setup: setup
 		https://deb.debian.org/debian/pool/main/z/zip/zip_$(DEBIAN_ZIP_V).debian.tar.xz
 	$(call EXTRACT_TAR,zip_$(ZIP_VERSION).orig.tar.gz,zip30,zip)
 	$(call EXTRACT_TAR,zip_$(DEBIAN_ZIP_V).debian.tar.xz,debian/patches,$(BUILD_PATCH)/zip-$(ZIP_VERSION))
+	rm $(BUILD_PATCH)/zip-$(ZIP_VERSION)/series
 	rm -rf $(BUILD_WORK)/debian
 	$(call DO_PATCH,zip-$(ZIP_VERSION),zip,-p1)
 
