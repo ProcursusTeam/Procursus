6555aa24ff43cf45b8e026ff095992ce1464b7d8
Sudhip Nashi <sudhip@nashi.us>
2021-07-01 16:34:40 -0500

    fix fzf build for non-arm64

--- makefiles/fzf.mk
+++ makefiles/fzf.mk
@@ -9,31 +9,13 @@ DEB_FZF_V    ?= $(FZF_VERSION)
 fzf-setup: setup
 	$(call GITHUB_ARCHIVE,junegunn,fzf,$(FZF_VERSION),$(FZF_VERSION))
 	$(call EXTRACT_TAR,fzf-$(FZF_VERSION).tar.gz,fzf-$(FZF_VERSION),fzf)
-	mkdir -p $(BUILD_STAGE)/fzf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
+	mkdir -p $(BUILD_STAGE)/fzf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX){/share,/bin}
 
-ifneq ($(MEMO_ARCH),arm64)
-fzf:
-	@echo "Unsupported target $(MEMO_TARGET)"
-else ifneq ($(wildcard $(BUILD_WORK)/fzf/.build_complete),)
-fzf:
-	@echo "Using previously built fzf."
-else
 fzf: fzf-setup
-	+$(MAKE) -C $(BUILD_WORK)/fzf bin/fzf \
-		FZF_VERSION="$(FZF_VERSION)" \
-		FZF_REVISION="Procursus" \
-		BINARY="fzf-darwin_arm8" \
-		GOARCH=arm64 \
-		GOOS=darwin \
-		CGO_CFLAGS="$(CFLAGS)" \
-		CGO_CPPFLAGS="$(CPPFLAGS)" \
-		CGO_LDFLAGS="$(LDFLAGS)" \
-		CGO_ENABLED=1 \
-		CC="$(CC)"
-	$(CP) -a $(BUILD_WORK)/fzf/bin $(BUILD_STAGE)/fzf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cd $(BUILD_WORK)/fzf && $(DEFAULT_GOLANG_FLAGS) go build
+	$(INSTALL) -Dm755 $(BUILD_WORK)/fzf/{/fzf,/bin/fzf-tmux} $(BUILD_STAGE)/fzf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 	$(CP) -a $(BUILD_WORK)/fzf/man $(BUILD_STAGE)/fzf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
 	touch $(BUILD_WORK)/fzf/.build_complete
-endif
 
 fzf-package: fzf-stage
 	# fzf.mk Package Structure
