e6d638356fa843d46a2e65f998c0a33c59680aaf
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-07-12 03:05:28 +0100

    Add Extra Line to Control/Makefile and fix indent

--- makefiles/amy-keyring.mk
+++ makefiles/amy-keyring.mk
@@ -2,7 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS           += amy-keyring
+SUBPROJECTS          += amy-keyring
 AMY_KEYRING_VERSION  := 2021.07.10
 DEB_AMY_KEYRING_V    ?= $(AMY_KEYRING_VERSION)
 
