67cb8046fbadb3bd9e53a9bac1be6221729f7cd8
Hayden <me@diatr.us>
2021-07-13 11:40:56 -0400

    Revert "Update gnu-getopt.mk"
    This reverts commit c12a7dc0f362ca220791e9d7daa1e3c8619cc791.

--- makefiles/gnu-getopt.mk
+++ makefiles/gnu-getopt.mk
@@ -18,12 +18,11 @@ gnu-getopt: gnu-getopt-setup
 	cd $(BUILD_WORK)/gnu-getopt && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--disable-silent-rules
-#	Need asciidoctor for manpages
-	+$(MAKE) -C $(BUILD_WORK)/gnu-getopt getopt #misc-utils/getopt.1
+	+$(MAKE) -C $(BUILD_WORK)/gnu-getopt getopt misc-utils/getopt.1
 	mkdir -p $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 	$(INSTALL) -Dm755 $(BUILD_WORK)/gnu-getopt/getopt $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/gnubin/getopt
 	$(LN) -s ../libexec/gnubin/getopt $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/ggetopt
-#	$(INSTALL) -Dm644 $(BUILD_WORK)/gnu-getopt/misc-utils/getopt.1 $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man1/ggetopt.1
+	$(INSTALL) -Dm644 $(BUILD_WORK)/gnu-getopt/misc-utils/getopt.1 $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man1/ggetopt.1
 	$(INSTALL) -Dm644 $(BUILD_WORK)/gnu-getopt/bash-completion/getopt $(BUILD_STAGE)/gnu-getopt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/ggetopt
 	touch $(BUILD_WORK)/gnu-getopt/.build_complete
 endif
