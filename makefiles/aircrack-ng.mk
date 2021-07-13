98f92f574d6048bd998fba0b473a6b1a4b83c96d
Hayden <pinseeker75@outlook.com>
2021-07-13 10:44:14 -0400

    Update aircrack-ng.mk

--- makefiles/aircrack-ng.mk
+++ makefiles/aircrack-ng.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS         += aircrack-ng
 AIRCRACK-NG_COMMIT  := 482f56c5a1433df6b8619f1de6d89d2d5f5ab743
-AIRCRACK-NG_VERSION := 1.6~20211007.$(shell echo $(AIRCRACK_COMMIT) | cut -c -7)
+AIRCRACK-NG_VERSION := 1.6~20211007.$(shell echo $(AIRCRACK-NG_COMMIT) | cut -c -7)
 DEB_AIRCRACK-NG_V   ?= $(AIRCRACK-NG_VERSION)
 
 aircrack-ng-setup: setup
