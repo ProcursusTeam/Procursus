2a6d0b69ad0acb2397aca9dfde2c84d58eafa04e
DaOfficialRolex <62307498+DaOfficialRolex@users.noreply.github.com>
2021-07-18 19:30:08 -0700

    mandoc done.

--- makefiles/mandoc.mk
+++ makefiles/mandoc.mk
@@ -9,20 +9,33 @@ DEB_MANDOC_V   ?= $(MANDOC_VERSION)
 mandoc-setup: setup
 	wget -q -nc -P$(BUILD_SOURCE) https://mandoc.bsd.lv/snapshots/mandoc-$(MANDOC_VERSION).tar.gz
 	$(call EXTRACT_TAR,mandoc-$(MANDOC_VERSION).tar.gz,mandoc-$(MANDOC_VERSION),mandoc)
-	echo -e "#!/bin/sh\n \
-	PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"\n \
-    INCLUDEDIR="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include"\n \
-    LIBDIR="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib"\n \
-    MANDIR="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/man"\n \
-    WWWPREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/www"\n \
-    EXAMPLEDIR="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/examples"\n" > $(BUILD_WORK)/mandoc/configure.local
+	echo -e ""PREFIX=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"\n \
+    "INCLUDEDIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include"\n \
+    "LIBDIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib"\n \
+    "MANDIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/man"\n \
+	"SBINDIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/sbin"\n \
+    "WWWPREFIX=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/var/www"\n \
+    "EXAMPLEDIR=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/examples"\n \
+	"BINM_MAN=mman"\n \
+	"BINM_APROPOS=mapropos"\n \
+	"BINM_WHATIS=mwhatis"\n \
+	"BINM_MAKEWHATIS=makewhatis"\n \
+	"MANM_MAN=man"\n \
+	"MANM_MDOC=mdoc"\n \
+	"MANM_ROFF=mandoc_roff"\n \
+	"MANM_EQN=eqn"\n \
+	"MANM_TBL=tbl"\n \
+	"MANPATH_DEFAULT=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man"\n \
+	"STATIC="\n \
+	"BUILD_CGI=1"\n" > $(BUILD_WORK)/mandoc/configure.local
+	mv $(BUILD_WORK)/mandoc/cgi.h.example $(BUILD_WORK)/mandoc/cgi.h
 
 ifneq ($(wildcard $(BUILD_WORK)/mandoc/.build_complete),)
 mandoc:
 	@echo "Using previously built mandoc."
 else
 mandoc: mandoc-setup
-	cd $(BUILD_WORK)/mandoc && ./configure && ./configure.local -C \
+	cd $(BUILD_WORK)/mandoc && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/mandoc
 	+$(MAKE) -C $(BUILD_WORK)/mandoc install \
@@ -33,6 +46,7 @@ endif
 mandoc-package: mandoc-stage
 	# mandoc.mk Package Structure
 	rm -rf $(BUILD_DIST)/mandoc
+	mkdir -p $(BUILD_DIST)/mandoc/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
 	
 	# mandoc.mk Prep mandoc
 	cp -a $(BUILD_STAGE)/mandoc/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/ $(BUILD_DIST)/mandoc/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
