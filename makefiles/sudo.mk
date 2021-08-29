b5a99e764cecbcf22ccee5be1ea6d6af32c1c7ae
m1stadev <37033149+m1stadev@users.noreply.github.com>
2021-08-29 00:16:58 -0500

    Remove TODO

--- makefiles/sudo.mk
+++ makefiles/sudo.mk
@@ -41,7 +41,7 @@ endif
 		--with-timeout=15 \
 		--with-password-timeout=0 \
 		--with-passprompt="[sudo] password for %p: " \
-		--with-vardir=$(MEMO_PREFIX)/var/db/sudo \ #TODO: Change these to an ifeq for macOS building
+		--with-vardir=$(MEMO_PREFIX)/var/db/sudo \
 		--with-rundir=$(MEMO_PREFIX)/var/run/sudo \
 		sudo_cv___func__=yes \
 		$(SUDO_CONFIGURE_ARGS)
