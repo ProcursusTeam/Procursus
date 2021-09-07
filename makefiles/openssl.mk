d7e45c7ea69676dae76339a4c33c56221949c3ae
Cameron Katri <me@cameronkatri.com>
2021-09-07 14:04:50 -0400

    Remove openssl1.mk, replace libssl1.1 from control files

--- makefiles/openssl.mk
+++ makefiles/openssl.mk
@@ -41,27 +41,27 @@ openssl-package: openssl-stage
 	# openssl.mk Package Structure
 	rm -rf $(BUILD_DIST)/{openssl,libssl{3,-dev}}
 	mkdir -p $(BUILD_DIST)/{openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin,libssl{3,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib}
-
+	
 	# openssl.mk Prep libssl3
 	cp -a $(BUILD_STAGE)/openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{*.3.dylib,engines-3,ossl-modules} $(BUILD_DIST)/libssl3/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
+	
 	# openssl.mk Prep libssl-dev
 	cp -a $(BUILD_STAGE)/openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{lib{ssl,crypto}.{a,dylib},pkgconfig} $(BUILD_DIST)/libssl-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libssl-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-
+	
 	# openssl.mk Prep openssl
 	cp -a $(BUILD_STAGE)/openssl/$(MEMO_PREFIX)/etc $(BUILD_DIST)/openssl/$(MEMO_PREFIX)
 	cp -a $(BUILD_STAGE)/openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/* $(BUILD_DIST)/openssl/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-
+	
 	# openssl.mk Sign
 	$(call SIGN,libssl3,general.xml)
 	$(call SIGN,openssl,general.xml)
-
+	
 	# openssl.mk Make .debs
 	$(call PACK,libssl3,DEB_OPENSSL_V)
 	$(call PACK,libssl-dev,DEB_OPENSSL_V)
 	$(call PACK,openssl,DEB_OPENSSL_V)
-
+	
 	# openssl.mk Build cleanup
 	rm -rf $(BUILD_DIST)/{openssl,libssl{3,-dev}}
 
