41aa6fe8da6d9356ef3e024ef9229c2b46498ad4
Cameron Katri <me@cameronkatri.com>
2021-09-11 16:25:42 -0400

    Use mosh commits, update libprotobuf

--- makefiles/libprotobuf.mk
+++ makefiles/libprotobuf.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS         += libprotobuf
-LIBPROTOBUF_VERSION := 3.14.0
+LIBPROTOBUF_VERSION := 3.17.3
 DEB_LIBPROTOBUF_V   ?= $(LIBPROTOBUF_VERSION)
 
 libprotobuf-setup: setup
@@ -20,55 +20,55 @@ libprotobuf: libprotobuf-setup
 		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/libprotobuf
 	+$(MAKE) -C $(BUILD_WORK)/libprotobuf install \
-		DESTDIR="$(BUILD_BASE)"
+		DESTDIR="$(BUILD_STAGE)/libprotobuf"
 	$(call AFTER_BUILD,copy)
 endif
 
 libprotobuf-package: libprotobuf-stage
 	# libprotobuf.mk Package Structure
-	rm -rf $(BUILD_DIST)/libprotobuf{{,-lite}25,-dev} $(BUILD_DIST)/libprotoc{25,-dev} $(BUILD_DIST)/protobuf-compiler
-	mkdir -p $(BUILD_DIST)/libprotobuf25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
-		$(BUILD_DIST)/libprotobuf-lite25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+	rm -rf $(BUILD_DIST)/libprotobuf{{,-lite}28,-dev} $(BUILD_DIST)/libprotoc{28,-dev} $(BUILD_DIST)/protobuf-compiler
+	mkdir -p $(BUILD_DIST)/libprotobuf28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/libprotobuf-lite28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
 		$(BUILD_DIST)/libprotobuf-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include/google/protobuf,lib} \
-		$(BUILD_DIST)/libprotoc25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/libprotoc28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
 		$(BUILD_DIST)/libprotoc-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include/google/protobuf,lib} \
 		$(BUILD_DIST)/protobuf-compiler/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-
-	# libprotobuf.mk Prep libprotobuf25
-	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotobuf.25.dylib $(BUILD_DIST)/libprotobuf25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
-	# libprotobuf.mk Prep libprotobuf-lite25
-	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotobuf-lite.25.dylib $(BUILD_DIST)/libprotobuf-lite25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
+	
+	# libprotobuf.mk Prep libprotobuf28
+	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotobuf.28.dylib $(BUILD_DIST)/libprotobuf28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	
+	# libprotobuf.mk Prep libprotobuf-lite28
+	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotobuf-lite.28.dylib $(BUILD_DIST)/libprotobuf-lite28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	
 	# libprotobuf.mk Prep libprotobuf-dev
 	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{pkgconfig,libprotobuf.{a,dylib}} $(BUILD_DIST)/libprotobuf-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/google/protobuf/!(compiler) $(BUILD_DIST)/libprotobuf-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/google/protobuf
-
-	# libprotobuf.mk Prep libprotoc25
-	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotoc.25.dylib $(BUILD_DIST)/libprotoc25/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-
+	
+	# libprotobuf.mk Prep libprotoc28
+	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotoc.28.dylib $(BUILD_DIST)/libprotoc28/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	
 	# libprotobuf.mk Prep libprotoc-dev
 	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libprotoc.{a,dylib} $(BUILD_DIST)/libprotoc-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/google/protobuf/compiler $(BUILD_DIST)/libprotoc-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/google/protobuf
-
+	
 	# libprotobuf.mk Prep protobuf-compiler
 	cp -a $(BUILD_STAGE)/libprotobuf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin $(BUILD_DIST)/protobuf-compiler/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-
+	
 	# libprotobuf.mk Sign
-	$(call SIGN,libprotobuf25,general.xml)
-	$(call SIGN,libprotobuf-lite25,general.xml)
-	$(call SIGN,libprotoc25,general.xml)
+	$(call SIGN,libprotobuf28,general.xml)
+	$(call SIGN,libprotobuf-lite28,general.xml)
+	$(call SIGN,libprotoc28,general.xml)
 	$(call SIGN,protobuf-compiler,general.xml)
-
+	
 	# libprotobuf.mk Make .debs
-	$(call PACK,libprotobuf25,DEB_LIBPROTOBUF_V)
-	$(call PACK,libprotobuf-lite25,DEB_LIBPROTOBUF_V)
+	$(call PACK,libprotobuf28,DEB_LIBPROTOBUF_V)
+	$(call PACK,libprotobuf-lite28,DEB_LIBPROTOBUF_V)
 	$(call PACK,libprotobuf-dev,DEB_LIBPROTOBUF_V)
-	$(call PACK,libprotoc25,DEB_LIBPROTOBUF_V)
+	$(call PACK,libprotoc28,DEB_LIBPROTOBUF_V)
 	$(call PACK,libprotoc-dev,DEB_LIBPROTOBUF_V)
 	$(call PACK,protobuf-compiler,DEB_LIBPROTOBUF_V)
-
+	
 	# libprotobuf.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libprotobuf{{,-lite}25,-dev} $(BUILD_DIST)/libprotoc{25,-dev} $(BUILD_DIST)/protobuf-compiler
+	rm -rf $(BUILD_DIST)/libprotobuf{{,-lite}28,-dev} $(BUILD_DIST)/libprotoc{28,-dev} $(BUILD_DIST)/protobuf-compiler
 
 .PHONY: libprotobuf libprotobuf-package
--- makefiles/mosh.mk
+++ makefiles/mosh.mk
@@ -3,19 +3,20 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS  += mosh
-MOSH_VERSION := 1.3.2
-DEB_MOSH_V   ?= $(MOSH_VERSION)-4
+MOSH_COMMIT  := e023e81c08897c95271b4f4f0726ec165bb6e1bd
+MOSH_VERSION := 1.3.2+git20210721.$(shell echo $(MOSH_COMMIT) | cut -c -7)
+DEB_MOSH_V   ?= $(MOSH_VERSION)
 
 mosh-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://mosh.org/mosh-$(MOSH_VERSION).tar.gz
-	$(call EXTRACT_TAR,mosh-$(MOSH_VERSION).tar.gz,mosh-$(MOSH_VERSION),mosh)
-	$(call DO_PATCH,mosh,mosh,-p1)
+	$(call GITHUB_ARCHIVE,mobile-shell,mosh,$(MOSH_COMMIT),$(MOSH_COMMIT))
+	$(call EXTRACT_TAR,mosh-$(MOSH_COMMIT).tar.gz,mosh-$(MOSH_COMMIT),mosh)
 
 ifneq ($(wildcard $(BUILD_WORK)/mosh/.build_complete),)
 mosh:
 	@echo "Using previously built mosh."
 else
 mosh: mosh-setup libprotobuf openssl ncurses
+	cd $(BUILD_WORK)/mosh && autoreconf -fi
 	cd $(BUILD_WORK)/mosh && ./configure \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--with-ncursesw \
@@ -33,17 +34,16 @@ endif
 mosh-package: mosh-stage
 	# mosh.mk Package Structure
 	rm -rf $(BUILD_DIST)/mosh
-	mkdir -p $(BUILD_DIST)/mosh
-
+	
 	# mosh.mk Prep mosh
-	cp -a $(BUILD_STAGE)/mosh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/mosh
-
+	cp -a $(BUILD_STAGE)/mosh $(BUILD_DIST)
+	
 	# mosh.mk Sign
 	$(call SIGN,mosh,general.xml)
-
+	
 	# mosh.mk Make .debs
 	$(call PACK,mosh,DEB_MOSH_V)
-
+	
 	# mosh.mk Build cleanup
 	rm -rf $(BUILD_DIST)/mosh
 
