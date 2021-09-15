794bcc4ba494dff7bcacc3b965e2b944707d2339
MidnightChip <34963874+midnightchip@users.noreply.github.com>
2021-09-15 08:42:25 -0600

    Update makefiles/arpoison.mk
    Co-authored-by: asdfugil <42699250+asdfugil@users.noreply.github.com>

--- makefiles/arpoison.mk
+++ makefiles/arpoison.mk
@@ -26,7 +26,6 @@ arpoison: arpoison-setup libnet
 		-o $(BUILD_WORK)/arpoison/arpoison \
 		$(BUILD_WORK)/arpoison/arpoison.o \
 		-l net
-	# remove linking dir
 	rm -rf $(BUILD_WORK)/linking
 
 	+mkdir -p $(BUILD_STAGE)/arpoison/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
