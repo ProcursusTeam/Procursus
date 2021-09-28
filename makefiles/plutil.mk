1485bd72d3a15c5f00c177a02877dca7fead2257
Cameron Katri <me@cameronkatri.com>
2021-09-28 13:46:46 -0400

    plutil: Use swift's open source plutil instead of bingner's

--- makefiles/plutil.mk
+++ makefiles/plutil.mk
@@ -2,39 +2,38 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS     += plutil
-PLUTIL_VERSION  := 0.2.2
-DEB_PLUTIL_V    ?= $(PLUTIL_VERSION)
+SUBPROJECTS  += plutil
+DEB_PLUTIL_V ?= $(SWIFT_VERSION)~$(SWIFT_SUFFIX)
 
 plutil-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://github.com/Diatrus/plutil/releases/download/v$(PLUTIL_VERSION)/plutil-$(PLUTIL_VERSION).tar.xz
-	$(call EXTRACT_TAR,plutil-$(PLUTIL_VERSION).tar.xz,plutil-$(PLUTIL_VERSION),plutil)
+	$(call GITHUB_ARCHIVE,apple,swift-corelibs-foundation,$(SWIFT_VERSION)-$(SWIFT_SUFFIX),swift-$(SWIFT_VERSION)-$(SWIFT_SUFFIX),plutil)
+	$(call EXTRACT_TAR,plutil-$(SWIFT_VERSION)-$(SWIFT_SUFFIX).tar.gz,swift-corelibs-foundation-swift-$(SWIFT_VERSION)-$(SWIFT_SUFFIX),plutil)
+	sed -i 's|SwiftFoundation|Foundation|g' $(BUILD_WORK)/plutil/Sources/Tools/plutil/main.swift
+	mkdir -p $(BUILD_STAGE)/plutil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
 ifneq ($(wildcard $(BUILD_WORK)/plutil/.build_complete),)
 plutil:
 	@echo "Using previously built plutil."
 else
 plutil: plutil-setup
-	+$(MAKE) -C $(BUILD_WORK)/plutil install \
-		CC="$(CC)" \
-		DESTDIR="$(BUILD_STAGE)/plutil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
+	cd $(BUILD_WORK)/plutil/Sources/Tools/plutil; \
+		swiftc -Osize --target=$(LLVM_TARGET) -sdk $(TARGET_SYSROOT) -o $(BUILD_STAGE)/plutil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/plutil main.swift
 	$(call AFTER_BUILD)
 endif
 
 plutil-package: plutil-stage
 	# plutil.mk Package Structure
 	rm -rf $(BUILD_DIST)/plutil
-	mkdir -p $(BUILD_DIST)/plutil
-
+	
 	# plutil.mk Prep plutil
-	cp -a $(BUILD_STAGE)/plutil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/plutil
-
+	cp -a $(BUILD_STAGE)/plutil $(BUILD_DIST)
+	
 	# plutil.mk Sign
 	$(call SIGN,plutil,general.xml)
-
+	
 	# plutil.mk Make .debs
 	$(call PACK,plutil,DEB_PLUTIL_V)
-
+	
 	# plutil.mk Build cleanup
 	rm -rf $(BUILD_DIST)/plutil
 
