4abf7be5cf9beedc367ff35ad3ff11745a21f2fd
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-10-03 19:38:55 +0800

    fix toybox

--- makefiles/toybox.mk
+++ makefiles/toybox.mk
@@ -13,11 +13,16 @@ endif
 toybox-setup: setup
 	$(call GITHUB_ARCHIVE,landley,toybox,$(TOYBOX_VERSION),$(TOYBOX_VERSION))
 	$(call EXTRACT_TAR,toybox-$(TOYBOX_VERSION).tar.gz,toybox-$(TOYBOX_VERSION),toybox)
-	$(SED) -i s/utmp.h/utmpx.h/g $(BUILD_WORK)/toybox/toys/pending/last.c
-	$(SED) -i 's|struct utmp|struct utmpx|g' $(BUILD_WORK)/toybox/toys/pending/last.c
-	$(SED) -i 's|syscall(__NR_|syscall(|g' $(BUILD_WORK)/toybox/toys/other/ionice.c
-	$(SED) -i 's|UT_LINESIZE|13838852|g' $(BUILD_WORK)/toybox/toys/pending/last.c
-	$(SED) -i '1 i\#define LOGIN_NAME_MAX 256' $(BUILD_WORK)/toybox/toys/pending/useradd.c
+	sed -i s/utmp.h/utmpx.h/g $(BUILD_WORK)/toybox/toys/pending/last.c
+	sed -i 's|struct utmp|struct utmpx|g' $(BUILD_WORK)/toybox/toys/pending/last.c
+	sed -i 's|syscall(__NR_|syscall(|g' $(BUILD_WORK)/toybox/toys/other/ionice.c
+	sed -i 's|UT_LINESIZE|_UTX_LINESIZE|g' $(BUILD_WORK)/toybox/toys/pending/last.c
+	sed -i '1 i\#define LOGIN_NAME_MAX _SC_LOGIN_NAME_MAX' $(BUILD_WORK)/toybox/toys/pending/{user,group}add.c
+	sed -i -e 's/-Wl,--gc-sections//g' -e 's/-Wl,--as-needed//g' $(BUILD_WORK)/toybox/configure
+ifneq (,$(findstring rootless,$(MEMO_TARGET)))
+	sed -i -e 's|/etc|$(MEMO_PREFIX)/etc|g' -e 's|/var|$(MEMO_PREFIX)/var|g' $(BUILD_WORK)/toybox/toys/pending/{useradd,userdel,crontab,crond,last,telnetd,init}.c
+endif
+	sed -i -e 's|/usr|$(MEMO_PREIFX)$(MEMO_SUB_PREFIX)|g' $(BUILD_WORK)/toybox/toys/posix/file.c
 	cp -a $(BUILD_MISC)/toybox/config $(BUILD_WORK)/toybox/.config
 
 ifneq ($(wildcard $(BUILD_WORK)/toybox/.build_complete),)
@@ -28,6 +33,7 @@ toybox: toybox-setup openssl $(TOYBOX_DEPS)
 	$(MAKE) -C $(BUILD_WORK)/toybox
 	+$(MAKE) -C $(BUILD_WORK)/toybox install \
 		PREFIX=$(BUILD_STAGE)/toybox
+	chmod 755 $(BUILD_STAGE)/toybox/$(MEMO_PREFIX)/bin/toybox
 	$(call AFTER_BUILD)
 endif
 
@@ -39,11 +45,11 @@ toybox-package: toybox-stage
 	# toybox.mk Prep toybox
 	$(INSTALL) -m755 $(BUILD_STAGE)/toybox/bin/toybox $(BUILD_DIST)/toybox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	
-	# toybox.mk Permissions
-	$(FAKEROOT) chmod u+s $(BUILD_DIST)/toybox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/toybox
-
 	# toybox.mk Sign
 	$(call SIGN,toybox,dd.xml)
+	
+	# toybox.mk Permissions
+	$(FAKEROOT) chmod u+s $(BUILD_DIST)/toybox/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/toybox
 	
 	# toybox.mk Make .debs
 	$(call PACK,toybox,DEB_TOYBOX_V)
