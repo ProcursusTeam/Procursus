0e17882544239f3303a995e58baec6010551b1d4
Cameron Katri <me@cameronkatri.com>
2021-10-11 19:58:41 -0400

    Update to 1.15.4

--- makefiles/gitea.mk
+++ makefiles/gitea.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS   += gitea
-GITEA_VERSION := 1.14.6
+GITEA_VERSION := 1.15.4
 DEB_GITEA_V   ?= $(GITEA_VERSION)
 
 gitea-setup: setup
@@ -14,8 +14,8 @@ gitea-setup: setup
 		mkdir -p $(BUILD_WORK)/gitea && \
 			tar xf $(BUILD_SOURCE)/gitea-$(GITEA_VERSION).tar.gz -C $(BUILD_WORK)/gitea
 	$(call DO_PATCH,gitea,gitea,-p1)
-	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/Makefile
-	$(SED) -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/custom/conf/app.example.ini
+	sed -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/Makefile
+	sed -i 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' $(BUILD_WORK)/gitea/custom/conf/app.example.ini
 
 ifneq ($(wildcard $(BUILD_WORK)/gitea/.build_complete),)
 gitea:
@@ -29,15 +29,15 @@ endif
 	cd $(BUILD_WORK)/gitea && go mod vendor
 	+$(DEFAULT_GOLANG_FLAGS) TAGS="bindata pam sqlite sqlite_unlock_notify" \
 		$(MAKE) -C $(BUILD_WORK)/gitea build
-	$(INSTALL) -Dm755 $(BUILD_WORK)/gitea/gitea $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gitea
-	$(INSTALL) -Dm755 $(BUILD_MISC)/gitea/gitea-wrapper \
+	install -Dm755 $(BUILD_WORK)/gitea/gitea $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gitea
+	install -Dm755 $(BUILD_MISC)/gitea/gitea-wrapper \
 		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/gitea-wrapper
-	$(INSTALL) -Dm644 $(BUILD_MISC)/gitea/io.gitea.web.plist \
+	install -Dm644 $(BUILD_MISC)/gitea/io.gitea.web.plist \
 		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/Library/LaunchDaemons/io.gitea.web.plist
-	$(SED) -i -e 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' -e 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' \
+	sed -i -e 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' -e 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' \
 		$(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/Library/LaunchDaemons/io.gitea.web.plist
-	$(INSTALL) -Dm644 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/app.example.ini
-	$(INSTALL) -Dm660 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/etc/gitea.ini
+	install -Dm644 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/gitea/app.example.ini
+	install -Dm660 $(BUILD_WORK)/gitea/custom/conf/app.example.ini $(BUILD_STAGE)/gitea/$(MEMO_PREFIX)/etc/gitea.ini
 	touch $(BUILD_WORK)/gitea/.build_complete
 endif
 
