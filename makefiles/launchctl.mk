9511bd7c5460cb83f516e9314bb36ed3d57c3129
asdfugil <42699250+asdfugil@users.noreply.github.com>
2021-10-14 23:26:35 +0800

    fix launchctl version

--- makefiles/launchctl.mk
+++ makefiles/launchctl.mk
@@ -5,7 +5,7 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS     += launchctl
-LAUNCHCTL_VERSION := 23-4
+LAUNCHCTL_VERSION := $(shell strings $(BUILD_MISC)/launchctl/launchctl.$(MEMO_CFVER) | grep '@(#)PROGRAM:launchctl  PROJECT:libxpc-'| cut -d- -f2)
 DEB_LAUNCHCTL_V   ?= $(LAUNCHCTL_VERSION)
 
 ifneq ($(wildcard $(BUILD_WORK)/launchctl/.build_complete),)
