28188dc00e2a6e879125f69f0cb71cfee4ad3a35
Nathan <dugarnathan@gmail.com>
2021-10-26 18:36:04 -0400

    fix js78/package

--- makefiles/mozjs78.mk
+++ makefiles/mozjs78.mk
@@ -10,13 +10,16 @@ mozjs78-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://download.gnome.org/teams/releng/tarballs-needing-help/mozjs/mozjs-$(MOZJS78_VERSION).tar.bz2
 	$(call EXTRACT_TAR,mozjs-$(MOZJS78_VERSION).tar.bz2,mozjs-$(MOZJS78_VERSION),mozjs78)
 	$(call DO_PATCH,mozjs78,mozjs78,-p1)
+ifeq (,$(findstring darwin,$(MEMO_TARGET)))
+	$(call DO_PATCH,mozjs78-ios,mozjs78,-p1)
+endif
 	sed -i 's/10.15.4/99/g' $(BUILD_WORK)/mozjs78/build/moz.configure/toolchain.configure
 	
 ifneq ($(wildcard $(BUILD_WORK)/mozjs78/.build_complete),)
 mozjs78:
 	@echo "Using previously built mozjs78."
 else
-mozjs78: mozjs78-setup
+mozjs78: mozjs78-setup icu4c readline
 	mkdir -p $(BUILD_WORK)/mozjs78/js/src/obj
 	cd $(BUILD_WORK)/mozjs78/js/src/obj && \
 	$(DEFAULT_SETUP_PY_ENV) && $(DEFAULT_RUST_FLAGS) RUST_TARGET=$(RUST_TARGET) \
@@ -31,7 +34,7 @@ mozjs78: mozjs78-setup
 		--enable-readline \
 		--disable-jemalloc \
 		--disable-tests \
-		--enable-jit \
+		--disable-jit \
 		RUSTFLAGS='-L $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib'
 	sed -i 's|HOST_CC = $(CC) -isysroot $(TARGET_SYSROOT)|HOST_CC = $(CC) -isysroot $(MACOSX_SYSROOT)|g' $(BUILD_WORK)/mozjs78/js/src/obj/config/autoconf.mk
 	sed -i 's|HOST_CXX = $(CXX)-isysroot $(TARGET_SYSROOT)|HOST_CXX = $(CXX) -isysroot $(MACOSX_SYSROOT)|g' $(BUILD_WORK)/mozjs78/js/src/obj/config/autoconf.mk
@@ -46,18 +49,27 @@ endif
 
 mozjs78-package: mozjs78-stage
 	# mozjs78.mk Package Structure
-	rm -rf $(BUILD_DIST)/mozjs78
+	rm -rf $(BUILD_DIST)/libmozjs-78-{0,dev}
+	mkdir -p $(BUILD_DIST)/libmozjs-78-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+	mkdir -p $(BUILD_DIST)/libmozjs-78-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib/pkgconfig,include,bin}
 
-	# mozjs78.mk Prep mozjs78
-	cp -a $(BUILD_STAGE)/mozjs78 $(BUILD_DIST)
+	# mozjs78.mk Prep libmozjs-78-0
+	cp -a $(BUILD_STAGE)/mozjs78/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libmozjs-78.dylib $(BUILD_DIST)/libmozjs-78-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+
+	# mozjs78.mk Prep libmozjs-78-dev
+	cp -a $(BUILD_STAGE)/mozjs78/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig/mozjs-78.pc  $(BUILD_DIST)/libmozjs-78-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig
+	cp -a $(BUILD_STAGE)/mozjs78/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/mozjs-78  $(BUILD_DIST)/libmozjs-78-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include
+	cp -a $(BUILD_STAGE)/mozjs78/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/{js78,js78-config}  $(BUILD_DIST)/libmozjs-78-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
 	# mozjs78.mk Sign
-	$(call SIGN,mozjs78,general.xml)
+	$(call SIGN,libmozjs-78-0,qemu-ios.xml)
+	$(call SIGN,libmozjs-78-dev,qemu-ios.xml)
 
 	# mozjs78.mk Make .debs
-	$(call PACK,mozjs78,DEB_MOZJS78_V)
+	$(call PACK,libmozjs-78-0,DEB_MOZJS78_V)
+	$(call PACK,libmozjs-78-dev,DEB_MOZJS78_V)
 
 	# mozjs78.mk Build cleanup
-	rm -rf $(BUILD_DIST)/mozjs78
+	rm -rf $(BUILD_DIST)/libmozjs-78-{0,dev}
 
 .PHONY: mozjs78 mozjs78-package
