35d6fb0d6b06da889631ab60f660fe5056d3161b
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2021-11-07 00:40:59 -0500

    fix xtrans compile

--- makefiles/xtrans.mk
+++ makefiles/xtrans.mk
@@ -28,10 +28,10 @@ endif
 xtrans-package: xtrans-stage
 	# xtrans.mk Package Structure
 	rm -rf $(BUILD_DIST)/xtrans-dev
-	mkdir -p $(BUILD_DIST)/xtrans-dev
+	mkdir -p $(BUILD_DIST)/xtrans-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# xtrans.mk Prep xtrans-dev
-	cp -a $(BUILD_STAGE)/xtrans/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) $(BUILD_DIST)/xtrans-dev
+	cp -a $(BUILD_STAGE)/xtrans/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/ $(BUILD_DIST)/xtrans-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# xtrans.mk Make .debs
 	$(call PACK,xtrans-dev,DEB_XTRANS_V)
