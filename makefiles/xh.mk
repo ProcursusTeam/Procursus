aa70701184ded9b978bea8d0a729a5de995329b8
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2021-11-10 19:36:22 -0600

    add xh docs

--- makefiles/xh.mk
+++ makefiles/xh.mk
@@ -4,7 +4,7 @@ endif
 
 SUBPROJECTS := xh
 XH_VERSION  := 0.13.0
-DEB_XH_V    ?= $(XH_VERSION)
+DEB_XH_V    ?= $(XH_VERSION)-1
 
 xh-setup: setup
 	$(call GITHUB_ARCHIVE,ducaale,xh,$(XH_VERSION),v$(XH_VERSION))
@@ -20,6 +20,10 @@ xh: xh-setup
 		--all-features \
 		--target=$(RUST_TARGET)
 	$(INSTALL) -Dm755 $(BUILD_WORK)/xh/target/$(RUST_TARGET)/release/xh $(BUILD_STAGE)/xh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/xh
+	$(INSTALL) -Dm644 $(BUILD_WORK)/xh/doc/xh.1 $(BUILD_STAGE)/xh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/xh.1
+	$(INSTALL) -Dm644 $(BUILD_WORK)/xh/completions/xh.bash $(BUILD_STAGE)/xh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/xh
+	$(INSTALL) -Dm644 $(BUILD_WORK)/xh/completions/xh.fish $(BUILD_STAGE)/xh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/xh.fish
+	$(INSTALL) -Dm644 $(BUILD_WORK)/xh/completions/_xh $(BUILD_STAGE)/xh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_xh
 	$(call AFTER_BUILD)
 endif
 
