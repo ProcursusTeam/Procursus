5fda5270b2e2e0bd3c5de37923194003d3ee4d5f
Cameron Katri <me@cameronkatri.com>
2021-11-13 10:51:39 -0500

    Enable openmp

--- makefiles/fftw.mk
+++ makefiles/fftw.mk
@@ -14,36 +14,40 @@ ifneq ($(wildcard $(BUILD_WORK)/fftw/.build_complete),)
 fftw:
 	@echo "Using previously built fftw."
 else
-fftw: fftw-setup
+fftw: fftw-setup openmp
 	cd $(BUILD_WORK)/fftw && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
+		--enable-openmp \
 		--enable-threads \
-		--enable-single
-	+$(MAKE) -C $(BUILD_WORK)/fftw
+		--enable-single \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
+	sed -i 's/|-fopenmp//' $(BUILD_WORK)/fftw/libtool
+	+$(MAKE) -C $(BUILD_WORK)/fftw \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
 	+$(MAKE) -C $(BUILD_WORK)/fftw install \
 		DESTDIR=$(BUILD_STAGE)/fftw
 	+$(MAKE) -C $(BUILD_WORK)/fftw clean
 	cd $(BUILD_WORK)/fftw && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
+		--enable-openmp \
 		--enable-threads \
-		--enable-double
-	+$(MAKE) -C $(BUILD_WORK)/fftw
+		--enable-double \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
+	sed -i 's/|-fopenmp//' $(BUILD_WORK)/fftw/libtool
+	+$(MAKE) -C $(BUILD_WORK)/fftw \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
 	+$(MAKE) -C $(BUILD_WORK)/fftw install \
 		DESTDIR=$(BUILD_STAGE)/fftw
 	+$(MAKE) -C $(BUILD_WORK)/fftw clean
 	cd $(BUILD_WORK)/fftw && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
+		--enable-openmp \
 		--enable-threads \
-		--enable-quad
-	+$(MAKE) -C $(BUILD_WORK)/fftw
-	+$(MAKE) -C $(BUILD_WORK)/fftw install \
-		DESTDIR=$(BUILD_STAGE)/fftw
-	+$(MAKE) -C $(BUILD_WORK)/fftw clean
-	cd $(BUILD_WORK)/fftw && ./configure -C \
-		$(DEFAULT_CONFIGURE_FLAGS) \
-		--enable-threads \
-		--enable-long-double
-	+$(MAKE) -C $(BUILD_WORK)/fftw
+		--enable-long-double \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
+	sed -i 's/|-fopenmp//' $(BUILD_WORK)/fftw/libtool
+	+$(MAKE) -C $(BUILD_WORK)/fftw \
+		OPENMP_CFLAGS="-Xpreprocessor -fopenmp -lomp"
 	+$(MAKE) -C $(BUILD_WORK)/fftw install \
 		DESTDIR=$(BUILD_STAGE)/fftw
 	+$(MAKE) -C $(BUILD_WORK)/fftw clean
@@ -59,9 +63,9 @@ fftw-package: fftw-stage
 		$(BUILD_DIST)/libfftw3-{double,long,single}3/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	
 	# fftw.mk Prep libfftw3-dev
-	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,f,l}{,_threads}.a \
+	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,f,l}{,_threads,_omp}.a \
 		$(BUILD_DIST)/libfftw3-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
-	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,f,l}{,_threads}.dylib \
+	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,f,l}{,_threads,_omp}.dylib \
 		$(BUILD_DIST)/libfftw3-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
 	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{cmake,pkgconfig} \
 		$(BUILD_DIST)/libfftw3-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
@@ -75,15 +79,15 @@ fftw-package: fftw-stage
 		$(BUILD_DIST)/libfftw3-bin/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/
 	
 	# fftw.mk Prep libfftw3-double3
-	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,_threads}.3.dylib \
+	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3{,_threads,_omp}.3.dylib \
 		$(BUILD_DIST)/libfftw3-double3/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
 	
 	# fftw.mk Prep libfftw3-long3
-	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3l{,_threads}.3.dylib \
+	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3l{,_threads,_omp}.3.dylib \
 		$(BUILD_DIST)/libfftw3-long3/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
 	
 	# fftw.mk Prep libfftw3-single3
-	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3f{,_threads}.3.dylib \
+	cp -a $(BUILD_STAGE)/fftw/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libfftw3f{,_threads,_omp}.3.dylib \
 		$(BUILD_DIST)/libfftw3-single3/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/
 	
 	# fftw.mk Sign
