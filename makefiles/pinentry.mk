d2e2292a5c9c7a9e27ea96b22825d42953e046f3
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2021-11-21 18:05:55 -0600

    add pinentry mac
    * Use GPGTools source instead of ftp.gnu.org tarballs

--- makefiles/pinentry.mk
+++ makefiles/pinentry.mk
@@ -3,32 +3,37 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS      += pinentry
-PINENTRY_VERSION := 1.1.1
+PINENTRY_VERSION := 1.1.1.1
 DEB_PINENTRY_V   ?= $(PINENTRY_VERSION)
 
+ifneq (,$(findstring darwin,$(MEMO_TARGET)))
+PINENTRY_SED_COMMAND := sed -i 's|pinentry_macosx=no|pinentry_macosx=yes|g' $(BUILD_WORK)/pinentry/configure
+else
+PINENTRY_SED_COMMAND := true
+endif
+
 pinentry-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://www.gnupg.org/ftp/gcrypt/pinentry/pinentry-$(PINENTRY_VERSION).tar.bz2{,.sig}
-	$(call PGP_VERIFY,pinentry-$(PINENTRY_VERSION).tar.bz2)
-	$(call EXTRACT_TAR,pinentry-$(PINENTRY_VERSION).tar.bz2,pinentry-$(PINENTRY_VERSION),pinentry)
+	$(call GITHUB_ARCHIVE,GPGTools,pinentry,$(PINENTRY_VERSION),v$(PINENTRY_VERSION))
+	$(call EXTRACT_TAR,pinentry-$(PINENTRY_VERSION).tar.gz,pinentry-$(PINENTRY_VERSION),pinentry)
 
 ifneq ($(wildcard $(BUILD_WORK)/pinentry/.build_complete),)
 pinentry:
-	@echo "Using previously built libassuan."
+	@echo "Using previously built pinentry."
 else
 pinentry: pinentry-setup libgpg-error libassuan ncurses
-	cd $(BUILD_WORK)/pinentry && ./configure -C \
+	touch $(BUILD_WORK)/pinentry/doc/version.texi
+	cd $(BUILD_WORK)/pinentry && autoreconf -fi && $(PINENTRY_SED_COMMAND) && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
-		--disable-pinentry-fltk \
-		--disable-pinentry-gnome3 \
-		--disable-pinentry-gtk2 \
-		--disable-pinentry-qt \
-		--disable-pinentry-qt5 \
-		--disable-pinentry-tqt \
 		--enable-pinentry-tty \
+		--enable-pinentry-ncurses \
 		NCURSES_CFLAGS="-DNCURSES_WIDECHAR"
 	+$(MAKE) -C $(BUILD_WORK)/pinentry
 	+$(MAKE) -C $(BUILD_WORK)/pinentry install \
 		DESTDIR="$(BUILD_STAGE)/pinentry"
+ifneq (,$(findstring darwin,$(MEMO_TARGET)))
+	mkdir -p $(BUILD_STAGE)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Applications
+	cp -r $(BUILD_WORK)/pinentry/macosx/pinentry-mac.app $(BUILD_STAGE)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Applications/pinentry-mac.app
+endif
 	$(call AFTER_BUILD)
 endif
 
@@ -39,6 +44,12 @@ pinentry-package: pinentry-stage
 
 	# pinentry.mk Prep pinentry
 	cp -a $(BUILD_STAGE)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share} $(BUILD_DIST)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+ifneq (,$(findstring darwin,$(MEMO_TARGET)))
+	cp -a $(BUILD_STAGE)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Applications $(BUILD_DIST)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	$(INSTALL) -Dm755 $(BUILD_MISC)/pinentry-macosx $(BUILD_DIST)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/pinentry-macosx
+	sed -i 's|@PREFIX@|$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/Applications/pinentry-mac.app/Contents/MacOS|g' \
+			$(BUILD_DIST)/pinentry/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/pinentry-macosx
+endif
 
 	# pinentry.mk Sign
 	$(call SIGN,pinentry,general.xml)
