48c82844bc4ffba081583fc150b16ce6912029d2
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2022-01-04 15:14:12 -0600

    update nginx makefile, add patch to fix ios compile, debianize

--- makefiles/nginx.mk
+++ makefiles/nginx.mk
@@ -5,36 +5,64 @@ SUBPROJECTS   += nginx
 NGINX_VERSION := 1.21.5
 DEB_NGINX_V   ?= $(NGINX_VERSION)
 
-DEFAULT_NGINX_FLAGS := --with-compat \
+ifeq ($(UNAME),Darwin)
+CC := /usr/bin/cc
+endif
+
+GLOBAL_NGINX_FLAGS := --prefix=$(MEMO_PREFIX)/etc/nginx \
+					   --sbin-path=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/nginx \
+					   --conf-path=$(MEMO_PREFIX)/etc/nginx/nginx.conf \
+					   --modules-path=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/nginx/modules \
+					   --pid-path=$(MEMO_PREFIX)/var/nginx/nginx.pid \
+					   --lock-path=$(MEMO_PREFIX)/var/nginx/nginx.lock \
+					   --http-log-path=$(MEMO_PREFIX)/var/log/nginx/access.log \
+					   --error-log-path=$(MEMO_PREFIX)/var/log/nginx/error.log \
+					   --with-cc-opt="$(CFLAGS) $(CXXFLAGS) -I$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include" \
+					   --with-ld-opt="$(LDFLAGS) -L$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib" \
+					   --with-compat \
 					   --with-debug \
+					   --with-pcre-jit \
+					   --with-threads \
+					   --with-http_ssl_module \
+			           --with-http_stub_status_module \
+			           --with-http_realip_module \
+			           --with-http_auth_request_module \
+			           --with-http_v2_module \
+			           --with-http_dav_module \
+			           --with-http_slice_module
+
+LIGHT_NGINX_FLAGS := $(GLOBAL_NGINX_FLAGS) \
+					   --with-http_addition_module
+
+CORE_NGINX_FLAGS := $(GLOBAL_NGINX_FLAGS) \
 					   --with-http_addition_module \
-					   --with-http_dav_module \
-					   --with-http_degradation_module \
-					   --with-http_flv_module \
-					   --with-http_geoip_module \
 					   --with-http_gunzip_module \
 					   --with-http_gzip_static_module \
-					   --with-http_mp4_module \
-					   --with-http_realip_module \
-					   --with-http_secure_link_module \
-					   --with-http_slice_module \
-					   --with-http_ssl_module \
-					   --with-http_stub_status_module \
-					   --with-http_sub_module \
-					   --with-http_v2_module \
-					   --with-ipv6 \
-					   --with-mail \
-					   --with-mail_ssl_module \
-					   --with-pcre \
-					   --with-pcre-jit \
-					   --with-stream \
-					   --with-stream_realip_module \
-					   --with-stream_ssl_module \
-					   --with-stream_ssl_preread_module \
+					   --with-http_sub_module
 
-nginx-setup: setup pcre openssl libgeoip
+FULL_NGINX_FLAGS := $(GLOBAL_NGINX_FLAGS) \
+            		   --with-http_addition_module \
+		               --with-http_flv_module \
+		               --with-http_geoip_module=dynamic \
+		               --with-http_gunzip_module \
+		               --with-http_gzip_static_module \
+		               --with-http_image_filter_module=dynamic \
+		               --with-http_mp4_module \
+  		               --with-http_random_index_module \
+  		               --with-http_secure_link_module \
+  		               --with-http_sub_module \
+  		               --with-http_xslt_module=dynamic \
+  		               --with-mail=dynamic \
+  		               --with-mail_ssl_module \
+  		               --with-stream=dynamic \
+  		               --with-stream_geoip_module=dynamic \
+  		               --with-stream_ssl_module \
+  		               --with-stream_ssl_preread_module
+
+nginx-setup: setup pcre openssl libgeoip libgd libxml2
 	wget -q -nc -P$(BUILD_SOURCE) https://nginx.org/download/nginx-$(NGINX_VERSION).tar.gz
 	$(call EXTRACT_TAR,nginx-$(NGINX_VERSION).tar.gz,nginx-$(NGINX_VERSION),nginx)
+	$(call DO_PATCH,nginx,nginx,-p0)
 
 ifneq ($(wildcard $(BUILD_WORK)/nginx/.build_complete),)
 nginx:
@@ -43,39 +71,60 @@ else
 nginx: nginx-setup
 	# do an config for the build machine and then sed out the cflags
 	# because nginxs build system doesnt support cross compiling
-	cd $(BUILD_WORK)/nginx && env -i ./configure \
-		$(DEFAULT_NGINX_FLAGS) \
-		--prefix=$(MEMO_PREFIX)/etc/nginx \
-		--sbin-path=$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/nginx \
-		--conf-path=$(MEMO_PREFIX)/etc/nginx/nginx.conf \
-		--pid-path=$(MEMO_PREFIX)/var/nginx/nginx.pid \
-		--lock-path=$(MEMO_PREFIX)/var/nginx/nginx.lock \
-		--with-cc-opt=-I$(MACOSX_SYSROOT)/include\ -I/opt/procursus/include \
-		--with-ld-opt=-L$(MACOSX_SYSROOT)/lib\ -L/opt/procursus/lib
+
+	# nginx-light
+	cd $(BUILD_WORK)/nginx && ./configure \
+		$(LIGHT_NGINX_FLAGS)
 
 	sed -i 's|CFLAGS =.*|CFLAGS=$(CFLAGS) $(CPPFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
 	sed -i 's|LINK =.*|LINK=$(CC) $(LDFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
 
+	+$(MAKE) -C $(BUILD_WORK)/nginx install \
+		DESTDIR=$(BUILD_STAGE)/nginx/light
+
+	# nginx-core
+	cd $(BUILD_WORK)/nginx && ./configure \
+		$(CORE_NGINX_FLAGS)
+
+	sed -i 's|CFLAGS =.*|CFLAGS=$(CFLAGS) $(CPPFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
+	sed -i 's|LINK =.*|LINK=$(CC) $(LDFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
 
 	+$(MAKE) -C $(BUILD_WORK)/nginx install \
-		DESTDIR=$(BUILD_STAGE)/nginx
+		DESTDIR=$(BUILD_STAGE)/nginx/core
+
+	# nginx-full
+	cd $(BUILD_WORK)/nginx && ./configure \
+		$(FULL_NGINX_FLAGS)
+
+	sed -i 's|CFLAGS =.*|CFLAGS=$(CFLAGS) $(CPPFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
+	sed -i 's|LINK =.*|LINK=$(CC) -L$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib $(LDFLAGS)|g' $(BUILD_WORK)/nginx/objs/Makefile
+
+	+$(MAKE) -C $(BUILD_WORK)/nginx install \
+		DESTDIR=$(BUILD_STAGE)/nginx/full
+
 	$(call AFTER_BUILD)
 endif
 
 nginx-package: nginx-stage
 	# nginx.mk Package Structure
-	rm -rf $(BUILD_DIST)/nginx
+	rm -rf $(BUILD_DIST)/nginx*
 
 	# nginx.mk Prep nginx
-	cp -a $(BUILD_STAGE)/nginx $(BUILD_DIST)
+	cp -a $(BUILD_STAGE)/nginx/core $(BUILD_DIST)/nginx-core
+	cp -a $(BUILD_STAGE)/nginx/light $(BUILD_DIST)/nginx-light
+	cp -a $(BUILD_STAGE)/nginx/full $(BUILD_DIST)/nginx-full
 
 	# nginx.mk Sign
-	$(call SIGN,nginx,general.xml)
+	$(call SIGN,nginx-core,general.xml)
+	$(call SIGN,nginx-light,general.xml)
+	$(call SIGN,nginx-full,general.xml)
 
 	# nginx.mk Make .debs
-	$(call PACK,nginx,DEB_NGINX_V)
+	$(call PACK,nginx-core,DEB_NGINX_V)
+	$(call PACK,nginx-light,DEB_NGINX_V)
+	$(call PACK,nginx-full,DEB_NGINX_V)
 
 	# nginx.mk Build cleanup
-	rm -rf $(BUILD_DIST)/nginx
+	rm -rf $(BUILD_DIST)/nginx*
 
 .PHONY: nginx nginx-package
