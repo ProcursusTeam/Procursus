8efe30b115cbb12ab1074eb67e7798423f694b1f
Cameron Katri <me@cameronkatri.com>
2022-01-23 16:11:57 -0500

    find_deps.sh: Fix for use with @rpath

--- build_tools/find_deps.sh
+++ build_tools/find_deps.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
 
 if [ -z ${OTOOL+x} ]; then
-if which otool&>/dev/null; then
-    OTOOL=$(which otool)
-elif which aarch64-apple-darwin-otool&>/dev/null; then
-    OTOOL=$(which aarch64-apple-darwin-otool)
-elif which x86_64-apple-darwin-otool&>/dev/null; then
-    OTOOL=$(which x86_64-apple-darwin-otool)
+if command -v otool&>/dev/null; then
+	OTOOL=$(command -v otool)
+elif command -v aarch64-apple-darwin-otool&>/dev/null; then
+	OTOOL=$(command -v aarch64-apple-darwin-otool)
+elif command -v x86_64-apple-darwin-otool&>/dev/null; then
+	OTOOL=$(command -v x86_64-apple-darwin-otool)
 else
-    echo "Install otool"
-    exit 1
+	echo "Install otool"
+	exit 1
 fi
 fi
 
-if $(which grep) --version | grep -q GNU&>/dev/null; then
-    GREP=grep
-elif $(which ggrep) --version | grep -q GNU&>/dev/null; then
-    GREP=$(which ggrep)
+if $(command -v grep) --help 2>&1 | grep -q -- -P&>/dev/null; then
+	GREP=grep
+elif $(command -v ggrep) --help 2>&1 | grep -q -- -P&>/dev/null; then
+	GREP=$(command -v ggrep)
+elif /usr/local/bin/grep --help 2>&1 | grep -q -- -P&>/dev/null; then
+	GREP=/usr/local/bin/grep
 else
-    echo "Install GNU grep"
-    exit 1
+	echo "Install GNU grep"
+	exit 1
 fi
 
 if [ -z ${1+x} ]; then
-    echo "Usage: $0 path/to/libs/or/bins"
-    exit 1
+	echo "Usage: $0 path/to/libs/or/bins"
+	exit 1
 fi
 
-find ${1} -type f -perm -111 -exec ${OTOOL} -L {} \; | \
-    ${GREP} -o -P '(?<=lib\/)(.*).*dylib(?!:)' | \
-    sort -u
+find ${1} -type f -exec sh -c "file -ib '{}' | grep -q 'x-mach-binary; charset=binary'" \; \
+	-exec otool -L {} \; | \
+	${GREP} -o -P '((?<=lib\/)|(?<=@rpath\/))(.*).*dylib(?!:)' | \
+	sort -u
