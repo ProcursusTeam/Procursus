3cc370893f7cee47cd7a2c8f48674b2ac9ac56c8
beerpiss <lacvtg.a1.2023@gmail.com>
2022-02-18 18:28:24 +0700

    Move generating Package.swift into setup stage

--- makefiles/ipatool.mk
+++ makefiles/ipatool.mk
@@ -10,6 +10,7 @@ DEB_IPATOOL_V   ?= $(IPATOOL_VERSION)
 ipatool-setup: setup
 	$(call GITHUB_ARCHIVE,majd,ipatool,$(IPATOOL_VERSION),v$(IPATOOL_VERSION))
 	$(call EXTRACT_TAR,ipatool-$(IPATOOL_VERSION).tar.gz,ipatool-$(IPATOOL_VERSION),ipatool)
+	sed -e 's|@IPATOOL_VERSION@|$(IPATOOL_VERSION)|g' < $(BUILD_MISC)/ipatool/Package.swift > $(BUILD_WORK)/ipatool/Sources/CLI/Package.swift
 	mkdir -p $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
 ifneq ($(wildcard $(BUILD_WORK)/ipatool/.build_complete),)
@@ -17,8 +18,6 @@ ipatool:
 	@echo "Using previously built ipatool."
 else
 ipatool: ipatool-setup
-	sed -e 's|@IPATOOL_VERSION@|$(IPATOOL_VERSION)|g' < $(BUILD_MISC)/ipatool/Package.swift > $(BUILD_WORK)/ipatool/Sources/CLI/Package.swift
-	
 	cd $(BUILD_WORK)/ipatool; \
 		swift build -c release -Xswiftc -sdk -Xswiftc $(TARGET_SYSROOT) -Xswiftc -target -Xswiftc $(LLVM_TARGET)
 	cp $(BUILD_WORK)/ipatool/.build/release/ipatool $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
