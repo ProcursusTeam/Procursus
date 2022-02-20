2c2056b6fe3c83753d9f427c87da82cccd5791ae
beerpiss <lacvtg.a1.2023@gmail.com>
2022-02-20 22:25:04 +0700

    ipatool: add shell completions

--- makefiles/ipatool.mk
+++ makefiles/ipatool.mk
@@ -11,7 +11,7 @@ ipatool-setup: setup
 	$(call GITHUB_ARCHIVE,majd,ipatool,$(IPATOOL_VERSION),v$(IPATOOL_VERSION))
 	$(call EXTRACT_TAR,ipatool-$(IPATOOL_VERSION).tar.gz,ipatool-$(IPATOOL_VERSION),ipatool)
 	sed -e 's|@IPATOOL_VERSION@|$(IPATOOL_VERSION)|g' < $(BUILD_MISC)/ipatool/Package.swift > $(BUILD_WORK)/ipatool/Sources/CLI/Package.swift
-	mkdir -p $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	mkdir -p $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/{zsh/site-functions,bash-completion/completions,fish/vendor_completions.d}}
 
 ifneq ($(wildcard $(BUILD_WORK)/ipatool/.build_complete),)
 ipatool:
@@ -19,7 +19,13 @@ ipatool:
 else
 ipatool: ipatool-setup
 	cd $(BUILD_WORK)/ipatool; \
-		swift build -c release -Xswiftc -sdk -Xswiftc $(TARGET_SYSROOT) -Xswiftc -target -Xswiftc $(LLVM_TARGET)
+		swift build -c release \
+		-Xswiftc -sdk -Xswiftc $(TARGET_SYSROOT) \
+		-Xswiftc -target -Xswiftc $(LLVM_TARGET)
+	
+	$(BUILD_WORK)/ipatool/.build/release/ipatool --generate-completion-script zsh > $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_ipatool
+ 	$(BUILD_WORK)/ipatool/.build/release/ipatool --generate-completion-script bash > $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/ipatool
+ 	$(BUILD_WORK)/ipatool/.build/release/ipatool --generate-completion-script fish > $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/ipatool.fish
 	cp $(BUILD_WORK)/ipatool/.build/release/ipatool $(BUILD_STAGE)/ipatool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 		
 	$(call AFTER_BUILD)
