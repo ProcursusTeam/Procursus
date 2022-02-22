7876d345be9ecce35c7880dbb014965fc838eaed
beerpiss <lacvtg.a1.2023@gmail.com>
2022-02-22 13:26:51 +0700

    fix: generating completion scripts for cross-compiling scenarios

--- makefiles/xcodes.mk
+++ makefiles/xcodes.mk
@@ -21,12 +21,18 @@ xcodes: xcodes-setup
 	cd $(BUILD_WORK)/xcodes; \
 		swift build -c release \
 			-Xswiftc -Onone \
+			--disable-sandbox \
+			--build-path ./.build_host
+
+	cd $(BUILD_WORK)/xcodes; \
+		swift build -c release \
+			-Xswiftc -Onone \
 			-Xswiftc -sdk -Xswiftc $(TARGET_SYSROOT) \
 			-Xswiftc -target -Xswiftc $(LLVM_TARGET) \
 			--disable-sandbox
-	$(BUILD_WORK)/xcodes/.build/release/xcodes --generate-completion-script zsh > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_xcodes
-	$(BUILD_WORK)/xcodes/.build/release/xcodes --generate-completion-script bash > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/xcodes
-	$(BUILD_WORK)/xcodes/.build/release/xcodes --generate-completion-script fish > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/xcodes.fish
+	$(BUILD_WORK)/xcodes/.build_host/release/xcodes --generate-completion-script zsh > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_xcodes
+	$(BUILD_WORK)/xcodes/.build_host/release/xcodes --generate-completion-script bash > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/xcodes
+	$(BUILD_WORK)/xcodes/.build_host/release/xcodes --generate-completion-script fish > $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/xcodes.fish
 	cp $(BUILD_WORK)/xcodes/.build/release/xcodes $(BUILD_STAGE)/xcodes/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 	$(call AFTER_BUILD)
 endif
