a70dbd304cf6d1c0e3d986f5b8b54c6fd8ae401f
asdfugil <42699250+asdfugil@users.noreply.github.com>
2022-02-22 23:32:40 +0800

    fix macos + darwin 21

--- makefiles/remote-cmds.mk
+++ makefiles/remote-cmds.mk
@@ -7,22 +7,23 @@ REMOTE-CMDS_VERSION := 64
 LIBTELNET_VERSION   := 13
 DEB_REMOTE-CMDS_V   ?= $(REMOTE-CMDS_VERSION)
 
-ifeq (,$(finstring darwin,$(MEMO_TARGET)))
-REMOTE_CMDS_BINS    := logger talk talkd telnet telnetd tftp tftpd wall
+ifneq (,$(findstring darwin,$(MEMO_TARGET)))
+REMOTE-CMDS_BINS    := telnet telnetd
 else
-REMOTE_CMDS_BINS    := telnet telnetd
+REMOTE-CMDS_BINS    := logger talk talkd telnet telnetd tftp tftpd wall
 endif
 
 remote-cmds-setup: setup
 	$(call GITHUB_ARCHIVE,apple-oss-distributions,remote_cmds,$(REMOTE-CMDS_VERSION),remote_cmds-$(REMOTE-CMDS_VERSION))
-	$(call GITHUB_ARCHIVE,apple-oss-distributions,libtelnet,$(LIBTELNET_VERSION),remote_cmds-$(LIBTELNET_VERSION))
+	$(call GITHUB_ARCHIVE,apple-oss-distributions,libtelnet,$(LIBTELNET_VERSION),libtelnet-$(LIBTELNET_VERSION))
 	$(call EXTRACT_TAR,remote_cmds-$(REMOTE-CMDS_VERSION).tar.gz,remote_cmds-remote_cmds-$(REMOTE-CMDS_VERSION),remote-cmds)
 	$(call EXTRACT_TAR,libtelnet-$(LIBTELNET_VERSION).tar.gz,libtelnet-libtelnet-$(LIBTELNET_VERSION),remote-cmds/libtelnet)
 	mkdir -p $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	sed -i -e 's/line\[/weneta\[/g' -e 's/line,/weneta,/g'  $(BUILD_WORK)/remote-cmds/telnetd.tproj/sys_term.c
+	sed -i '/size_t strlcpy(dst, src, siz)/d' $(BUILD_WORK)/remote-cmds/telnetd.tproj/strlcpy.c
 	sed -i -e '86d' $(BUILD_WORK)/remote-cmds/telnetd.tproj/authenc.c
-	# not mentioned in xcode project
-	rm -f $(BUILD_WORK)/remote-cmds/libtelnet/pk.{c,h}
+	rm -f $(BUILD_WORK)/remote-cmds/{telnetd.tproj/strlcpy.c,libtelnet/pk.{c,h}}
+
 ifneq ($(wildcard $(BUILD_WORK)/remote-cmds/.build_complete),)
 remote-cmds:
 	@echo "Using previously built remote-cmds."
@@ -41,7 +42,7 @@ remote-cmds: remote-cmds-setup ncurses
 	telnetd="-DTIOCEXT -DNO_UTMP -DLINEMODE -DKLUDGELINEMODE -DUSE_TERMIO -DOLD_ENVIRON -DENV_HACK -DINET6 -D_PATH_WTMP -ltelnet -lncursesw"; \
 	tftp=-ledit; \
 	$(CC) $(CFLAGS) -c wall.tproj/ttymsg.c; \
-	for bin in $(REMOTE_CMDS_BINS); do \
+	for bin in $(REMOTE-CMDS_BINS); do \
 		echo $$bin; \
 		$(CC) -Iinclude $${!bin} $$CFLAGS $(LDFLAGS) -o $$bin $$bin.tproj/*.c; \
 		[ -f $$bin.tproj/$$bin.1 ] && $(INSTALL) -Dm644 $$bin.tproj/$$bin.1 $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/  || true; \
@@ -49,7 +50,7 @@ remote-cmds: remote-cmds-setup ncurses
 	done;
 	install -m755 $(BUILD_WORK)/remote-cmds/telnetd $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec;
 	install -m755 $(BUILD_WORK)/remote-cmds/telnet $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin;
-ifneq (,$(findstring darwin,$(MEMO_TARGET)))
+ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 	install -m755 $(BUILD_WORK)/remote-cmds/tftpd $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin;
 	install -m755 $(BUILD_WORK)/remote-cmds/talkd $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/ntalkd;
 	install -m755 $(BUILD_WORK)/remote-cmds/{logger,wall,talk,tftp} $(BUILD_STAGE)/remote-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin;
