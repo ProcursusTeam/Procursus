ad1738062bbfb376371cf78a6501b78f1f19c295
beerpiss <lacvtg.a1.2023@gmail.com>
2022-02-22 13:33:06 +0700

    chezmoi: slightly fix makefile and bump to 2.12.1

--- makefiles/chezmoi.mk
+++ makefiles/chezmoi.mk
@@ -3,12 +3,13 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS     += chezmoi
-CHEZMOI_VERSION := 2.0.10
+CHEZMOI_COMMIT  := 8714b95999fcfc7247a3ee70e4232f438b610c82 
+CHEZMOI_VERSION := 2.12.1
 DEB_CHEZMOI_V   ?= $(CHEZMOI_VERSION)
 
 chezmoi-setup: setup
 	$(call GITHUB_ARCHIVE,twpayne,chezmoi,$(CHEZMOI_VERSION),v$(CHEZMOI_VERSION))
-	$(call EXTRACT_TAR,chezmoi-v$(CHEZMOI_VERSION).tar.gz,chezmoi-$(CHEZMOI_VERSION),chezmoi)
+	$(call EXTRACT_TAR,chezmoi-$(CHEZMOI_VERSION).tar.gz,chezmoi-$(CHEZMOI_VERSION),chezmoi)
 	mkdir -p $(BUILD_STAGE)/chezmoi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
 ifneq ($(wildcard $(BUILD_WORK)/chezmoi/.build_complete),)
@@ -23,7 +24,10 @@ endif
 	cd $(BUILD_WORK)/chezmoi && go get -d -v .
 	cd $(BUILD_WORK)/chezmoi && \
 		$(DEFAULT_GOLANG_FLAGS) \
-		go build
+		go build -ldflags "-X main.version=$(CHEZMOI_VERSION) \
+			-X main.commit=$(CHEZMOI_COMMIT) \
+			-X main.builtBy=Procursus \
+			-X main.date=$(shell date -u +%Y-%m-%dT%H:%M:%SZ)"
 	cp -a $(BUILD_WORK)/chezmoi/chezmoi $(BUILD_STAGE)/chezmoi/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	$(call AFTER_BUILD)
 endif
