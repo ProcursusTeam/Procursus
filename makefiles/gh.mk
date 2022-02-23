02bff28632c826bc8cb70d1a634837544be02d4b
beerpiss <lacvtg.a1.2023@gmail.com>
2022-02-23 12:17:45 +0700

    gh: interact with go build directly instead of through the makefile

--- makefiles/gh.mk
+++ makefiles/gh.mk
@@ -16,21 +16,29 @@ gh:
 	@echo "Using previously built gh."
 else
 gh: gh-setup
-	+$(MAKE) -C $(BUILD_WORK)/gh bin/gh \
-		CGO_CC="$(CC_FOR_BUILD)" CGO_CFLAGS="$(CFLAGS_FOR_BUILD)" CGO_CPPFLAGS="$(CPPFLAGS_FOR_BUILD)" CGO_LDFLAGS="$(LDFLAGS_FOR_BUILD)" \
-		GH_VERSION="v$(GH_VERSION)-procursus"
-	$(BUILD_WORK)/gh/bin/gh completion zsh > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_gh
-	$(BUILD_WORK)/gh/bin/gh completion bash > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/gh
-	$(BUILD_WORK)/gh/bin/gh completion fish > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/gh.fish
-	
-	+$(MAKE) -C $(BUILD_WORK)/gh clean
+	cd $(BUILD_WORK)/gh; \
+		CGO_CC="$(CC_FOR_BUILD)" CGO_CFLAGS="$(CFLAGS_FOR_BUILD)" CGO_CPPFLAGS="$(CPPFLAGS_FOR_BUILD)" CGO_LDFLAGS="$(LDFLAGS_FOR_BUILD)" go build \
+			-trimpath \
+			-o bin/gh-host \
+			$(BUILD_WORK)/gh/cmd/gh
 
-	+$(MAKE) -C $(BUILD_WORK)/gh bin/gh \
-		$(DEFAULT_GOLANG_FLAGS) \
-		GH_VERSION="v$(GH_VERSION)-procursus"
-	+unset CC CXX CFLAGS CPPFLAGS LDFLAGS && $(MAKE) -C $(BUILD_WORK)/gh manpages
-	cp -a $(BUILD_WORK)/gh/bin/gh $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-	cp -a $(BUILD_WORK)/gh/share $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cd $(BUILD_WORK)/gh; \
+		$(DEFAULT_GOLANG_FLAGS) go build \
+			-trimpath \
+			-ldflags "-X github.com/cli/cli/v2/internal/build.Date=$(shell date -u +%Y-%m-%d) \
+				-X github.com/cli/cli/v2/internal/build.Version=$(GH_VERSION)-procursus " \
+			-o $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gh \
+			$(BUILD_WORK)/gh/cmd/gh
+
+	cd $(BUILD_WORK)/gh; \
+		go run $(BUILD_WORK)/gh/cmd/gen-docs \
+			--man-page \
+			--doc-path $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/
+
+	$(BUILD_WORK)/gh/bin/gh-host completion zsh > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_gh
+	$(BUILD_WORK)/gh/bin/gh-host completion bash > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/gh
+	$(BUILD_WORK)/gh/bin/gh-host completion fish > $(BUILD_STAGE)/gh/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d/gh.fish
+
 	$(call AFTER_BUILD)
 endif
 
