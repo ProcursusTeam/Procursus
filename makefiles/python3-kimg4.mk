27d30e7c7ca553ee31c9ecf74d4849985289c4e4
Cameron Katri <me@cameronkatri.com>
2022-02-24 14:12:36 -0800

    kimg4, pyaes: python3- prefix fixes

--- makefiles/python3-kimg4.mk
+++ makefiles/python3-kimg4.mk
@@ -4,39 +4,40 @@ endif
 
 SUBPROJECTS           += pythnon3-kimg4
 PYTHON3_KIMG4_VERSION := 0.1.1
+PYTHON3_KIMG4_COMMIT  := 2e117290a90fbd53a4f5c4a8b60f8e32e6af0ad8
 DEB_PYTHON3_KIMG4_V   ?= $(PYTHON3_KIMG4_VERSION)
 
 python3-kimg4-setup: setup
-	$(call GITHUB_ARCHIVE,cxnder,kimg4,master,master)
-	$(call EXTRACT_TAR,kimg4-master.tar.gz,kimg4-master,kimg4)
+	$(call GITHUB_ARCHIVE,cxnder,kimg4,$(PYTHON3_KIMG4_VERSION),$(PYTHON3_KIMG4_COMMIT))
+	$(call EXTRACT_TAR,kimg4-$(PYTHON3_KIMG4_VERSION).tar.gz,kimg4-$(PYTHON3_KIMG4_COMMIT),python3-kimg4)
 
 ifneq ($(wildcard $(BUILD_WORK)/kimg4/.build_complete),)
 python3-kimg4:
 	@echo "Using previously built kimg4."
 else
 python3-kimg4: python3-kimg4-setup python3-pyaes python3
-	cd $(BUILD_WORK)/kimg4 && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py \
+	cd $(BUILD_WORK)/python3-kimg4 && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py \
 		build \
 		--executable="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/python3" \
 		install \
 		--install-layout=deb \
-		--root="$(BUILD_STAGE)/kimg4" \
+		--root="$(BUILD_STAGE)/python3-kimg4" \
 		--prefix="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
-	find $(BUILD_STAGE)/kimg4 -name __pycache__ -prune -exec rm -rf {} \;
+	find $(BUILD_STAGE)/python3-kimg4 -name __pycache__ -prune -exec rm -rf {} \;
 	$(call AFTER_BUILD)
 endif
 
 python3-kimg4-package: python3-kimg4-stage
-	# kimg4.mk Package Structure
-	rm -rf $(BUILD_DIST)/kimg4
+	# python3-kimg4.mk Package Structure
+	rm -rf $(BUILD_DIST)/python3-kimg4
 
-	# kimg4.mk Prep kimg4
-	cp -a $(BUILD_STAGE)/kimg4 $(BUILD_DIST)
+	# kimg4.mk Prep python3-kimg4
+	cp -a $(BUILD_STAGE)/python3-kimg4 $(BUILD_DIST)
 
-	#kimg4.mk Make .debs
+	# python3-kimg4.mk Make .debs
 	$(call PACK,python3-kimg4,DEB_PYTHON3_KIMG4_V)
 
 	# kimg4.mk Build cleanup
-	rm -rf $(BUILD_DIST)/kimg4
+	rm -rf $(BUILD_DIST)/python3-kimg4
 
 .PHONY: python3-kimg4 python3-kimg4-package
--- makefiles/python3-pyaes.mk
+++ makefiles/python3-pyaes.mk
@@ -8,35 +8,35 @@ DEB_PYTHON3_PYAES_V   ?= $(PYTHON3_PYAES_VERSION)
 
 python3-pyaes-setup: setup
 	$(call GITHUB_ARCHIVE,ricmoo,pyaes,$(PYTHON3_PYAES_VERSION),v$(PYTHON3_PYAES_VERSION))
-	$(call EXTRACT_TAR,pyaes-$(PYTHON3_PYAES_VERSION).tar.gz,pyaes-$(PYTHON3_PYAES_VERSION),pyaes)
+	$(call EXTRACT_TAR,pyaes-$(PYTHON3_PYAES_VERSION).tar.gz,pyaes-$(PYTHON3_PYAES_VERSION),python3-pyaes)
 
 ifneq ($(wildcard $(BUILD_WORK)/pyaes/.build_complete),)
 python3-pyaes:
 	@echo "Using previously built pyaes."
 else
 python3-pyaes: python3-pyaes-setup python3
-	cd $(BUILD_WORK)/pyaes && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py \
+	cd $(BUILD_WORK)/python3-pyaes && $(DEFAULT_SETUP_PY_ENV) python3 ./setup.py \
 		build \
 		--executable="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/python3" \
 		install \
 		--install-layout=deb \
-		--root="$(BUILD_STAGE)/pyaes" \
+		--root="$(BUILD_STAGE)/python3-pyaes" \
 		--prefix="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
-	find $(BUILD_STAGE)/pyaes -name __pycache__ -prune -exec rm -rf {} \;
+	find $(BUILD_STAGE)/python3-pyaes -name __pycache__ -prune -exec rm -rf {} \;
 	$(call AFTER_BUILD)
 endif
 
 python3-pyaes-package: python3-pyaes-stage
 	# pyaes.mk Package Structure
-	rm -rf $(BUILD_DIST)/pyaes
+	rm -rf $(BUILD_DIST)/python3-pyaes
 
 	# pyaes.mk Prep pyaes
-	cp -a $(BUILD_STAGE)/pyaes $(BUILD_DIST)
+	cp -a $(BUILD_STAGE)/python3-pyaes $(BUILD_DIST)
 
 	#pyaes.mk Make .debs
 	$(call PACK,python3-pyaes,DEB_PYTHON3_PYAES_V)
 
 	# pyaes.mk Build cleanup
-	rm -rf $(BUILD_DIST)/pyaes
+	rm -rf $(BUILD_DIST)/python3-pyaes
 
 .PHONY: python3-pyaes python3-pyaes-package
