19a2e971607e3539744ead7992021dff45a56db5
demhademha <62548586+demhademha@users.noreply.github.com>
2022-02-25 11:08:37 +0000

    This commit updates lrzip-next to version 0.8.9

--- makefiles/lrzip-next.mk
+++ makefiles/lrzip-next.mk
@@ -3,22 +3,21 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS        += lrzip-next
-LRZIP_NEXT_VERSION := 0.7.62
+LRZIP_NEXT_VERSION := 0.8.9
 DEB_LRZIP_NEXT_V   ?= $(LRZIP_NEXT_VERSION)
 
 lrzip-next-setup: setup
 	$(call GITHUB_ARCHIVE,pete4abw,lrzip-next,$(LRZIP_NEXT_VERSION),v$(LRZIP_NEXT_VERSION))
 	$(call EXTRACT_TAR,lrzip-next-$(LRZIP_NEXT_VERSION).tar.gz,lrzip-next-$(LRZIP_NEXT_VERSION),lrzip-next)
-
+	$(call DO_PATCH,lrzip-next,lrzip-next,-p1)
 ifneq ($(wildcard $(BUILD_WORK)/lrzip-next/.build_complete),)
 lrzip-next:
 	@echo "Using previously built lrzip-next."
 else
 lrzip-next: lrzip-next-setup lz4 liblzo2 libgcrypt
 	cd $(BUILD_WORK)/lrzip-next && ./autogen.sh && ./configure -C \
-		$(DEFAULT_CONFIGURE_FLAGS) \
-		--disable-asm
-	rm -rf $(BUILD_WORK)/lrzip-next/version
+		$(DEFAULT_CONFIGURE_FLAGS)
+	rm -f $(BUILD_WORK)/lrzip-next/version
 	+$(MAKE) -C $(BUILD_WORK)/lrzip-next
 	+$(MAKE) -C $(BUILD_WORK)/lrzip-next install \
 		DESTDIR=$(BUILD_STAGE)/lrzip-next
