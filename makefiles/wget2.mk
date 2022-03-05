0b06a75f33f7a814e74bf0188247acd6aaa7ea7e
beerpiss <lacvtg.a1.2023@gmail.com>
2022-03-05 14:48:49 +0700

    debianize wget2

--- makefiles/wget2.mk
+++ makefiles/wget2.mk
@@ -4,7 +4,8 @@ endif
 
 SUBPROJECTS  += wget2
 WGET2_VERSION := 2.0.0
-DEB_WGET2_V   ?= $(WGET2_VERSION)-2
+WGET2_API_V   := 1
+DEB_WGET2_V   ?= $(WGET2_VERSION)
 
 wget2-setup: setup
 	wget -q -nc -P $(BUILD_SOURCE) https://ftpmirror.gnu.org/wget/wget2-$(WGET2_VERSION).tar.gz{,.sig}
@@ -29,19 +30,36 @@ endif
 
 wget2-package: wget2-stage
 	# wget2.mk Package Structure
-	rm -rf $(BUILD_DIST)/wget2
-	mkdir -p $(BUILD_DIST)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include,lib,bin,share/man/man1}
+	rm -rf $(BUILD_DIST)/{wget2,libwget$(WGET2_API_V),wget2-dev}
+	mkdir -p $(BUILD_DIST)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+		$(BUILD_DIST)/libwget$(WGET2_API_V)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
+		$(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,include,share/man}
 
 	# wget2.mk Prep wget2
-	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,include,lib,share} $(BUILD_DIST)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin $(BUILD_DIST)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+
+	# wget2.mk Prep libwget$(WGET2_API_V)
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libwget*.$(WGET2_API_V).dylib $(BUILD_DIST)/libwget$(WGET2_API_V)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+
+	# wget2.mk Prep wget2-dev
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libwget*.a $(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib 
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig $(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	find $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib -type l -name '*.dylib' -exec cp -a '{}' $(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \;
+
+	cp -a $(BUILD_STAGE)/wget2/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man3 $(BUILD_DIST)/wget2-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man
 
 	# wget2.mk Sign
 	$(call SIGN,wget2,general.xml)
+	$(call SIGN,libwget$(WGET2_API_V),general.xml)
 
 	# wget2.mk Make .debs
 	$(call PACK,wget2,DEB_WGET2_V)
+	$(call PACK,libwget$(WGET2_API_V),DEB_WGET2_V)
+	$(call PACK,wget2-dev,DEB_WGET2_V)
 
 	# wget2.mk Build cleanup
-	rm -rf $(BUILD_DIST)/wget2
+	rm -rf $(BUILD_DIST)/{wget2,libwget$(WGET2_API_V),wget2-dev}
 
 .PHONY: wget2 wget2-package
