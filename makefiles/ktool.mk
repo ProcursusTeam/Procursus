a156e28254efaee49b14c997f15e8c2c9a406742
TheRealKeto <therealketo@gmail.com>
2022-03-05 13:47:27 -0500

    ktool: Update to 1.1.1
    * Update ktool to latest release (1.1.1)
    
    * Add (unfinished) manpage to the package
    
    * Change legacy setup patch to handle versioning
    
    * Add patch that changes instructions for updating
    
    Ideally, the newest patch (no_update.patch) added should
    prevent users from using pip over apt(8) and adds info
    about contacting us over the package author
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/ktool.mk
+++ makefiles/ktool.mk
@@ -3,19 +3,21 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS    += ktool
-KTOOL_VERSION  := 1.0.0
+KTOOL_VERSION  := 1.1.1
 DEB_KTOOL_V    ?= $(KTOOL_VERSION)
 
 ktool-setup: setup
 	$(call GITHUB_ARCHIVE,cxnder,ktool,$(KTOOL_VERSION),$(KTOOL_VERSION))
 	$(call EXTRACT_TAR,ktool-$(KTOOL_VERSION).tar.gz,ktool-$(KTOOL_VERSION),ktool)
 	$(call DO_PATCH,ktool,ktool,-p1)
+	mkdir -p $(BUILD_STAGE)/ktool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
 
 ifneq ($(wildcard $(BUILD_WORK)/ktool/.build_complete),)
 ktool:
 	@echo "Using previously built ktool."
 else
 ktool: ktool-setup python3-kimg4 python3-pyaes pygments python3
+	sed -i 's|@DEB_KTOOL_V@|$(DEB_KTOOL_V)|g' $(BUILD_WORK)/ktool/.legacy_setup.py
 	cd $(BUILD_WORK)/ktool && $(DEFAULT_SETUP_PY_ENV) python3 ./.legacy_setup.py \
 		build \
 		--executable="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/python3" \
@@ -23,6 +25,7 @@ ktool: ktool-setup python3-kimg4 python3-pyaes pygments python3
 		--install-layout=deb \
 		--root="$(BUILD_STAGE)/ktool" \
 		--prefix="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
+	cp -a $(BUILD_WORK)/ktool/docs/ktool.1 $(BUILD_STAGE)/ktool/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
 	find $(BUILD_STAGE)/ktool -name __pycache__ -prune -exec rm -rf {} \;
 	$(call AFTER_BUILD)
 endif
