95e1811a7ec0132fdafe29d3139ca8a8edfd0aaf
TheRealKeto <therealketo@gmail.com>
2022-03-18 19:05:00 -0400

    ncurses: Update to 6.3-2
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/ncurses.mk
+++ makefiles/ncurses.mk
@@ -7,12 +7,12 @@ STRAPPROJECTS   += ncurses
 else # ($(MEMO_TARGET),darwin-\*)
 SUBPROJECTS     += ncurses
 endif # ($(MEMO_TARGET),darwin-\*)
-NCURSES_VERSION := 6.2+20210905
+NCURSES_VERSION := 6.3-2
 DEB_NCURSES_V   ?= $(NCURSES_VERSION)
 
 ncurses-setup: setup
-	wget -q -nc -P $(BUILD_SOURCE) https://salsa.debian.org/debian/ncurses/-/archive/upstream/$(NCURSES_VERSION)/ncurses-upstream-$(NCURSES_VERSION).tar.gz
-	$(call EXTRACT_TAR,ncurses-upstream-$(NCURSES_VERSION).tar.gz,ncurses-upstream-$(NCURSES_VERSION),ncurses)
+	wget -q -nc -P $(BUILD_SOURCE) https://salsa.debian.org/debian/ncurses/-/archive/debian/$(NCURSES_VERSION)/ncurses-debian-$(NCURSES_VERSION).tar.gz
+	$(call EXTRACT_TAR,ncurses-debian-$(NCURSES_VERSION).tar.gz,ncurses-debian-$(NCURSES_VERSION),ncurses)
 
 ifneq ($(wildcard $(BUILD_WORK)/ncurses/.build_complete),)
 ncurses:
