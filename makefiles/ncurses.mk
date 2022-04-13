cabc29d7ced4097fd17dc4806f6ffac67b960dcd
Cameron Katri <me@cameronkatri.com>
2022-04-13 05:14:33 -0700

    Fix some things that were broken

--- makefiles/ncurses.mk
+++ makefiles/ncurses.mk
@@ -66,10 +66,10 @@ endif # (/usr,$(MEMO_PREFIX)$(MEMO_SUB_PREFIX))
 		ln -Tsf "$${ti##*/}" $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/terminfo/$${LINK}; \
 	done
 
-	for h in $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/ncursesw/*; do \
-		if [[ ! -d $$h ]]; then \
-			$(LN_SR) $$h $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include; \
-		fi \
+	install -d $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/ncursesw
+	set -e; \
+	for file in $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/*.h; do \
+		ln -s ../$${file##*/} $(BUILD_STAGE)/ncurses/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/ncursesw; \
 	done
 
 	for pc in formw menuw ncurses++w ncursesw panelw; do \
