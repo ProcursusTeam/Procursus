3847ff8d42f7aea537873f6251ac706f81cd00c4
Nick Chan <towinchenmi@gmail.com>
2022-05-12 13:19:42 +0800

    libetamydata: formatting fixes

--- makefiles/libeatmydata.mk
+++ makefiles/libeatmydata.mk
@@ -29,23 +29,23 @@ libeatmydata-package: libeatmydata-stage
 	rm -rf $(BUILD_DIST)/{eatmydata,libeatmydata1}
 	mkdir -p $(BUILD_DIST)/libeatmydata1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	mkdir -p $(BUILD_DIST)/eatmydata/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-	
+
 	# libeatmydata.mk Prep libeatmydata1
 	cp -a $(BUILD_STAGE)/libeatmydata/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libeatmydata.dylib $(BUILD_DIST)/libeatmydata1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libeatmydata.1.dylib
 	$(LN_SR) $(BUILD_DIST)/libeatmydata1/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libeatmydata.{1.,}dylib
 	# nothing should be linking this so no -dev package
-	
+
 	# libeatmydata.mk Prep eatmydata
 	cp -a $(BUILD_STAGE)/libeatmydata/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,libexec} $(BUILD_DIST)/eatmydata/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
-	
+
 	# libeatmydata.mk Sign
 	$(call SIGN,libeatmydata1,general.xml)
 	$(call SIGN,eatmydata,general.xml)
-	
+
 	# libeatmydata.mk Make .debs
 	$(call PACK,eatmydata,DEB_LIBEATMYDATA_V)
 	$(call PACK,libeatmydata1,DEB_LIBEATMYDATA_V)
-	
+
 	# libeatmydata.mk Build cleanup
 	rm -rf $(BUILD_DIST)/{libeatmydata1,eatmydata}
 
