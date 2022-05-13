4b1c34d632fbe7115ef78a8e53ad83f8d89c8a02
TheRealKeto <therealketo@gmail.com>
2022-05-12 21:28:00 -0400

    dimentio: Update package date version
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/dimentio.mk
+++ makefiles/dimentio.mk
@@ -6,7 +6,7 @@ ifneq (,$(findstring arm64,$(MEMO_TARGET)))
 
 STRAPPROJECTS      += dimentio
 DIMENTIO_COMMIT    := 7ffffff7129281e3e5e0472ded3a2dea6e74b518
-DIMENTIO_VERSION   := 1:0~20210608.$(shell echo $(DIMENTIO_COMMIT) | cut -c -7)
+DIMENTIO_VERSION   := 1:0~20220512.$(shell echo $(DIMENTIO_COMMIT) | cut -c -7)
 DEB_DIMENTIO_V     ?= $(DIMENTIO_VERSION)
 
 DIMENTIO_SOVERSION := 0
