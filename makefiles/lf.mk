27cc9665fcf5d7d2b777b069129449c3e82fbac8
TheRealKeto <therealketo@gmail.com>
2022-05-12 21:24:00 -0400

    lf: This is just... better, honestly
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/lf.mk
+++ makefiles/lf.mk
@@ -9,7 +9,6 @@ DEB_LF_V    ?= 0~$(LF_VERSION)
 lf-setup: setup
 	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_VERSION),$(LF_VERSION))
 	$(call EXTRACT_TAR,lf-$(LF_VERSION).tar.gz,lf-$(LF_VERSION),lf)
-	mkdir -p $(BUILD_STAGE)/lf/{etc/profile.d,$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/{lf,man/man1,zsh/site-functions,vim/vimfiles/syntax}}}
 
 ifneq ($(wildcard $(BUILD_WORK)/lf/.build_complete),)
 lf:
@@ -18,16 +17,20 @@ else
 lf: lf-setup
 	# Compile lf and move binaries
 	cd $(BUILD_WORK)/lf && $(DEFAULT_GOLANG_FLAGS) go build \
+		-o releases/bin/lf \
 		-ldflags="-s -w -X main.gVersion=$(DEB_LF_V)" .
-	cp -a $(BUILD_WORK)/lf/lf $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-	cp -a $(BUILD_WORK)/lf/lf.1 $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
+	$(INSTALL) -Dm755 $(BUILD_WORK)/lf/releases/bin/lf \
+	    $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/lf
+	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/lf.1 \
+	    -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
 	# Copy over other files for zsh, vim, csh, etc
-	cp -a $(BUILD_WORK)/lf/etc/lf.zsh \
-		$(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_lf
-	cp -a $(BUILD_WORK)/lf/etc/lfcd.sh $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/lf
-	cp -a $(BUILD_WORK)/lf/etc/lf.vim \
-		$(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/syntax
-	cp -a $(BUILD_WORK)/lf/etc/{lf.csh,lfcd.csh} $(BUILD_STAGE)/lf/etc/profile.d
+	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.zsh \
+	    $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_lf
+	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lfcd.sh \
+	    -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/lf
+	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.vim \
+	    -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/syntax
+	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/{lf.csh,lfcd.csh} -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)/etc/profile.d
 	$(call AFTER_BUILD)
 endif
 
