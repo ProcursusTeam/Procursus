03ef99d47baf1cec3d0937b3efba3772200aebd9
TheRealKeto <therealketo@gmail.com>
2022-05-15 16:47:00 -0400

    Add kakoune
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

---
+++ makefiles/kakoune.mk
@@ -0,0 +1,42 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS     += kakoune
+KAKOUNE_VERSION := 2021.11.08
+DEB_KAKOUNE_V   ?= $(KAKOUNE_VERSION)
+
+kakoune-setup: setup
+	$(call GITHUB_ARCHIVE,mawww,kakoune,$(KAKOUNE_VERSION),v$(KAKOUNE_VERSION))
+	$(call EXTRACT_TAR,kakoune-$(KAKOUNE_VERSION).tar.gz,kakoune-$(KAKOUNE_VERSION),kakoune)
+
+ifneq ($(wildcard $(BUILD_WORK)/kakoune/.build_complete),)
+kakoune:
+	@echo "Using previously built kakoune."
+else
+kakoune: kakoune-setup
+	+$(MAKE) -C $(BUILD_WORK)/kakoune \
+		CXX="$(CXX)" \
+		PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
+	+$(MAKE) -C $(BUILD_WORK)/kakoune install \
+		DESTDIR="$(BUILD_STAGE)/kakoune"
+	$(call AFTER_BUILD)
+endif
+
+kakoune-package: kakoune-stage
+	# kakoune.mk Package Structure
+	rm -rf $(BUILD_DIST)/kakoune
+
+	# kakoune.mk Prep kakoune
+	cp -a $(BUILD_STAGE)/kakoune $(BUILD_DIST)
+
+	# kakoune.mk Sign
+	$(call SIGN,kakoune,general.xml)
+
+	# kakoune.mk Make .debs
+	$(call PACK,kakoune,DEB_KAKOUNE_V)
+
+	# kakoune.mk Build cleanup
+	rm -rf $(BUILD_DIST)/kakoune
+
+.PHONY: kakoune kakoune-package
