76633f880ec692cc541110128b2309a7f0cba64e
Cameron Katri <me@cameronkatri.com>
2022-07-25 16:02:27 -0700

    Soversion libimobiledevice-glue

--- makefiles/libimobiledevice-glue.mk
+++ makefiles/libimobiledevice-glue.mk
@@ -2,7 +2,7 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS              += libimobiledevice-glue
+SUBPROJECTS                  += libimobiledevice-glue
 LIBIMOBILEDEVICEGLUE_COMMIT  := d2ff7969dcd0a12e4f18f63dab03e6cd03054fcb
 LIBIMOBILEDEVICEGLUE_VERSION := 1.0.0+git20220522.$(shell echo $(LIBIMOBILEDEVICEGLUE_COMMIT) | cut -c -7)
 DEB_LIBIMOBILEDEVICEGLUE_V   ?= $(LIBIMOBILEDEVICEGLUE_VERSION)
@@ -27,25 +27,24 @@ endif
 
 libimobiledevice-glue-package: libimobiledevice-glue-stage
 	# libimobiledevice-glue.mk Package Structure
-	rm -rf $(BUILD_DIST)/libimobiledevice-glue{,-dev}
-	mkdir -p $(BUILD_DIST)/libimobiledevice-glue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
-		$(BUILD_DIST)/libimobiledevice-glue-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	rm -rf $(BUILD_DIST)/libimobiledevice-glue{1.0-0,-dev}
+	mkdir -p $(BUILD_DIST)/libimobiledevice-glue{1.0-0,-dev}/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib \
 
 	# libimobiledevice-glue.mk Prep libimobiledevice-glue
-	cp -a $(BUILD_STAGE)/libimobiledevice-glue$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libimobiledevice-glue-1.0.0.dylib $(BUILD_DIST)/libimobiledevice-glue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libimobiledevice-glue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libimobiledevice-glue-1.0.0.dylib $(BUILD_DIST)/libimobiledevice-glue1.0-0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	# libimobiledevice-glue.mk Prep libimobiledevice-glue-dev
-	cp -a $(BUILD_STAGE)/libimobiledevice-glue$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{pkgconfig,libimobiledevice-glue-1.0.{a,dylib}} $(BUILD_DIST)/libimobiledevice-glue-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/libimobiledevice-glue$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libimobiledevice-glue-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/libimobiledevice-glue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/{pkgconfig,libimobiledevice-glue-1.0.{a,dylib}} $(BUILD_DIST)/libimobiledevice-glue-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libimobiledevice-glue/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_DIST)/libimobiledevice-glue-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# libimobiledevice-glue.mk Sign
-	$(call SIGN,libimobiledevice-glue,general.xml)
+	$(call SIGN,libimobiledevice-glue1.0-0,general.xml)
 
 	# libimobiledevice-glue.mk Make .debs
-	$(call PACK,libimobiledevice-glue,DEB_LIBIMOBILEDEVICEGLUE_V)
+	$(call PACK,libimobiledevice-glue1.0-0,DEB_LIBIMOBILEDEVICEGLUE_V)
 	$(call PACK,libimobiledevice-glue-dev,DEB_LIBIMOBILEDEVICEGLUE_V)
 
 	# libimobiledevice-glue.mk Build cleanup
-	rm -rf $(BUILD_DIST)/libimobiledevice-glue{,-dev}
+	rm -rf $(BUILD_DIST)/libimobiledevice-glue{1.0-0,-dev}
 
 .PHONY: libimobiledevice-glue libimobiledevice-glue-package
