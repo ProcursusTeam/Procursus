433909ff0b696d54456d866fe916d936570ebe6d
Teutates <103068388+Teutates@users.noreply.github.com>
2022-07-28 15:34:27 -0400

    Update usbutils

--- makefiles/usbutils.mk
+++ makefiles/usbutils.mk
@@ -3,28 +3,24 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS      += usbutils
-USBUTILS_VERSION := 007
+USBUTILS_VERSION := 014
 DEB_USBUTILS_V   ?= $(USBUTILS_VERSION)
 
 usbutils-setup: setup
 	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://www.kernel.org/pub/linux/utils/usb/usbutils/usbutils-$(USBUTILS_VERSION).tar.xz)
 	$(call EXTRACT_TAR,usbutils-$(USBUTILS_VERSION).tar.xz,usbutils-$(USBUTILS_VERSION),usbutils)
+	$(call DO_PATCH,usbutils,usbutils,-p1)
 
 ifneq ($(wildcard $(BUILD_WORK)/usbutils/.build_complete),)
 usbutils:
 	@echo "Using previously built usbutils."
 else
 usbutils: usbutils-setup libusb
+	cd $(BUILD_WORK)/usbutils && autoreconf -fi
 	cd $(BUILD_WORK)/usbutils && ./configure \
 		$(DEFAULT_CONFIGURE_FLAGS)
 	+$(MAKE) -C $(BUILD_WORK)/usbutils install \
-		DESTDIR="$(BUILD_STAGE)/usbutils" \
-		CFLAGS="$(CFLAGS) -I$(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include/libusb-1.0" \
-		AM_LDFLAGS="" \
-		SUBDIRS="" \
-		bin_SCRIPTS="" \
-		sbin_SCRIPTS="" \
-		"man_MANS=lsusb.8"
+		DESTDIR="$(BUILD_STAGE)/usbutils"
 	rm -f $(BUILD_STAGE)/usbutils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/usb.ids{,.gz}
 	$(call DOWNLOAD_FILES,$(BUILD_STAGE)/usbutils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share,http://www.linux-usb.org/usb.ids{$(comma).gz})
 	$(call AFTER_BUILD)
