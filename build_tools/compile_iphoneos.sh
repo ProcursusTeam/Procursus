dde8d558e1c7af43fb6442c385730c9f52120efa
Cameron Katri <me@cameronkatri.com>
2022-12-13 21:12:11 -0800

    Update ldid and compile_iphoneos.sh

--- build_tools/compile_iphoneos.sh
+++ build_tools/compile_iphoneos.sh
@@ -8,4 +8,5 @@ ${MAKE} MEMO_TARGET=iphoneos-arm64 rebuild-${1}-setup
 ${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1500 rebuild-${1}-package &
 ${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1600 rebuild-${1}-package &
 ${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1700 rebuild-${1}-package &
+${MAKE} MEMO_TARGET=iphoneos-arm64-rootless MEMO_CFVER=1800 rebuild-${1}-package &
 wait
