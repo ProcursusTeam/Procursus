9f7e8fc6e75949015cd12883e3d8a1e5211dc09c
Cameron Katri <me@cameronkatri.com>
2022-12-24 07:56:57 -0800

    New branch for tracking iOS 12 and 13 security updates

--- build_tools/compile_iphoneos.sh
+++ build_tools/compile_iphoneos.sh
@@ -7,7 +7,4 @@ fi
 ${MAKE} MEMO_TARGET=iphoneos-arm64 rebuild-${1}-setup
 ${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1500 rebuild-${1}-package &
 ${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1600 rebuild-${1}-package &
-${MAKE} MEMO_TARGET=iphoneos-arm64 MEMO_CFVER=1700 rebuild-${1}-package &
-${MAKE} MEMO_TARGET=iphoneos-arm64-rootless MEMO_CFVER=1800 rebuild-${1}-package &
-${MAKE} MEMO_TARGET=iphoneos-arm64-rootless MEMO_CFVER=1900 rebuild-${1}-package &
 wait
