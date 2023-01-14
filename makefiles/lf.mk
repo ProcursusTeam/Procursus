8ff9b4821260d55e858b8115b177f51631df941b
TheRealKeto <therealketo@gmail.com>
2023-01-13 19:08:00 -0500

    lf: Use Debian-style versioning
    * Use Debian-style versioning
    
    * Use a symbolic link in vimfiles
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/lf.mk
+++ makefiles/lf.mk
@@ -3,12 +3,13 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS += lf
-LF_VERSION  := r28
-DEB_LF_V    ?= 0-$(LF_VERSION)
+LF_RELEASE  := r28
+LF_VERSION  := $(shell echo $(LF_RELEASE) | cut -b 2-3)
+DEB_LF_V    ?= $(LF_VERSION)
 
 lf-setup: setup
-	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_VERSION),$(LF_VERSION))
-	$(call EXTRACT_TAR,lf-$(LF_VERSION).tar.gz,lf-$(LF_VERSION),lf)
+	$(call GITHUB_ARCHIVE,gokcehan,lf,$(LF_RELEASE),$(LF_RELEASE))
+	$(call EXTRACT_TAR,lf-$(LF_RELEASE).tar.gz,lf-$(LF_RELEASE),lf)
 
 ifneq ($(wildcard $(BUILD_WORK)/lf/.build_complete),)
 lf:
@@ -17,14 +18,14 @@ else
 lf: lf-setup
 	cd $(BUILD_WORK)/lf && $(DEFAULT_GOLANG_FLAGS) go build \
 		-trimpath \
-		-ldflags "-s -w -X main.gVersion=$(DEB_LF_V)" \
+		-ldflags "-s -w -X main.gVersion=$(LF_RELEASE)" \
 		-o $(BUILD_WORK)/lf/lf
 	$(INSTALL) -Dm755 $(BUILD_WORK)/lf/lf -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/lf.1 -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.zsh $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/zsh/site-functions/_lf
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lfcd.sh -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/lf
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.vim -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/syntax
-	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.vim -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/ftdetect
+	$(LN_S) syntax $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/vim/vimfiles/ftdetect
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.bash $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion/completions/lf
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf{,cd}.csh -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)/etc/profile.d
 	$(INSTALL) -Dm644 $(BUILD_WORK)/lf/etc/lf.fish -t $(BUILD_STAGE)/lf/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/fish/vendor_completions.d
