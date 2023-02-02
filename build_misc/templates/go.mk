d124ba6831cfd585af73eb1c902811898ab1ce39
TheRealKeto <therealketo@gmail.com>
2023-02-01 19:53:00 -0500

    templates: Add -trimpath to Go template
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- build_misc/templates/go.mk
+++ build_misc/templates/go.mk
@@ -17,7 +17,8 @@ ifneq ($(wildcard $(BUILD_WORK)/@pkg@/.build_complete),)
 else
 @pkg@: @pkg@-setup
 	cd $(BUILD_WORK)/@pkg@ && $(DEFAULT_GOLANG_FLAGS) go build \
-			-o $(BUILD_WORK)/@pkg@/@pkg@
+		-trimpath \
+		-o $(BUILD_WORK)/@pkg@/@pkg@
 	$(INSTALL) -Dm755 $(BUILD_WORK)/@pkg@/@pkg@ $(BUILD_STAGE)/@pkg@/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
 	$(call AFTER_BUILD)
 endif
