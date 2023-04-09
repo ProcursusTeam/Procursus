69db75646ac4b5484fdccb9ed7632831e04b25d0
tihmstar <tihmstar@gmail.com>
2023-04-09 09:13:26 +0200

    fix openssl

--- makefiles/openssl.mk
+++ makefiles/openssl.mk
@@ -5,9 +5,9 @@ endif
 
 ifeq ($(shell [ "$(CFVER_WHOLE)" -ge 1400 ] && echo 1),1)
 OPENSSL_VERSION := $(OPENSSL_CF1400_VERSION)
-diskdev-cmds-setup: openssl_CF1400-setup
-diskdev-cmds: openssl_CF1400
-diskdev-cmds-package: openssl_CF1400-package
+openssl-setup: openssl_CF1400-setup
+openssl: openssl_CF1400
+openssl-package: openssl_CF1400-package
 
 else
 OPENSSL_VERSION := $(OPENSSL_CFBASE_VERSION)
