7856ebdc82a06bde23c875e4f68735c396b6b2eb
Teutates <103068388+Teutates@users.noreply.github.com>
2023-04-18 22:07:52 -0400

    Dynamic link libcrypt in pam_modules
    * This was broken once, seems to work now...

--- makefiles/pam-modules.mk
+++ makefiles/pam-modules.mk
@@ -10,7 +10,6 @@ DEB_PAM-MODULES_V   ?= $(PAM-MODULES_VERSION)
 
 ###
 # TODO: Have a look at the sacl and launchd and utmpw modules.
-# NOTE: For some reason here, libSystem's crypt() really likes being dominant. Static link to fix the issue!
 ###
 
 pam-modules-setup: setup
@@ -27,7 +26,7 @@ else
 pam-modules: pam-modules-setup openpam libxcrypt
 	set -e; \
 	cd $(BUILD_WORK)/pam-modules; \
-	$(CC) $(CFLAGS) -I$(BUILD_WORK)/pam-modules -I$(BUILD_WORK)/pam-modules/libc/gen -I$(BUILD_WORK)/pam-modules/libutil -bundle -o pam_unix.so pam_unix/*.c libutil/*.c $(BUILD_WORK)/pam-modules/libc/gen/pw_scan.c $(LDFLAGS) -lpam $(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libcrypt.a || true; \
+	$(CC) $(CFLAGS) -I$(BUILD_WORK)/pam-modules -I$(BUILD_WORK)/pam-modules/libc/gen -I$(BUILD_WORK)/pam-modules/libutil -bundle -o pam_unix.so pam_unix/*.c libutil/*.c $(BUILD_WORK)/pam-modules/libc/gen/pw_scan.c $(LDFLAGS) -lpam -lcrypt || true; \
 	$(CC) $(CFLAGS) -I$(BUILD_WORK)/pam-modules -I$(BUILD_WORK)/pam-modules/libc/gen -I$(BUILD_WORK)/pam-modules/libutil -bundle -o pam_nologin.so pam_nologin/*.c libutil/*.c $(BUILD_WORK)/pam-modules/libc/gen/pw_scan.c $(LDFLAGS) -lpam || true; \
 	cp -a pam_unix.so pam_nologin.so $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pam; \
 	install -m644 pam_unix/pam_unix.8 pam_nologin/pam_nologin.8 $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man8/; \
@@ -37,20 +36,6 @@ pam-modules: pam-modules-setup openpam libxcrypt
 		cp -a pam_$${module}.so $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pam; \
 		install -m644 pam_$${module}/pam_$${module}.8 $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man8/; \
 	done
-ifeq (0,1)
-	set -e; \
-	cd $(BUILD_WORK)/pam-modules/modules; \
-	for module in group launchd nologin rootok sacl self uwtmp; do \
-		echo $${module}; \
-		$(CC) $(CFLAGS) -I$(BUILD_WORK)/pam-modules/include -bundle -o pam_$${module}.so pam_$${module}/*.c $(LDFLAGS) -lpam || true; \
-		cp -a pam_$${module}.so $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pam; \
-		install -m644 pam_$${module}/pam_$${module}.8 $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man8/; \
-	done
-	cd $(BUILD_STAGE)/openpam/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pam; \
-	for so in *.2.so; do \
-		cp -a $$so $(BUILD_STAGE)/pam-modules/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pam/$${so//".2"/}; \
-	done
-endif
 	$(call AFTER_BUILD)
 endif
 
