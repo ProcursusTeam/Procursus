372b4f460014ec7cb29fd0bff380bdd11f307000
TheRealKeto <therealketo@gmail.com>
2023-05-02 00:39:00 -0400

    mandoc: Migrate to DOWNLOAD_FILES
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/mandoc.mk
+++ makefiles/mandoc.mk
@@ -2,12 +2,12 @@ ifneq ($(PROCURSUS),1)
 $(error Use the main Makefile)
 endif
 
-SUBPROJECTS   += mandoc
+SUBPROJECTS    += mandoc
 MANDOC_VERSION := 1.14.6
 DEB_MANDOC_V   ?= $(MANDOC_VERSION)
 
 mandoc-setup: setup
-	wget -q -nc -P$(BUILD_SOURCE) https://mandoc.bsd.lv/snapshots/mandoc-$(MANDOC_VERSION).tar.gz
+	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://mandoc.bsd.lv/snapshots/mandoc-$(MANDOC_VERSION).tar.gz)
 	$(call EXTRACT_TAR,mandoc-$(MANDOC_VERSION).tar.gz,mandoc-$(MANDOC_VERSION),mandoc)
 	$(call DO_PATCH,mandoc,mandoc,-p1)
 	sed -i -e 's|@CC@|$(CC)|' \
@@ -17,8 +17,7 @@ mandoc-setup: setup
 		-e 's|@MEMO_PREFIX@|$(MEMO_PREFIX)|g' \
 		-e 's|@MEMO_SUB_PREFIX@|$(MEMO_SUB_PREFIX)|g' \
 		$(BUILD_WORK)/mandoc/configure.local
-	sed -i '/int dummy;/d' \
-		$(BUILD_WORK)/mandoc/compat_*.c
+	sed -i '/int dummy;/d' $(BUILD_WORK)/mandoc/compat_*.c
 
 ifneq ($(wildcard $(BUILD_WORK)/mandoc/.build_complete),)
 mandoc:
@@ -30,7 +29,7 @@ mandoc: mandoc-setup
 	+$(MAKE) -C $(BUILD_WORK)/mandoc \
 		LDADD="-lz"
 	+$(MAKE) -C $(BUILD_WORK)/mandoc install \
-		DESTDIR=$(BUILD_STAGE)/mandoc
+		DESTDIR="$(BUILD_STAGE)/mandoc"
 	$(call AFTER_BUILD)
 endif
 
