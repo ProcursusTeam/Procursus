422d8308cc4406184ea108f7a2f2957bd070f884
TheRealKeto <therealketo@gmail.com>
2023-06-20 17:47:00 -0400

    2048: Use Makefile, include LICENSE in package
    * Use Makefile, include LICENSE in package
    
    * Use GITHUB_ARCHIVE over downloading single file
    
    Signed-off-by: TheRealKeto <therealketo@gmail.com>

--- makefiles/2048.mk
+++ makefiles/2048.mk
@@ -3,27 +3,25 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS   += 2048
+2048_COMMIT   := 6c04517bb59c28f3831585da338f021bc2ea86d6
 2048_VERSION  := 2022.10.23
 DEB_2048_V    ?= 0.$(2048_VERSION)
-2048_GIT_HASH := 6c04517bb59c28f3831585da338f021bc2ea86d6
 
 2048-setup: setup
-	$(call DOWNLOAD_FILE,$(BUILD_SOURCE)/2048-$(2048_GIT_HASH).c, \
-		https://github.com/mevdschee/2048.c/raw/$(2048_GIT_HASH)/2048.c)
-	mkdir -p $(BUILD_WORK)/2048 $(BUILD_STAGE)/2048/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
-	cp $(BUILD_SOURCE)/2048-$(2048_GIT_HASH).c $(BUILD_WORK)/2048/2048.c
+	$(call GITHUB_ARCHIVE,mevdschee,2048.c,$(2048_COMMIT),$(2048_COMMIT))
+	$(call EXTRACT_TAR,2048.c-$(2048_COMMIT).tar.gz,2048.c-$(2048_COMMIT),2048)
+	sed -i "/#define _XOPEN_SOURCE 500/d" $(BUILD_WORK)/2048/2048.c
 
 ifneq ($(wildcard $(BUILD_WORK)/2048/.build_complete),)
 2048:
 	@echo "Using previously built 2048."
 else
 2048: 2048-setup
-	sed -i '/#define _XOPEN_SOURCE 500/d' $(BUILD_WORK)/2048/2048.c
-	cd $(BUILD_WORK)/2048 && $(CC) -std=c99 \
-		$(CFLAGS) \
-		2048.c \
-		-o $(BUILD_WORK)/2048/2048
-	$(INSTALL) -Dm755 $(BUILD_WORK)/2048/2048 $(BUILD_STAGE)/2048/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	+$(MAKE) -C $(BUILD_WORK)/2048 \
+		CFLAGS="-std=c99 $(CFLAGS)"
+	+$(MAKE) -C $(BUILD_WORK)/2048 install \
+		PREFIX="$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)" \
+		DESTDIR="$(BUILD_STAGE)/2048"
 	$(call AFTER_BUILD)
 endif
 
