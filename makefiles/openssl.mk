0bd6ee0e993ac3fa8ed151f7d4802110c5483994
Cameron Katri <me@cameronkatri.com>
2023-09-11 08:58:46 -0700

    openssl: 3.0.10

--- makefiles/openssl.mk
+++ makefiles/openssl.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS   += openssl
-OPENSSL_VERSION := 3.0.8
+OPENSSL_VERSION := 3.0.10
 DEB_OPENSSL_V   ?= $(OPENSSL_VERSION)
 
 openssl-setup: setup
