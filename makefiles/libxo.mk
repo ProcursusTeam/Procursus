0f62f8db05ff4bf6ff2d04ebd736cf40d03d96a2
Nick Chan <towinchenmi@gmail.com>
2023-09-27 16:35:36 +0800

    libxo: Disable installation into build base on CFVER 2000+
    it is provieded by iOS though there are no headers

--- makefiles/libxo.mk
+++ makefiles/libxo.mk
@@ -6,6 +6,14 @@ SUBPROJECTS   += libxo
 LIBXO_VERSION := 1.4.0
 DEB_LIBXO_V   ?= $(LIBXO_VERSION)
 
+ifeq ($(shell [ "$(CFVER_WHOLE)" -ge 2000 ] && echo 1),1)
+LIBXO_LIBDIR  := $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)$(MEMO_ALT_PREFIX)/lib
+else
+LIBXO_LIBDIR  := $(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+endif
+
+# Provided by macOS 14/iOS 17+ and only used for tools and headers on said versions
+
 libxo-setup: setup
 	$(call DOWNLOAD_FILES,$(BUILD_SOURCE), \
 		https://github.com/Juniper/libxo/releases/download/$(LIBXO_VERSION)/libxo-$(LIBXO_VERSION).tar.gz)
@@ -19,6 +27,7 @@ libxo: libxo-setup
 	cd $(BUILD_WORK)/libxo && ./configure -C \
 		$(DEFAULT_CONFIGURE_FLAGS) \
 		--with-gettext=$(BUILD_BASE)/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX) \
+		--libdir=$(LIBXO_LIBDIR) \
 		ac_cv_func_malloc_0_nonnull=yes \
 		ac_cv_func_realloc_0_nonnull=yes
 	+$(MAKE) -C $(BUILD_WORK)/libxo \
@@ -26,32 +35,40 @@ libxo: libxo-setup
 	+$(MAKE) -C $(BUILD_WORK)/libxo install \
 		LIBTOOL='$(BUILD_WORK)/libxo/libtool' \
 		DESTDIR=$(BUILD_STAGE)/libxo
+ifeq ($(shell [ "$(CFVER_WHOLE)" -ge 2000 ] && echo 1),1)
+	mkdir -p $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig
+	# must use pc file from build misc because builtin lib prefix is always /usr
+	$(INSTALL) -m644 $(BUILD_MISC)/libxo.pc $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig
+	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include $(BUILD_BASE)$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	$(call AFTER_BUILD)
+else
+	$(call AFTER_BUILD,copy)
+endif
 endif
 
 libxo-package: libxo-stage
 	# libxo.mk Package Structure
 	rm -rf $(BUILD_DIST)/libxo{0,-dev,-tools}
-	mkdir -p $(BUILD_DIST)/libxo0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,share/man} \
-		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,bin,share/man/man1} \
+	mkdir -p $(BUILD_DIST)/libxo0/{$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man,$(LIBXO_LIBDIR)} \
+		$(BUILD_DIST)/libxo-dev/{$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/man/man1},$(LIBXO_LIBDIR)} \
 		$(BUILD_DIST)/libxo-tools/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/man/man1}
 
 	# libxo.mk Prep libxo0
-	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo{,.0.dylib} \
-		$(BUILD_DIST)/libxo0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	rm -f $(BUILD_DIST)/libxo0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo/encoder/*.a \
-		$(BUILD_DIST)/libxo0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo/**/*test*
+	cp -a $(BUILD_STAGE)/libxo/$(LIBXO_LIBDIR)/libxo{,.0.dylib} \
+		$(BUILD_DIST)/libxo0/$(LIBXO_LIBDIR)
+	rm -f $(BUILD_DIST)/libxo0/$(LIBXO_LIBDIR)/libxo/encoder/*.a \
+		$(BUILD_DIST)/libxo0/$(LIBXO_LIBDIR)/libxo/**/*test*
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man7 \
 		$(BUILD_DIST)/libxo0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/
 
 	# libxo.mk Prep libxo-dev
-	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo{,.dylib,.a} \
-		$(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/pkgconfig \
-		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/libxo/$(LIBXO_LIBDIR)/libxo{,.dylib,.a} \
+		$(BUILD_STAGE)/libxo/$(LIBXO_LIBDIR)/pkgconfig \
+		$(BUILD_DIST)/libxo-dev/$(LIBXO_LIBDIR)
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/include \
 		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/
-	rm -f $(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo/encoder/*.{enc,dylib} \
-		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libxo/**/*test*
+	rm -f $(BUILD_DIST)/libxo-dev/$(LIBXO_LIBDIR)/libxo/encoder/*.{enc,dylib} \
+		$(BUILD_DIST)/libxo-dev/$(LIBXO_LIBDIR)/libxo/**/*test*
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man{3,5} \
 		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/xo{po,lint}.1$(MEMO_MANPAGE_SUFFIX) \
@@ -59,7 +76,7 @@ libxo-package: libxo-stage
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/{libxo-config,xo{po,lint}} \
 		$(BUILD_DIST)/libxo-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
-	# libxo.mk Prep libxo0
+	# libxo.mk Prep libxo-tools
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/xo{,html} \
 		$(BUILD_DIST)/libxo-tools/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_STAGE)/libxo/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/xo{,html}.1$(MEMO_MANPAGE_SUFFIX) \
