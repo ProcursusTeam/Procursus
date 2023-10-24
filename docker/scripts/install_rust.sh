bbbfd18604bac3549f04f372095bb773efa1d0fe
tihmstar <tihmstar@gmail.com>
2023-10-24 12:55:39 +0200

    drop rust from appletv

--- docker/scripts/install_rust.sh
+++ docker/scripts/install_rust.sh
@@ -6,7 +6,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-t
 RUST_TARGETS=(
   "aarch64-apple-darwin"
   "aarch64-apple-ios"
-  "aarch64-apple-tvos"
   "x86_64-apple-darwin"
 )
 
