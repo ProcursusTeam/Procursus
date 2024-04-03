e44418e93383aa4aff8a59fc48d904338be1ae77
Nick Chan <towinchenmi@gmail.com>
2024-04-03 18:54:11 +0800

    Fix bastet build

--- makefiles/bastet.mk
+++ makefiles/bastet.mk
@@ -10,7 +10,8 @@ bastet-setup: setup
 	$(call GITHUB_ARCHIVE,fph,bastet,$(BASTET_VERSION),$(BASTET_VERSION))
 	$(call EXTRACT_TAR,bastet-$(BASTET_VERSION).tar.gz,bastet-$(BASTET_VERSION),bastet)
 	$(call DO_PATCH,bastet,bastet,-p1)
-	sed -i 's/-lncurses/-lncursesw/' $(BUILD_WORK)/bastet/Makefile
+	sed -i 's/-lncurses /-lncursesw /' $(BUILD_WORK)/bastet/Makefile
+	sed -i 's/bind2end/std::bind2nd/g' $(BUILD_WORK)/bastet/BastetBlockChooser.cpp
 	mkdir -p $(BUILD_STAGE)/bastet/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share/man/man6}
 
 ifneq ($(wildcard $(BUILD_WORK)/bastet/.build_complete),)
@@ -19,7 +20,7 @@ bastet:
 else
 bastet: bastet-setup ncurses libboost
 	+$(MAKE) -C $(BUILD_WORK)/bastet all \
-		CXX="$(CXX) $(CXXFLAGS)"
+		CXX="$(CXX) $(CXXFLAGS) -std=c++14"
 	cp -a $(BUILD_WORK)/bastet/bastet $(BUILD_STAGE)/bastet/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_WORK)/bastet/bastet.6 $(BUILD_STAGE)/bastet/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man6
 	$(call AFTER_BUILD)
