5e98461e709f18fbc5611b8b4bd392e2400e7583
Cryptiiiic <liamwqs@gmail.com>
2024-04-20 23:05:41 -0700

    Fix libkrw tfp0 and switch libkrw-dopamine repos

--- makefiles/libkrw-dopamine.mk
+++ makefiles/libkrw-dopamine.mk
@@ -3,17 +3,26 @@ $(error Use the main Makefile)
 endif
 
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
-ifeq ($(shell [ "$(MEMO_CFVER)" -eq 1800 ] && echo 1),1)
+ifeq ($(shell [ "$(MEMO_CFVER)" -ge 1800 ] && echo 1),1)
 
 SUBPROJECTS             += libkrw-dopamine
-LIBKRW_DOPAMINE_VERSION := 1.0.0
-LIBKRW_DOPAMINE_MINOR   := 1
+DOPAMINE_VERSION        := 2.0.11
+CHOMA_COMMIT            := 54fedaf26175d5db27246d66b55d540ff6bcb83d
+LIBKRW_DOPAMINE_VERSION := 2.0.1
 DEB_LIBKRW_DOPAMINE_V   ?= $(LIBKRW_DOPAMINE_VERSION)
 LIBKRW_DOPAMINE_LIBS    := -ljailbreak -framework Foundation
 
 libkrw-dopamine-setup: setup
-	$(call GITHUB_ARCHIVE,Cryptiiiic,libjbdrw,$(LIBKRW_DOPAMINE_VERSION),v$(LIBKRW_DOPAMINE_VERSION))
-	$(call EXTRACT_TAR,libjbdrw-$(LIBKRW_DOPAMINE_VERSION).tar.gz,libjbdrw-$(LIBKRW_DOPAMINE_VERSION),libkrw-dopamine)
+	$(call GITHUB_ARCHIVE,opa334,Dopamine,$(DOPAMINE_VERSION),$(DOPAMINE_VERSION),Dopamine)
+	$(call EXTRACT_TAR,Dopamine-$(DOPAMINE_VERSION).tar.gz,Dopamine-$(DOPAMINE_VERSION),libkrw-dopamine/Dopamine)
+	$(call GITHUB_ARCHIVE,opa334,ChOma,$(CHOMA_COMMIT),$(CHOMA_COMMIT),ChOma)
+	$(call EXTRACT_TAR,ChOma-$(CHOMA_COMMIT).tar.gz,ChOma-$(CHOMA_COMMIT),libkrw-dopamine/ChOma)
+	mkdir -p $(BUILD_WORK)/libkrw-dopamine/include/{libjailbreak,choma}
+	cp -a $(BUILD_WORK)/libkrw-dopamine/Dopamine/Packages/libkrw-provider/src $(BUILD_WORK)/libkrw-dopamine/
+	cp -a $(BUILD_WORK)/libkrw-dopamine/Dopamine/BaseBin/libjailbreak/src/*.h $(BUILD_WORK)/libkrw-dopamine/include/libjailbreak
+	cp -a $(BUILD_WORK)/libkrw-dopamine/Dopamine/BaseBin/_external/include/libkrw $(BUILD_WORK)/libkrw-dopamine/include/
+	cp -a $(BUILD_WORK)/libkrw-dopamine/ChOma/src/*.h $(BUILD_WORK)/libkrw-dopamine/include/choma
+	rm -rf $(BUILD_WORK)/libkrw-dopamine/{Dopamine,ChOma}
 	mkdir -p $(BUILD_STAGE)/libkrw-dopamine/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libkrw
 
 ifneq ($(wildcard $(BUILD_WORK)/libkrw-dopamine/.build_complete),)
@@ -25,12 +34,11 @@ libkrw-dopamine: libkrw-dopamine-setup
 	# libkrw-dopamine.dylib
 	$(CC) $(CFLAGS) -fobjc-arc -dynamiclib \
 		-I$(BUILD_WORK)/libkrw-dopamine/include \
-		-DVERSION=$(LIBKRW_DOPAMINE_MINOR) \
 		-install_name "$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libkrw/libkrw-dopamine.dylib" \
 		-o $(BUILD_STAGE)/libkrw-dopamine/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libkrw/libkrw-dopamine.dylib \
-		$(BUILD_WORK)/libkrw-dopamine/src/jbdrw.m \
+		$(BUILD_WORK)/libkrw-dopamine/src/main.c \
 		$(LDFLAGS) \
-		-L$(BUILD_WORK)/libkrw-dopamine/lib \
+		-L$(BUILD_MISC)/libjailbreak \
 		$(LIBKRW_DOPAMINE_LIBS)
 	$(call AFTER_BUILD)
 endif
