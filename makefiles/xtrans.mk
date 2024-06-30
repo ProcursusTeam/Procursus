2cc0cd28d809f082df28ee4d4780412cd87dda6b
Nick Chan <towinchenmi@gmail.com>
2024-07-01 03:25:25 +0800

    xtrans: fix header include in installed files

--- makefiles/xtrans.mk
+++ makefiles/xtrans.mk
@@ -5,13 +5,13 @@ endif
 SUBPROJECTS    += xtrans
 XTRANS_VERSION := 1.4.0
 XTRANS_EPOCH   := 1.5.0
-DEB_XTRANS_V   ?= $(XTRANS_EPOCH)+really$(XTRANS_VERSION)
+DEB_XTRANS_V   ?= $(XTRANS_EPOCH)+really$(XTRANS_VERSION)-1
 
 xtrans-setup: setup
 	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://www.x.org/archive/individual/lib/xtrans-$(XTRANS_VERSION).tar.bz2{$(comma).sig})
 	$(call PGP_VERIFY,xtrans-$(XTRANS_VERSION).tar.bz2)
 	$(call EXTRACT_TAR,xtrans-$(XTRANS_VERSION).tar.bz2,xtrans-$(XTRANS_VERSION),xtrans)
-	sed -i 's|# include <stropts.h>|# include <sys/ioctl.h>|' $(BUILD_WORK)/xtrans/Xtranslcl.c
+	sed -i 's|# include <.*stropts\.h>|# include <sys/ioctl.h>|' $(BUILD_WORK)/xtrans/Xtranslcl.c
 
 ifneq ($(wildcard $(BUILD_WORK)/xtrans/.build_complete),)
 xtrans:
