b76c08329e92346501ef4036c7e6af0ede544455
Lorenzo <lorenzo_ferron@hotmail.it>
2024-07-11 18:54:38 +0200

    libassuan: Downgrade (again) to 2.5.7

--- makefiles/libassuan.mk
+++ makefiles/libassuan.mk
@@ -3,7 +3,7 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS     += libassuan
-LIBASSUAN_VERSION := 3.0.1
+LIBASSUAN_VERSION := 2.5.7
 DEB_LIBASSUAN_V   ?= $(LIBASSUAN_VERSION)
 
 libassuan-setup: setup
