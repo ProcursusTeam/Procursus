f026ae4e2e791131e319e95c63f9fcc8e428b91b
Keto <therealketo@gmail.com>
2024-08-12 16:05:00 -0400

    micro: Update to 2.0.13

--- makefiles/micro.mk
+++ makefiles/micro.mk
@@ -3,13 +3,18 @@ $(error Use the main Makefile)
 endif
 
 SUBPROJECTS    += micro
-MICRO_VERSION  := 2.0.10
+MICRO_VERSION  := 2.0.13
 DEB_MICRO_V    ?= $(MICRO_VERSION)
 
+# NOTE: Generating syntax files for micro on an iOS host doesn't work
+# NOTE: Make sure to clear Go's module cache (GOMODCACHE) after
+# each build so that syntax highlighting works on supported targets
+
 micro-setup: setup
-	$(call GITHUB_ARCHIVE,zyedidia,micro,$(MICRO_VERSION),v$(MICRO_VERSION),micro)
+	$(call GITHUB_ARCHIVE,zyedidia,micro,$(MICRO_VERSION),v$(MICRO_VERSION))
 	$(call EXTRACT_TAR,micro-$(MICRO_VERSION).tar.gz,micro-$(MICRO_VERSION),micro)
-	mkdir -p $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	$(call DO_PATCH,micro,micro,-p1)
+	mkdir -p $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{share/man/man1,bin}
 
 ifneq ($(wildcard $(BUILD_WORK)/micro/.build_complete),)
 micro:
@@ -17,20 +22,20 @@ micro:
 else
 micro: micro-setup
 	$(MAKE) -C $(BUILD_WORK)/micro build \
-		$(DEFAULT_GOLANG_FLAGS)
-	$(INSTALL) -Dm755 $(BUILD_WORK)/micro/micro $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/micro
-	$(INSTALL) -Dm644 $(BUILD_WORK)/micro/assets/packaging/micro.1 $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/micro.1
-	$(INSTALL) -Dm644 $(BUILD_WORK)/micro/assets/packaging/micro.desktop $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/applications/micro.desktop
+		$(DEFAULT_GOLANG_FLAGS) \
+		VERSION="$(MICRO_VERSION)" \
+		DATE="Procursus"
+	$(INSTALL) -Dm755 $(BUILD_WORK)/micro/micro $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/
+	$(INSTALL) -Dm644 $(BUILD_WORK)/micro/assets/packaging/micro.1 $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/
 	$(call AFTER_BUILD)
 endif
 
 micro-package: micro-stage
 	# micro.mk Package Structure
 	rm -rf $(BUILD_DIST)/micro
-	mkdir -p $(BUILD_DIST)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 
 	# micro.mk Prep micro
-	cp -a $(BUILD_STAGE)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,share} $(BUILD_DIST)/micro/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
+	cp -a $(BUILD_STAGE)/micro $(BUILD_DIST)
 
 	# micro.mk Sign
 	$(call SIGN,micro,general.xml)
