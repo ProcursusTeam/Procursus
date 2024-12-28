6e473671728e4de868f75a1adeee8e7299a7b75a
Cryptiiiic <liamwqs@gmail.com>
2024-12-28 04:43:02 -0800

    Fix include

--- makefiles/x8A4.mk
+++ makefiles/x8A4.mk
@@ -14,19 +14,20 @@ DEB_X8A4_V   ?= $(X8A4_VERSION)
 LIBX8A4_SOVERSION := 1
 
 x8A4-setup: setup
-#	$(call GITHUB_ARCHIVE,Cryptiiiic,x8A4,$(X8A4_VERSION),v$(X8A4_VERSION))
-#	$(call EXTRACT_TAR,x8A4-$(X8A4_VERSION).tar.gz,x8A4-$(X8A4_VERSION),x8A4)
-	cp -a ~/source/x8A4 $(BUILD_WORK)/
+	$(call GITHUB_ARCHIVE,Cryptiiiic,x8A4,$(X8A4_VERSION),v$(X8A4_VERSION))
+	$(call EXTRACT_TAR,x8A4-$(X8A4_VERSION).tar.gz,x8A4-$(X8A4_VERSION),x8A4)
 	rm -rf $(BUILD_WORK)/x8A4/Lib/{ChOma,XPF} $(BUILD_WORK)/x8A4/Lib/lib{choma,XPF}.a
 	$(call GITHUB_ARCHIVE,opa334,ChOma,$(CHOMA_COMMIT),$(CHOMA_COMMIT),ChOma)
 	$(call GITHUB_ARCHIVE,opa334,XPF,$(XPF_COMMIT),$(XPF_COMMIT),XPF)
 	$(call EXTRACT_TAR,ChOma-$(CHOMA_COMMIT).tar.gz,ChOma-$(CHOMA_COMMIT),x8A4/Lib/ChOma)
 	$(call EXTRACT_TAR,XPF-$(XPF_COMMIT).tar.gz,XPF-$(XPF_COMMIT),x8A4/Lib/XPF)
 	$(call DO_PATCH,choma,x8A4/Lib/ChOma,-p1)
+	$(call DO_PATCH,xpf,x8A4/Lib/XPF,-p1)
 	rm -rf $(BUILD_WORK)/x8A4/Lib/XPF/external/ios/include/choma/* $(BUILD_WORK)/x8A4/Lib/XPF/external/ios/lib/libchoma.*
-	rm -rf $(BUILD_WORK)/x8A4/Include/choma/* $(BUILD_WORK)/x8A4/Lib/XPF/external/ios/lib/libchoma.*
+	rm -rf $(BUILD_WORK)/x8A4/Include/XPF/* $(BUILD_WORK)/x8A4/Include/choma/* $(BUILD_WORK)/x8A4/Lib/XPF/external/ios/lib/libchoma.*
 	cp -a $(BUILD_WORK)/x8A4/Lib/ChOma/src/*.h $(BUILD_WORK)/x8A4/Lib/XPF/external/ios/include/choma/
 	cp -a $(BUILD_WORK)/x8A4/Lib/ChOma/src/*.h $(BUILD_WORK)/x8A4/Include/choma/
+	cp -a $(BUILD_WORK)/x8A4/Lib/XPF/src/*.h $(BUILD_WORK)/x8A4/Include/XPF
 	mkdir -p $(BUILD_STAGE)/x8A4/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{include,lib,bin}
 
 
