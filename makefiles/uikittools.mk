b6185221bf1ce3dda8b4baeee61e75e3f7c7d399
Nick Chan <towinchenmi@gmail.com>
2026-05-30 13:24:51 +0800

    uikittools: Update to 2.1.8

--- makefiles/uikittools.mk
+++ makefiles/uikittools.mk
@@ -7,7 +7,7 @@ SUBPROJECTS        += uikittools
 else
 STRAPPROJECTS      += uikittools
 endif
-UIKITTOOLS_VERSION := 2.1.7
+UIKITTOOLS_VERSION := 2.1.8
 DEB_UIKITTOOLS_V   ?= $(UIKITTOOLS_VERSION)
 
 uikittools-setup: setup
