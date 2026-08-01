3c398b0bd9736eb1518cc00b53b8fd417f1cfdfc
Nick Chan <towinchenmi@gmail.com>
2026-08-01 16:34:22 +0800

    apt: Update to 3.3.2
    Signed-off-by: Nick Chan <towinchenmi@gmail.com>

--- makefiles/apt.mk
+++ makefiles/apt.mk
@@ -3,8 +3,8 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS += apt
-APT_VERSION   := 2.9.4
-DEB_APT_V     ?= $(APT_VERSION)-1
+APT_VERSION   := 3.3.2
+DEB_APT_V     ?= $(APT_VERSION)
 
 ifeq ($(shell [ "$(CFVER_WHOLE)" -lt 1500 ] && echo 1),1)
 APT_CMAKE_ARGS := -DHAVE_PTSNAME_R=0
@@ -16,9 +16,7 @@ apt-setup: setup
 	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://salsa.debian.org/apt-team/apt/-/archive/$(APT_VERSION)/apt-$(APT_VERSION).tar.gz)
 	$(call EXTRACT_TAR,apt-$(APT_VERSION).tar.gz,apt-$(APT_VERSION),apt)
 	$(call DO_PATCH,apt,apt,-p1)
-ifneq (,$(findstring darwin,$(MEMO_TARGET)))
-	$(call DO_PATCH,apt-macos,apt,-p1)
-endif
+
 	if [ -f "$(BUILD_WORK)/apt/apt-private/private-output.cc" ]; then \
 		mv -f $(BUILD_WORK)/apt/apt-private/private-output.{cc,mm}; \
 	fi
@@ -35,6 +33,7 @@ else
 apt: apt-setup libgcrypt berkeleydb lz4 xxhash xz zstd gnutls gettext
 	cd $(BUILD_WORK)/apt/build && unset MACOSX_DEPLOYMENT_TARGET && cmake . \
 		$(DEFAULT_CMAKE_FLAGS) \
+		-DCMAKE_CXX_FLAGS="$(CXXFLAGS) -std=c++11" \
 		-DSTATE_DIR=$(MEMO_PREFIX)/var/lib/apt \
 		-DCACHE_DIR=$(MEMO_PREFIX)/var/cache/apt \
 		-DLOG_DIR=$(MEMO_PREFIX)/var/log/apt \
@@ -62,11 +61,11 @@ apt-package: apt-stage
 	rm -rf $(BUILD_DIST)/apt{,-utils} $(BUILD_DIST)/libapt*/
 	mkdir -p $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,lib,libexec/apt/{planners,solvers}} \
 	$(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{bin,libexec/apt/{planners,solvers}} \
-	$(BUILD_DIST)/libapt-pkg6.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,share} \
+	$(BUILD_DIST)/libapt-pkg7.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/{lib,share} \
 	$(BUILD_DIST)/libapt-pkg-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 
 	# apt.mk Prep apt
-	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/apt{,-cache,-cdrom,-config,-get,-key,-mark} $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/apt{,-cache,-cdrom,-config,-get,-mark} $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libapt-private.*.dylib $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/dpkg $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/{methods,apt-helper} $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt
@@ -76,12 +75,13 @@ apt-package: apt-stage
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)/{etc,var} $(BUILD_DIST)/apt/$(MEMO_PREFIX)
 	rm -f $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/*/man1/apt-{extracttemplates,ftparchive,sortpkgs}.1$(MEMO_MANPAGE_SUFFIX)
 	rm -f $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/apt-{extracttemplates,ftparchive,sortpkgs}.1$(MEMO_MANPAGE_SUFFIX)
-	rm -f $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/{apt-utils,libapt-pkg6.0}.mo
+	rm -f $(BUILD_DIST)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/{apt-utils,libapt-pkg7.0}.mo
 
 	# apt.mk Prep apt-utils
-	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/apt-{extracttemplates,ftparchive,sortpkgs} $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
+	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/apt-{ftparchive,sortpkgs} $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/planners/apt $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/planners
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/solvers/apt $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/solvers
+	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt/apt-extracttemplates $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/libexec/apt
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)
 	rm -rf $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man{5,7,8}
 	rm -rf $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/bash-completion
@@ -90,12 +90,12 @@ apt-package: apt-stage
 	done
 	rm -f $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/!(man1)/man1/apt-transport*.1$(MEMO_MANPAGE_SUFFIX)
 	rm -f $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/man/man1/apt-transport*.1$(MEMO_MANPAGE_SUFFIX)
-	rm -f $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/{apt,libapt-pkg6.0}.mo
+	rm -f $(BUILD_DIST)/apt-utils/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/{apt,libapt-pkg7.0}.mo
 
-	# apt.mk Prep libapt-pkg6.0
-	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libapt-pkg.6.0*.dylib $(BUILD_DIST)/libapt-pkg6.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
-	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale $(BUILD_DIST)/libapt-pkg6.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
-	rm -f $(BUILD_DIST)/libapt-pkg6.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/apt{,-utils}.mo
+	# apt.mk Prep libapt-pkg7.0
+	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libapt-pkg.7.0*.dylib $(BUILD_DIST)/libapt-pkg7.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
+	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale $(BUILD_DIST)/libapt-pkg7.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share
+	rm -f $(BUILD_DIST)/libapt-pkg7.0/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/share/locale/*/LC_MESSAGES/apt{,-utils}.mo
 
 	# apt.mk Prep libapt-pkg-dev
 	cp -a $(BUILD_STAGE)/apt/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib/libapt-pkg.dylib $(BUILD_DIST)/libapt-pkg-dev/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/lib
@@ -105,12 +105,12 @@ apt-package: apt-stage
 	# apt.mk Sign
 	$(call SIGN,apt,general.xml)
 	$(call SIGN,apt-utils,general.xml)
-	$(call SIGN,libapt-pkg6.0,general.xml)
+	$(call SIGN,libapt-pkg7.0,general.xml)
 
 	# apt.mk Make .debs
 	$(call PACK,apt,DEB_APT_V)
 	$(call PACK,apt-utils,DEB_APT_V)
-	$(call PACK,libapt-pkg6.0,DEB_APT_V)
+	$(call PACK,libapt-pkg7.0,DEB_APT_V)
 	$(call PACK,libapt-pkg-dev,DEB_APT_V)
 
 	# apt.mk Build cleanup
